a3fded301e8ac3edf025d5b43029ab3ff2d4b677
Warren Dukes <warren.dukes@gmail.com>
2004-03-12 15:26:41 +0000

    autogen.sh updates
    git-svn-id: https://svn.musicpd.org/mpd/trunk@255 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- autogen.sh
+++ autogen.sh
@@ -1,65 +1,138 @@
 #!/bin/sh
-#
-echo "Generating build information using aclocal, automake and autoconf"
-echo "This may take a while ..."
+# Run this to set up the build system: configure, makefiles, etc.
+# (based on the version in enlightenment's cvs)
 
-# Touch the timestamps on all the files since CVS messes them up
-#touch Makefile.am configure.in
-#touch libid3tag/Makefile.am libid3tag/configure.ac
-#touch libmad/Makefile.am libmad/configure.ac
+package="mpd"
 
-#rm -f configure
-#rm -f libid3tag/configure
-#rm -f libmad/configure
-#rm -f config.cache
-#rm -f config.status
-#rm -f libid3tag/config.status
-#rm -f libmad/config.status
-#rm -rf autom4te*.cache
-#rm -rf libid3tag/autom4te*.cache
-#rm -rf libmad/autom4te*.cache
-#rm -f aclocal.m4
-#rm -f libid3tag/aclocal.m4
-#rm -f libmad/aclocal.m4
+olddir=`pwd`
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-# Regenerate configuration files
-libtoolize -f -c
+cd "$srcdir"
+DIE=0
 
-for i in -1.7 -1.6 ''; do
-	if [ -z $ACLOCAL ]; then
-		which aclocal$i
-		if [ "$?" = "0" ]; then
-			ACLOCAL=aclocal$i
-		fi
-	fi
-	if [ -z $AUTOMAKE ]; then
-		which automake$i
-		if [ "$?" = "0" ]; then
-			AUTOMAKE=automake$i
-		fi
-	fi
+echo "checking for autoconf... "
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have autoconf installed to compile $package."
+        echo "Download the appropriate package for your distribution,"
+        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+        DIE=1
+}
+
+VERSIONGREP="sed -e s/.*[^0-9\.]\([0-9]\.[0-9]\).*/\1/"
+VERSIONMKINT="sed -e s/[^0-9]//"
+                                                                                
+# do we need automake?
+if test -r Makefile.am; then
+  AM_NEEDED=`fgrep AUTOMAKE_OPTIONS Makefile.am | $VERSIONGREP`
+  if test -z $AM_NEEDED; then
+    echo -n "checking for automake... "
+    AUTOMAKE=automake
+    ACLOCAL=aclocal
+    if ($AUTOMAKE --version < /dev/null > /dev/null 2>&1); then
+      echo "no"
+      AUTOMAKE=
+    else
+      echo "yes"
+    fi
+  else
+    echo -n "checking for automake $AM_NEEDED or later... "
+    for am in automake-$AM_NEEDED automake$AM_NEEDED automake; do
+      ($am --version < /dev/null > /dev/null 2>&1) || continue
+      ver=`$am --version < /dev/null | head -n 1 | $VERSIONGREP | $VERSIONMKINT`
+      verneeded=`echo $AM_NEEDED | $VERSIONMKINT`
+      if test $ver -ge $verneeded; then
+        AUTOMAKE=$am
+        echo $AUTOMAKE
+        break
+      fi
+    done
+    test -z $AUTOMAKE &&  echo "no"
+    echo -n "checking for aclocal $AM_NEEDED or later... "
+    for ac in aclocal-$AM_NEEDED aclocal$AM_NEEDED aclocal; do
+      ($ac --version < /dev/null > /dev/null 2>&1) || continue
+      ver=`$ac --version < /dev/null | head -n 1 | $VERSIONGREP | $VERSIONMKINT`
+      verneeded=`echo $AM_NEEDED | $VERSIONMKINT`
+      if test $ver -ge $verneeded; then
+        ACLOCAL=$ac
+        echo $ACLOCAL
+        break
+      fi
+    done
+    test -z $ACLOCAL && echo "no"
+  fi
+  test -z $AUTOMAKE || test -z $ACLOCAL && {
+        echo
+        echo "You must have automake installed to compile $package."
+        echo "Download the appropriate package for your distribution,"
+        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+        exit 1
+  }
+fi
+
+echo -n "checking for libtool... "
+for LIBTOOLIZE in libtoolize glibtoolize nope; do
+  (which $LIBTOOLIZE) > /dev/null 2>&1 && break
 done
+if test x$LIBTOOLIZE = xnope; then
+  echo "nope."
+  LIBTOOLIZE=libtoolize
+else
+  echo $LIBTOOLIZE
+fi
+($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile $package."
+	echo "Download the appropriate package for your system,"
+	echo "or get the source from one of the GNU ftp sites"
+	echo "listed in http://www.gnu.org/order/ftp.html"
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+        exit 1
+fi
+
+if test -z "$*"; then
+        echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+echo "Generating configuration files for $package, please wait...."
 
 if [ -d /usr/local/share/aclocal ]; then
-	$ACLOCAL -I /usr/local/share/aclocal
-else
-	$ACLOCAL
+	ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I /usr/local/share/aclocal"
 fi
-$AUTOMAKE --foreign --add-missing -c
+echo "  $ACLOCAL $ACLOCAL_FLAGS"
+$ACLOCAL $ACLOCAL_FLAGS
+
+echo "  $LIBTOOLIZE --automake"
+$LIBTOOLIZE --automake
+
+echo "  $AUTOMAKE --add-missing $AUTOMAKE_FLAGS"
+$AUTOMAKE --add-missing $AUTOMAKE_FLAGS 
+
+echo "  autoconf"
 autoconf
 
 cd src/libid3tag
-$ACLOCAL 
-$AUTOMAKE --foreign --add-missing -c
+echo "  [src/libid3tag] $ACLOCAL $ACLOCAL_FLAGS"
+$ACLOCAL $ACLOCAL_FLAGS
+echo "  [src/libid3tag] $AUTOMAKE --add-missing $AUTOMAKE_FLAGS"
+$AUTOMAKE --add-missing $AUTOMAKE_FLAGS 
+echo "  [src/libid3tag] autoconf"
 autoconf
 cd ../..
 
 cd src/libmad
-$ACLOCAL 
-$AUTOMAKE --foreign --add-missing -c
+echo "  [src/libmad] $ACLOCAL $ACLOCAL_FLAGS"
+$ACLOCAL $ACLOCAL_FLAGS
+echo "  [src/libmad] $AUTOMAKE --add-missing $AUTOMAKE_FLAGS"
+$AUTOMAKE --add-missing $AUTOMAKE_FLAGS 
+echo "  [src/libmad] autoconf"
 autoconf
 cd ../..
 
-# Run configure for this platform
-./configure "$@"
-
+cd $olddir
+$srcdir/configure "$@" && echo
