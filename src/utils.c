8216f304c46060a07d7cd93e9935de26d971c6c4
Warren Dukes <warren.dukes@gmail.com>
2004-04-01 13:43:10 +0000

    move sys/types.h include in utils.c, so it works for FreeBSD, from MDR
    git-svn-id: https://svn.musicpd.org/mpd/trunk@581 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/utils.c
+++ src/utils.c
@@ -21,9 +21,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
 #include <sys/select.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 char * myFgets(char * buffer, int bufferSize, FILE * fp) {
