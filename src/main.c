963754b0d4dbee560b69ce766013a6f60dd4ff15
Warren Dukes <warren.dukes@gmail.com>
2004-04-16 01:06:14 +0000

    msells versions patch to list supported formats
    git-svn-id: https://svn.musicpd.org/mpd/trunk@783 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/main.c
+++ src/main.c
@@ -32,6 +32,7 @@
 #include "volume.h"
 #include "log.h"
 #include "permission.h"
+#include "../config.h"
 
 #include <stdio.h>
 #include <sys/select.h>
@@ -87,6 +88,25 @@ void version() {
         ERROR("Copyright (C) 2003 Warren Dukes <shank@mercury.chem.pitt.edu>\n");
         ERROR("This is free software; see the source for copying conditions.  There is NO\n");
         ERROR("warranty; not even MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
+        ERROR("\n");
+        ERROR("Supported formats:\n");
+
+#ifdef HAVE_AUDIOFILE
+        ERROR(" wav");
+#endif
+#ifdef HAVE_MAD
+        ERROR(" mp3");
+#endif
+#ifdef HAVE_FAAD
+        ERROR(" mp4 aac");
+#endif
+#ifdef HAVE_FLAC
+        ERROR(" flac");
+#endif
+#ifdef HAVE_OGG
+        ERROR(" ogg");
+#endif
+        ERROR("\n");
 }
 
 void parseOptions(int argc, char ** argv, Options * options) {
