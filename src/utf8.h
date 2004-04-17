955428b8a27313d24faa546f92e9c5719232bc32
Warren Dukes <warren.dukes@gmail.com>
2004-04-17 17:42:57 +0000

    don't expose functions in utf8.h that don't need it
    git-svn-id: https://svn.musicpd.org/mpd/trunk@812 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/utf8.h
+++ src/utf8.h
@@ -19,15 +19,9 @@
 #ifndef UTF_8_H
 #define UTF_8_H
 
-unsigned char * latin1ToUtf8(unsigned char c);
-
 unsigned char * latin1StrToUtf8Dup(unsigned char * latin1);
 
 unsigned char * utf8StrToLatin1Dup(unsigned char * utf8);
-
-unsigned char utf8ToLatin1(unsigned char * utf8);
-
-int validateUtf8Char(unsigned char * utf8Char);
 
 int validUtf8String(unsigned char * string);
 
