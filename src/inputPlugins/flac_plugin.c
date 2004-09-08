2d9c0572835a0b67613ec88477eef6ec7aa814c0
Warren Dukes <warren.dukes@gmail.com>
2004-09-08 18:24:18 +0000

    flac spelling fixes from np
    git-svn-id: https://svn.musicpd.org/mpd/trunk@2116 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/inputPlugins/flac_plugin.c
+++ src/inputPlugins/flac_plugin.c
@@ -277,7 +277,7 @@ void flacError(const FLAC__SeekableStreamDecoder *dec,
 		ERROR("crc mismatch %s\n", data->path);
 		break;
 	default:
-		ERROR("unknow flac error %s\n", data->path);
+		ERROR("unknown flac error %s\n", data->path);
 	}
 }
 
@@ -298,7 +298,7 @@ void flacPrintErroredState(FLAC__SeekableStreamDecoderState state,
 		ERROR("flac seekable stream error: %s\n",file);
 		break;
 	case FLAC__SEEKABLE_STREAM_DECODER_ALREADY_INITIALIZED:
-		ERROR("flac decoder already initilaized: %s\n",file);
+		ERROR("flac decoder already initialized: %s\n",file);
 		break;
 	case FLAC__SEEKABLE_STREAM_DECODER_INVALID_CALLBACK:
 		ERROR("invalid flac callback\n");
