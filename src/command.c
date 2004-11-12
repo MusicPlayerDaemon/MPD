4735ddce40ae815593ee78e83410c05b7a687d1b
Warren Dukes <warren.dukes@gmail.com>
2004-11-12 01:47:52 +0000

    correct a type in the error message
    git-svn-id: https://svn.musicpd.org/mpd/trunk@2625 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/command.c
+++ src/command.c
@@ -449,7 +449,7 @@ int handleFind(FILE * fp, unsigned int * permission, int argArrayLength,
 			argArrayLength-1, &items);
 
 	if(numItems <= 0) {
-		commandError(fp, ACK_ERROR_ARG, "incorrect ags", NULL); 
+		commandError(fp, ACK_ERROR_ARG, "incorrect arguments", NULL); 
 		return -1;
 	}
 
