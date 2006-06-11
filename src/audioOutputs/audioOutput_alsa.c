30df7d49c5a4c5d377fed6111f8867f16be05a8c
J. Alexander Treuman <jat@spatialrift.net>
2006-06-11 23:59:18 +0000

    Reverting patch to "fix" the alsa plugin when used with dmix.  It ended up breaking the alsa rate plugin, and dmix seems to work fine without it.  Thanks to Skee from #mpd for testing.
    git-svn-id: https://svn.musicpd.org/mpd/trunk@4269 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/audioOutputs/audioOutput_alsa.c
+++ src/audioOutputs/audioOutput_alsa.c
@@ -240,7 +240,8 @@ static int alsa_openDevice(AudioOutput * audioOutput)
 	if(err < 0) goto error;
 
 	cmd = "snd_pcm_sw_params_set_start_threshold";
-	err = snd_pcm_sw_params_set_start_threshold(ad->pcmHandle, swparams, 0);
+	err = snd_pcm_sw_params_set_start_threshold(ad->pcmHandle, swparams,
+			alsa_buffer_size - alsa_period_size);
 	if(err < 0) goto error;
 
 	cmd = "snd_pcm_sw_params_set_avail_min";
