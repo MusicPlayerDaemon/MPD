ecb26ef1b0d3dcd1d0cae7ec51417013285d53e1
J. Alexander Treuman <jat@spatialrift.net>
2006-07-14 19:45:13 +0000

    Update copyright in --version
    git-svn-id: https://svn.musicpd.org/mpd/trunk@4334 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/main.c
+++ src/main.c
@@ -127,7 +127,7 @@ void usage(char * argv[]) {
 void version() {
         LOG("mpd (MPD: Music Player Daemon) %s\n",VERSION);
         LOG("\n");
-        LOG("Copyright (C) 2003-2004 Warren Dukes <warren.dukes@gmail.com>\n");
+        LOG("Copyright (C) 2003-2006 Warren Dukes <warren.dukes@gmail.com>\n");
         LOG("This is free software; see the source for copying conditions.  There is NO\n");
         LOG("warranty; not even MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
         LOG("\n");
