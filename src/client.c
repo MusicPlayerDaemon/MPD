a580f5fe688e7f987d3cf6ee21f27faed2557b01
Max Kellermann <max@duempel.org>
2008-09-10 11:41:34 +0200

    client: renamed local variable "selret" to "ret"
    It's easier to reuse the variable if it has a more generic name.

--- src/client.c
+++ src/client.c
@@ -506,7 +506,7 @@ int client_manager_io(void)
 	fd_set wfds;
 	fd_set efds;
 	struct client *client, *n;
-	int selret;
+	int ret;
 	int fdmax = 0;
 
 	FD_ZERO( &efds );
@@ -516,17 +516,17 @@ int client_manager_io(void)
 	registered_IO_add_fds(&fdmax, &rfds, &wfds, &efds);
 
 	main_notify_lock();
-	selret = select(fdmax + 1, &rfds, &wfds, &efds, NULL);
+	ret = select(fdmax + 1, &rfds, &wfds, &efds, NULL);
 	main_notify_unlock();
 
-	if (selret < 0) {
+	if (ret < 0) {
 		if (errno == EINTR)
 			return 0;
 
 		FATAL("select() failed: %s\n", strerror(errno));
 	}
 
-	registered_IO_consume_fds(&selret, &rfds, &wfds, &efds);
+	registered_IO_consume_fds(&ret, &rfds, &wfds, &efds);
 
 	getConnections(&rfds);
 
