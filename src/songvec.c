45334a23e18da4928f53ccb9ecc7afd5f7eca28e
Eric Wong <normalperson@yhbt.net>
2008-10-07 22:07:34 +0200

    songvec: lock traversals for thread-safe updates/reads
    Only one lock is used for all songvec traversals since
    they're rarely changed.  Also, minimize lock time and
    release it before calling iterator functions since they
    may block (updateSongInfo => stat/open/seek/read).
    
    This lock only protects songvecs (and all of them) during
    traversals; not the individual song structures themselves.

--- src/songvec.c
+++ src/songvec.c
@@ -1,6 +1,8 @@
 #include "songvec.h"
 #include "utils.h"
 
+static pthread_mutex_t nr_lock = PTHREAD_MUTEX_INITIALIZER;
+
 /* Only used for sorting/searchin a songvec, not general purpose compares */
 static int songvec_cmp(const void *s1, const void *s2)
 {
@@ -16,23 +18,32 @@ static size_t sv_size(struct songvec *sv)
 
 void songvec_sort(struct songvec *sv)
 {
+	pthread_mutex_lock(&nr_lock);
 	qsort(sv->base, sv->nr, sizeof(Song *), songvec_cmp);
+	pthread_mutex_unlock(&nr_lock);
 }
 
 Song *songvec_find(struct songvec *sv, const char *url)
 {
 	int i;
+	Song *ret = NULL;
 
-	for (i = sv->nr; --i >= 0; )
-		if (!strcmp(sv->base[i]->url, url))
-			return sv->base[i];
-	return NULL;
+	pthread_mutex_lock(&nr_lock);
+	for (i = sv->nr; --i >= 0; ) {
+		if (strcmp(sv->base[i]->url, url))
+			continue;
+		ret = sv->base[i];
+		break;
+	}
+	pthread_mutex_unlock(&nr_lock);
+	return ret;
 }
 
 int songvec_delete(struct songvec *sv, const Song *del)
 {
 	int i;
 
+	pthread_mutex_lock(&nr_lock);
 	for (i = sv->nr; --i >= 0; ) {
 		if (sv->base[i] != del)
 			continue;
@@ -45,37 +56,50 @@ int songvec_delete(struct songvec *sv, const Song *del)
 				(sv->nr - i + 1) * sizeof(Song *));
 			sv->base = xrealloc(sv->base, sv_size(sv));
 		}
-		return i;
+		break;
 	}
+	pthread_mutex_unlock(&nr_lock);
 
-	return -1; /* not found */
+	return i;
 }
 
 void songvec_add(struct songvec *sv, Song *add)
 {
+	pthread_mutex_lock(&nr_lock);
 	++sv->nr;
 	sv->base = xrealloc(sv->base, sv_size(sv));
 	sv->base[sv->nr - 1] = add;
+	pthread_mutex_unlock(&nr_lock);
 }
 
 void songvec_destroy(struct songvec *sv)
 {
+	pthread_mutex_lock(&nr_lock);
 	if (sv->base) {
 		free(sv->base);
 		sv->base = NULL;
 	}
 	sv->nr = 0;
+	pthread_mutex_unlock(&nr_lock);
 }
 
 int songvec_for_each(struct songvec *sv, int (*fn)(Song *, void *), void *arg)
 {
-	int i;
-	Song **sp = sv->base;
+	size_t i;
 
-	for (i = sv->nr; --i >= 0; ) {
-		if (fn(*sp++, arg) < 0)
+	pthread_mutex_lock(&nr_lock);
+	for (i = 0; i < sv->nr; ++i) {
+		Song *song = sv->base[i];
+
+		assert(song);
+		assert(*song->url);
+
+		pthread_mutex_unlock(&nr_lock); /* fn() may block */
+		if (fn(song, arg) < 0)
 			return -1;
+		pthread_mutex_lock(&nr_lock); /* sv->nr may change in fn() */
 	}
+	pthread_mutex_unlock(&nr_lock);
 
 	return 0;
 }
