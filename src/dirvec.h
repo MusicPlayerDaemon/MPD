96c681e2db9f8f707f80061360c9b6b680fe72e3
Max Kellermann <max@duempel.org>
2008-10-09 15:37:41 +0200

    dirvec: added dirvec_clear()

--- src/dirvec.h
+++ src/dirvec.h
@@ -16,6 +16,12 @@ int dirvec_delete(struct dirvec *dv, struct directory *del);
 
 void dirvec_add(struct dirvec *dv, struct directory *add);
 
+static inline void
+dirvec_clear(struct dirvec *dv)
+{
+	dv->nr = 0;
+}
+
 void dirvec_destroy(struct dirvec *dv);
 
 #endif /* DIRVEC_H */
