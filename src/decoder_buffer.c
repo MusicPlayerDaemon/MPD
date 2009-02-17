1a9756156e5f305271536aa7e1fd33be6c774ce2
Max Kellermann <max@duempel.org>
2009-02-17 22:39:45 +0100

    added the "decoder buffer" library
    The decoder buffer library may be used by decoder libraries such as
    "faad".

---
+++ src/decoder_buffer.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2003-2009 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "decoder_buffer.h"
+#include "decoder_api.h"
+
+#include <glib.h>
+
+#include <assert.h>
+
+struct decoder_buffer {
+	struct decoder *decoder;
+	struct input_stream *is;
+
+	/** the allocated size of the buffer */
+	size_t size;
+
+	/** the current length of the buffer */
+	size_t length;
+
+	/** number of bytes already consumed at the beginning of the
+	    buffer */
+	size_t consumed;
+
+	/** the actual buffer (dynamic size) */
+	unsigned char data[sizeof(size_t)];
+};
+
+struct decoder_buffer *
+decoder_buffer_new(struct decoder *decoder, struct input_stream *is,
+		   size_t size)
+{
+	struct decoder_buffer *buffer =
+		g_malloc(sizeof(*buffer) - sizeof(buffer->data) + size);
+
+	assert(is != NULL);
+	assert(size > 0);
+
+	buffer->decoder = decoder;
+	buffer->is = is;
+	buffer->size = size;
+	buffer->length = 0;
+	buffer->consumed = 0;
+
+	return buffer;
+}
+
+void
+decoder_buffer_free(struct decoder_buffer *buffer)
+{
+	assert(buffer != NULL);
+
+	g_free(buffer);
+}
+
+bool
+decoder_buffer_is_empty(const struct decoder_buffer *buffer)
+{
+	return buffer->consumed == buffer->length;
+}
+
+bool
+decoder_buffer_is_full(const struct decoder_buffer *buffer)
+{
+	return buffer->consumed == 0 && buffer->length == buffer->size;
+}
+
+static void
+decoder_buffer_shift(struct decoder_buffer *buffer)
+{
+	assert(buffer->consumed > 0);
+
+	buffer->length -= buffer->consumed;
+	memmove(buffer->data, buffer->data + buffer->consumed, buffer->length);
+	buffer->consumed = 0;
+}
+
+bool
+decoder_buffer_fill(struct decoder_buffer *buffer)
+{
+	size_t nbytes;
+
+	if (buffer->consumed > 0)
+		decoder_buffer_shift(buffer);
+
+	if (buffer->length >= buffer->size)
+		/* buffer is full */
+		return false;
+
+	nbytes = decoder_read(buffer->decoder, buffer->is,
+			      buffer->data + buffer->length,
+			      buffer->size - buffer->length);
+	if (nbytes == 0)
+		/* end of file, I/O error or decoder command
+		   received */
+		return false;
+
+	buffer->length += nbytes;
+	assert(buffer->length <= buffer->size);
+
+	return true;
+}
+
+const void *
+decoder_buffer_read(const struct decoder_buffer *buffer, size_t *length_r)
+{
+	if (buffer->consumed >= buffer->length)
+		/* buffer is empty */
+		return NULL;
+
+	*length_r = buffer->length - buffer->consumed;
+	return buffer->data + buffer->consumed;
+}
+
+void
+decoder_buffer_consume(struct decoder_buffer *buffer, size_t nbytes)
+{
+	/* just move the "consumed" pointer - decoder_buffer_shift()
+	   will do the real work later (called by
+	   decoder_buffer_fill()) */
+	buffer->consumed += nbytes;
+
+	assert(buffer->consumed <= buffer->length);
+}
---
+++ src/decoder_buffer.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2003-2009 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef MPD_DECODER_BUFFER_H
+#define MPD_DECODER_BUFFER_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+/**
+ * This objects handles buffered reads in decoder plugins easily.  You
+ * create a buffer object, and use its high-level methods to fill and
+ * read it.  It will automatically handle shifting the buffer.
+ */
+struct decoder_buffer;
+
+struct decoder;
+struct input_stream;
+
+/**
+ * Creates a new buffer.
+ *
+ * @param decoder the decoder object, used for decoder_read(), may be NULL
+ * @param is the input stream object where we should read from
+ * @param size the maximum size of the buffer
+ * @return the new decoder_buffer object
+ */
+struct decoder_buffer *
+decoder_buffer_new(struct decoder *decoder, struct input_stream *is,
+		   size_t size);
+
+/**
+ * Frees resources used by the decoder_buffer object.
+ */
+void
+decoder_buffer_free(struct decoder_buffer *buffer);
+
+bool
+decoder_buffer_is_empty(const struct decoder_buffer *buffer);
+
+bool
+decoder_buffer_is_full(const struct decoder_buffer *buffer);
+
+/**
+ * Read data from the input_stream and append it to the buffer.
+ *
+ * @return true if data was appended; false if there is no data
+ * available (yet), end of file, I/O error or a decoder command was
+ * received
+ */
+bool
+decoder_buffer_fill(struct decoder_buffer *buffer);
+
+/**
+ * Reads data from the buffer.  This data is not yet consumed, you
+ * have to call decoder_buffer_consume() to do that.  The returned
+ * buffer becomes invalid after a decoder_buffer_fill() or a
+ * decoder_buffer_consume() call.
+ *
+ * @param buffer the decoder_buffer object
+ * @param length_r pointer to a size_t where you will receive the
+ * number of bytes available
+ * @return a pointer to the read buffer, or NULL if there is no data
+ * available
+ */
+const void *
+decoder_buffer_read(const struct decoder_buffer *buffer, size_t *length_r);
+
+/**
+ * Consume (delete, invalidate) a part of the buffer.  The "nbytes"
+ * parameter must not be larger than the length returned by
+ * decoder_buffer_read().
+ *
+ * @param buffer the decoder_buffer object
+ * @param nbytes the number of bytes to consume
+ */
+void
+decoder_buffer_consume(struct decoder_buffer *buffer, size_t nbytes);
+
+#endif
