644fc4877642078c63cf13d6e65d6a3e7272996b
Max Kellermann <max@duempel.org>
2009-07-05 06:54:45 +0200

    conf: registered option "filter"
    Add this option to the user's manual.

--- src/conf.c
+++ src/conf.c
@@ -94,6 +94,7 @@ static struct config_entry config_entries[] = {
 	{ .name = CONF_DECODER, true, true },
 	{ .name = CONF_INPUT, true, true },
 	{ .name = CONF_GAPLESS_MP3_PLAYBACK, false, false },
+	{ .name = "filter", true, true },
 };
 
 static bool
