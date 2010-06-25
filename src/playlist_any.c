82ee278f539d51a26e5bf4796650d30821ccec58
Max Kellermann <max@duempel.org>
2010-06-25 21:45:04 +0200

    playlist_any: new library to simplify opening playlist files
    The new function playlist_open_any() combines playlist_mapper_open(),
    playlist_list_open_uri() and playlist_list_open_stream(), providing an
    easy API for all of them.

---
+++ src/playlist_any.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2003-2010 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "config.h"
+#include "playlist_any.h"
+#include "playlist_list.h"
+#include "playlist_mapper.h"
+#include "uri.h"
+#include "input_stream.h"
+
+#include <assert.h>
+
+static struct playlist_provider *
+playlist_open_remote(const char *uri, struct input_stream **is_r)
+{
+	assert(uri_has_scheme(uri));
+
+	struct playlist_provider *playlist = playlist_list_open_uri(uri);
+	if (playlist != NULL) {
+		*is_r = NULL;
+		return playlist;
+	}
+
+	GError *error = NULL;
+	struct input_stream *is = input_stream_open(uri, &error);
+	if (is == NULL) {
+		if (error != NULL) {
+			g_warning("Failed to open %s: %s",
+				  uri, error->message);
+			g_error_free(error);
+		}
+
+		return NULL;
+	}
+
+	playlist = playlist_list_open_stream(is, uri);
+	if (playlist == NULL) {
+		input_stream_close(is);
+		return NULL;
+	}
+
+	*is_r = is;
+	return playlist;
+}
+
+struct playlist_provider *
+playlist_open_any(const char *uri, struct input_stream **is_r)
+{
+	return uri_has_scheme(uri)
+		? playlist_open_remote(uri, is_r)
+		: playlist_mapper_open(uri, is_r);
+}
---
+++ src/playlist_any.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2003-2010 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MPD_PLAYLIST_ANY_H
+#define MPD_PLAYLIST_ANY_H
+
+#include <stdbool.h>
+
+struct playlist_provider;
+struct input_stream;
+
+/**
+ * Opens a playlist from the specified URI, which can be either an
+ * absolute remote URI (with a scheme) or a relative path to the
+ * music orplaylist directory.
+ *
+ * @param is_r on success, an input_stream object may be returned
+ * here, which must be closed after the playlist_provider object is
+ * freed
+ */
+struct playlist_provider *
+playlist_open_any(const char *uri, struct input_stream **is_r);
+
+#endif
