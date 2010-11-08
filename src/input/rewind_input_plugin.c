ad52eb236d8746dffce0a5b07bf58a62bf0835d8
Max Kellermann <max@duempel.org>
2010-11-08 10:37:09 +0100

    input/rewind: fix assertion failure
    The assertion added in MPD 0.15.14 was too much, it failed when the
    MIME type of a stream was NULL.

--- src/input/rewind_input_plugin.c
+++ src/input/rewind_input_plugin.c
@@ -84,7 +84,7 @@ copy_attributes(struct input_stream *dest)
 	const struct input_stream *src = &r->input;
 
 	assert(dest != src);
-	assert(dest->mime != src->mime);
+	assert(src->mime == NULL || dest->mime != src->mime);
 
 	dest->ready = src->ready;
 	dest->seekable = src->seekable;
