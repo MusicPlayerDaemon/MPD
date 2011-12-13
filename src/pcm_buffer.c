34aab116aee5fb7aa128c86ce8377f62ae63fe6e
Max Kellermann <max@duempel.org>
2011-12-13 19:48:33 +0100

    pcm_buffer: eliminate merge conflict fallout

--- src/pcm_buffer.c
+++ src/pcm_buffer.c
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
  * Copyright (C) 2003-2011 The Music Player Daemon Project
-=======
- * Copyright (C) 2003-2010 The Music Player Daemon Project
->>>>>>> v0.16.x
  * http://www.musicpd.org
  *
  * This program is free software; you can redistribute it and/or modify
