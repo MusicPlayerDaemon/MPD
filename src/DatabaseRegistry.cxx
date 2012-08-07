bb75e3ea553f2d577548430e2e99423ee74850ca
Max Kellermann <max@duempel.org>
2012-08-07 20:07:17 +0200

    DatabaseRegistry: list of plugins

---
+++ src/DatabaseRegistry.cxx
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2003-2012 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "DatabaseRegistry.hxx"
+#include "db/SimpleDatabasePlugin.hxx"
+
+#include <string.h>
+
+const DatabasePlugin *const database_plugins[] = {
+	&simple_db_plugin,
+	NULL
+};
+
+const DatabasePlugin *
+GetDatabasePluginByName(const char *name)
+{
+	for (auto i = database_plugins; *i != nullptr; ++i)
+		if (strcmp((*i)->name, name) == 0)
+			return *i;
+
+	return nullptr;
+}
