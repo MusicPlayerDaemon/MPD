MusicPlayerDaemon/MPD

commit 18076ac9b766485efc45931e62a164e5cc1a8542

PlayerThread: use pc.cond instead of main_cond
The main_cond variable was completely unnecessary.  The pc.cond object
can be used for both main->pc and pc->main.

src/PlayerControl.cxx
@@ -76,7 +76,7 @@ static void
 player_command_wait_locked(struct player_control *pc)
 {
 	while (pc->command != PLAYER_COMMAND_NONE)
-		g_cond_wait(main_cond, pc->mutex);
+		g_cond_wait(pc->cond, pc->mutex);
 }
 
 static void

src/PlayerThread.cxx
@@ -147,7 +147,7 @@ player_command_finished_locked(struct player_control *pc)
 	assert(pc->command != PLAYER_COMMAND_NONE);
 
 	pc->command = PLAYER_COMMAND_NONE;
-	g_cond_signal(main_cond);
+	g_cond_signal(pc->cond);
 }
 
 static void

src/Main.cxx
@@ -98,8 +98,6 @@ enum {
 GThread *main_task;
 GMainLoop *main_loop;
 
-GCond *main_cond;
-
 Partition *global_partition;
 
 static bool
@@ -404,7 +402,6 @@ int mpd_main(int argc, char *argv[])
 
 	main_task = g_thread_self();
 	main_loop = g_main_loop_new(NULL, FALSE);
-	main_cond = g_cond_new();
 
 	GlobalEvents::Initialize();
 	GlobalEvents::Register(GlobalEvents::IDLE, idle_event_emitted);
@@ -536,7 +533,6 @@ int mpd_main(int argc, char *argv[])
 	sticker_global_finish();
 #endif
 
-	g_cond_free(main_cond);
 	GlobalEvents::Deinitialize();
 
 	playlist_list_global_finish();

src/Main.hxx
@@ -26,8 +26,6 @@ extern GThread *main_task;
 
 extern GMainLoop *main_loop;
 
-extern GCond *main_cond;
-
 extern struct Partition *global_partition;
 
 /**

commit a9b62a2ece7f8b05bbb785edb47be8f37a19284e

PlayerControl: add second Cond object
This fixes a deadlock bug introduced by 18076ac9.  After all, the
second Cond was necessary.

The problem: two threads can wait for a signal at the same time.  The
player thread waits for the output thread to finish playback.  The
main thread waits for the player thread to complete a command.  The
output thread finishes playback, and sends a signal, which
unfortunately does not wake up the player thread, but the main
thread.  The main thread sees that the command is still not finished,
and waits again.  The signal is lost forever, and MPD is deadlocked.

src/PlayerControl.cxx
@@ -60,7 +60,7 @@ static void
 player_command_wait_locked(struct player_control *pc)
 {
 	while (pc->command != PLAYER_COMMAND_NONE)
-		pc->cond.wait(pc->mutex);
+		pc->ClientWait();
 }
 
 static void

src/PlayerControl.hxx
@@ -108,6 +108,13 @@ struct player_control {
 	 */
 	Cond cond;
 
+	/**
+	 * This object gets signalled when the player thread has
+	 * finished the #command.  It wakes up the client that waits
+	 * (i.e. the main thread).
+	 */
+	Cond client_cond;
+
 	enum player_command command;
 	enum player_state state;
 
@@ -191,7 +198,32 @@ struct player_control {
 	 * prior to calling this function.
 	 */
 	void Wait() {
+		assert(thread == g_thread_self());
+
 		cond.wait(mutex);
+	}
+
+	/**
+	 * Wake up the client waiting for command completion.
+	 *
+	 * Caller must lock the object.
+	 */
+	void ClientSignal() {
+		assert(thread == g_thread_self());
+
+		client_cond.signal();
+	}
+
+	/**
+	 * The client calls this method to wait for command
+	 * completion.
+	 *
+	 * Caller must lock the object.
+	 */
+	void ClientWait() {
+		assert(thread != g_thread_self());
+
+		client_cond.wait(mutex);
 	}
 
 	/**

src/PlayerThread.cxx
@@ -147,7 +147,7 @@ player_command_finished_locked(struct player_control *pc)
 	assert(pc->command != PLAYER_COMMAND_NONE);
 
 	pc->command = PLAYER_COMMAND_NONE;
-	pc->cond.signal();
+	pc->ClientSignal();
 }
 
 static void