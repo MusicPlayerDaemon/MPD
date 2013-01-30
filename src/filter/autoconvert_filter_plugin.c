9ee52d85d7cbff6c70f07503aa4698cbc6fbc00e
Max Kellermann <max@duempel.org>
2013-01-30 23:19:11 +0100

    filter/autoconvert: include cleanup

--- src/filter/autoconvert_filter_plugin.c
+++ src/filter/autoconvert_filter_plugin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2011 The Music Player Daemon Project
+ * Copyright (C) 2003-2013 The Music Player Daemon Project
  * http://www.musicpd.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -23,13 +23,11 @@
 #include "filter_plugin.h"
 #include "filter_internal.h"
 #include "filter_registry.h"
-#include "conf.h"
-#include "pcm_convert.h"
 #include "audio_format.h"
-#include "poison.h"
+
+#include <glib.h>
 
 #include <assert.h>
-#include <string.h>
 
 struct autoconvert_filter {
 	struct filter base;
