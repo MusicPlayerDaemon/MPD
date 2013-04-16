ac9ebe1439b3ec14e78b2fa898b435485c68e362
Max Kellermann <max@duempel.org>
2013-04-17 00:36:28 +0200

    scripts: delete obsolete scripts

--- scripts/mpd-indent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-indent -npro -kr -i8 -ts8 -sob -l80 -ss -ncs -cdw -cd0 -c0 -cp0 "$@"
-
-# there doesn't seem to be an indent switch for this, but this
-# forces goto labels to the left-most column, without indentation
-perl -i -p -e 's/^\s+(\w+):/$1:/g unless /^\s+default:/' "$@"
