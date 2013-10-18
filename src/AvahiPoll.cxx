MusicPlayerDaemon/MPD

commit c1f4f1fdb64d97b5c3461723a8482ca64efea30e

EventLoop: new implementation using epoll
Implement an event loop without GLib.

src/ZeroconfAvahi.cxx
@@ -41,7 +41,9 @@
 
 static char *avahiName;
 static int avahiRunning;
+#ifndef USE_EPOLL
 static AvahiGLibPoll *avahi_glib_poll;
+#endif
 static const AvahiPoll *avahi_poll;
 static AvahiClient *avahiClient;
 static AvahiEntryGroup *avahiGroup;
@@ -229,9 +231,14 @@ AvahiInit(EventLoop &loop, const char *serviceName)
 
 	avahiRunning = 1;
 
+#ifdef USE_EPOLL
+	// TODO
+	(void)loop;
+#else
 	avahi_glib_poll = avahi_glib_poll_new(loop.GetContext(),
 					      G_PRIORITY_DEFAULT);
 	avahi_poll = avahi_glib_poll_get(avahi_glib_poll);
+#endif
 
 	avahiClient = avahi_client_new(avahi_poll, AVAHI_CLIENT_NO_FAIL,
 				       avahiClientCallback, NULL, &error);
@@ -258,10 +265,14 @@ AvahiDeinit(void)
 		avahiClient = NULL;
 	}
 
+#ifdef USE_EPOLL
+	// TODO
+#else
 	if (avahi_glib_poll != NULL) {
 		avahi_glib_poll_free(avahi_glib_poll);
 		avahi_glib_poll = NULL;
 	}
+#endif
 
 	avahi_free(avahiName);
 	avahiName = NULL;

src/event/Call.cxx
@@ -27,7 +27,11 @@
 
 #include <assert.h>
 
-class BlockingCallMonitor final : DeferredMonitor {
+class BlockingCallMonitor final
+#ifndef USE_EPOLL
+	: DeferredMonitor
+#endif
+{
 	const std::function<void()> f;
 
 	Mutex mutex;
@@ -36,13 +40,24 @@ class BlockingCallMonitor final : DeferredMonitor {
 	bool done;
 
 public:
+#ifdef USE_EPOLL
+	BlockingCallMonitor(EventLoop &loop, std::function<void()> &&_f)
+		:f(std::move(_f)), done(false) {
+		loop.AddCall([this](){
+				this->DoRun();
+			});
+	}
+#else
 	BlockingCallMonitor(EventLoop &_loop, std::function<void()> &&_f)
 		:DeferredMonitor(_loop), f(std::move(_f)), done(false) {}
+#endif
 
 	void Run() {
+#ifndef USE_EPOLL
 		assert(!done);
 
 		Schedule();
+#endif
 
 		mutex.lock();
 		while (!done)
@@ -50,8 +65,18 @@ public:
 		mutex.unlock();
 	}
 
+#ifndef USE_EPOLL
 private:
 	virtual void RunDeferred() override {
+		DoRun();
+	}
+
+#else
+public:
+#endif
+	void DoRun() {
+		assert(!done);
+
 		f();
 
 		mutex.lock();

src/event/DeferredMonitor.cxx
@@ -24,19 +24,43 @@
 void
 DeferredMonitor::Cancel()
 {
+#ifdef USE_EPOLL
+	pending = false;
+#else
 	const auto id = source_id.exchange(0);
 	if (id != 0)
 		g_source_remove(id);
+#endif
 }
 
 void
 DeferredMonitor::Schedule()
 {
+#ifdef USE_EPOLL
+	if (!pending.exchange(true))
+		fd.Write();
+#else
 	const unsigned id = loop.AddIdle(Callback, this);
 	const auto old_id = source_id.exchange(id);
 	if (old_id != 0)
 		g_source_remove(old_id);
+#endif
 }
+
+#ifdef USE_EPOLL
+
+bool
+DeferredMonitor::OnSocketReady(unsigned)
+{
+	fd.Read();
+
+	if (pending.exchange(false))
+		RunDeferred();
+
+	return true;
+}
+
+#else
 
 void
 DeferredMonitor::Run()
@@ -53,3 +77,5 @@ DeferredMonitor::Callback(gpointer data)
 	monitor.Run();
 	return false;
 }
+
+#endif

src/event/DeferredMonitor.hxx
@@ -21,8 +21,14 @@
 #define MPD_SOCKET_DEFERRED_MONITOR_HXX
 
 #include "check.h"
+#include "gcc.h"
 
+#ifdef USE_EPOLL
+#include "SocketMonitor.hxx"
+#include "WakeFD.hxx"
+#else
 #include <glib.h>
+#endif
 
 #include <atomic>
 
@@ -31,21 +37,47 @@ class EventLoop;
 /**
  * Defer execution of an event into an #EventLoop.
  */
-class DeferredMonitor {
+class DeferredMonitor
+#ifdef USE_EPOLL
+	: private SocketMonitor
+#endif
+{
+#ifdef USE_EPOLL
+	std::atomic_bool pending;
+	WakeFD fd;
+#else
 	EventLoop &loop;
 
 	std::atomic<guint> source_id;
+#endif
 
 public:
+#ifdef USE_EPOLL
+	DeferredMonitor(EventLoop &_loop)
+		:SocketMonitor(_loop), pending(false) {
+		SocketMonitor::Open(fd.Get());
+		SocketMonitor::Schedule(SocketMonitor::READ);
+	}
+#else
 	DeferredMonitor(EventLoop &_loop)
 		:loop(_loop), source_id(0) {}
+#endif
 
 	~DeferredMonitor() {
+#ifdef USE_EPOLL
+		/* avoid closing the WakeFD twice */
+		SocketMonitor::Steal();
+#else
 		Cancel();
+#endif
 	}
 
 	EventLoop &GetEventLoop() {
+#ifdef USE_EPOLL
+		return SocketMonitor::GetEventLoop();
+#else
 		return loop;
+#endif
 	}
 
 	void Schedule();
@@ -55,8 +87,12 @@ protected:
 	virtual void RunDeferred() = 0;
 
 private:
+#ifdef USE_EPOLL
+	virtual bool OnSocketReady(unsigned flags) override final;
+#else
 	void Run();
 	static gboolean Callback(gpointer data);
+#endif
 };
 
 #endif /* MAIN_NOTIFY_H */

src/event/IdleMonitor.cxx
@@ -29,8 +29,13 @@ IdleMonitor::Cancel()
 	if (!IsActive())
 		return;
 
+#ifdef USE_EPOLL
+	active = false;
+	loop.RemoveIdle(*this);
+#else
 	g_source_remove(source_id);
 	source_id = 0;
+#endif
 }
 
 void
@@ -42,18 +47,31 @@ IdleMonitor::Schedule()
 		/* already scheduled */
 		return;
 
+#ifdef USE_EPOLL
+	active = true;
+	loop.AddIdle(*this);
+#else
 	source_id = loop.AddIdle(Callback, this);
+#endif
 }
 
 void
 IdleMonitor::Run()
 {
 	assert(loop.IsInside());
+
+#ifdef USE_EPOLL
+	assert(active);
+	active = false;
+#else
 	assert(source_id != 0);
 	source_id = 0;
+#endif
 
 	OnIdle();
 }
+
+#ifndef USE_EPOLL
 
 gboolean
 IdleMonitor::Callback(gpointer data)
@@ -62,3 +80,5 @@ IdleMonitor::Callback(gpointer data)
 	monitor.Run();
 	return false;
 }
+
+#endif

src/event/IdleMonitor.hxx
@@ -22,7 +22,9 @@
 
 #include "check.h"
 
+#ifndef USE_EPOLL
 #include <glib.h>
+#endif
 
 class EventLoop;
 
@@ -32,13 +34,26 @@ class EventLoop;
  * methods must be run from EventLoop's thread.
  */
 class IdleMonitor {
+#ifdef USE_EPOLL
+	friend class EventLoop;
+#endif
+
 	EventLoop &loop;
 
+#ifdef USE_EPOLL
+	bool active;
+#else
 	guint source_id;
+#endif
 
 public:
+#ifdef USE_EPOLL
+	IdleMonitor(EventLoop &_loop)
+		:loop(_loop), active(false) {}
+#else
 	IdleMonitor(EventLoop &_loop)
 		:loop(_loop), source_id(0) {}
+#endif
 
 	~IdleMonitor() {
 		Cancel();
@@ -49,7 +64,11 @@ public:
 	}
 
 	bool IsActive() const {
+#ifdef USE_EPOLL
+		return active;
+#else
 		return source_id != 0;
+#endif
 	}
 
 	void Schedule();
@@ -60,7 +79,9 @@ protected:
 
 private:
 	void Run();
+#ifndef USE_EPOLL
 	static gboolean Callback(gpointer data);
+#endif
 };
 
 #endif /* MAIN_NOTIFY_H */

src/event/Loop.cxx
@@ -19,6 +19,89 @@
 
 #include "config.h"
 #include "Loop.hxx"
+#include "system/clock.h"
+
+#ifdef USE_EPOLL
+
+#include "TimeoutMonitor.hxx"
+#include "SocketMonitor.hxx"
+#include "IdleMonitor.hxx"
+
+#include <algorithm>
+
+EventLoop::EventLoop(Default)
+	:SocketMonitor(*this),
+	 now_ms(::monotonic_clock_ms()),
+	 quit(false),
+	 n_events(0)
+{
+	SocketMonitor::Open(wake_fd.Get());
+	SocketMonitor::Schedule(SocketMonitor::READ);
+}
+
+EventLoop::~EventLoop()
+{
+	assert(idle.empty());
+	assert(timers.empty());
+
+	/* avoid closing the WakeFD twice */
+	SocketMonitor::Steal();
+}
+
+void
+EventLoop::Break()
+{
+	if (IsInside())
+		quit = true;
+	else
+		AddCall([this]() { Break(); });
+}
+
+bool
+EventLoop::RemoveFD(int _fd, SocketMonitor &m)
+{
+	for (unsigned i = 0, n = n_events; i < n; ++i)
+		if (events[i].data.ptr == &m)
+			events[i].events = 0;
+
+	return epoll.Remove(_fd);
+}
+
+void
+EventLoop::AddIdle(IdleMonitor &i)
+{
+	assert(std::find(idle.begin(), idle.end(), &i) == idle.end());
+
+	idle.push_back(&i);
+}
+
+void
+EventLoop::RemoveIdle(IdleMonitor &i)
+{
+	auto it = std::find(idle.begin(), idle.end(), &i);
+	assert(it != idle.end());
+
+	idle.erase(it);
+}
+
+void
+EventLoop::AddTimer(TimeoutMonitor &t, unsigned ms)
+{
+	timers.insert(TimerRecord(t, now_ms + ms));
+}
+
+void
+EventLoop::CancelTimer(TimeoutMonitor &t)
+{
+	for (auto i = timers.begin(), end = timers.end(); i != end; ++i) {
+		if (&i->timer == &t) {
+			timers.erase(i);
+			return;
+		}
+	}
+}
+
+#endif
 
 void
 EventLoop::Run()
@@ -26,10 +109,121 @@ EventLoop::Run()
 	assert(thread.IsNull());
 	thread = ThreadId::GetCurrent();
 
+#ifdef USE_EPOLL
+	assert(!quit);
+
+	do {
+		now_ms = ::monotonic_clock_ms();
+
+		/* invoke timers */
+
+		int timeout_ms;
+		while (true) {
+			auto i = timers.begin();
+			if (i == timers.end()) {
+				timeout_ms = -1;
+				break;
+			}
+
+			timeout_ms = i->due_ms - now_ms;
+			if (timeout_ms > 0)
+				break;
+
+			TimeoutMonitor &m = i->timer;
+			timers.erase(i);
+
+			m.Run();
+
+			if (quit)
+				return;
+		}
+
+		/* invoke idle */
+
+		const bool idle_empty = idle.empty();
+		while (!idle.empty()) {
+			IdleMonitor &m = *idle.front();
+			idle.pop_front();
+			m.Run();
+
+			if (quit)
+				return;
+		}
+
+		if (!idle_empty)
+			/* re-evaluate timers because one of the
+			   IdleMonitors may have added a new
+			   timeout */
+			continue;
+
+		/* wait for new event */
+
+		const int n = epoll.Wait(events, MAX_EVENTS, timeout_ms);
+		n_events = std::max(n, 0);
+
+		now_ms = ::monotonic_clock_ms();
+
+		assert(!quit);
+
+		/* invoke sockets */
+
+		for (int i = 0; i < n; ++i) {
+			const auto &e = events[i];
+
+			if (e.events != 0) {
+				SocketMonitor &m = *(SocketMonitor *)e.data.ptr;
+				m.Dispatch(e.events);
+
+				if (quit)
+					break;
+			}
+		}
+
+		n_events = 0;
+	} while (!quit);
+#else
 	g_main_loop_run(loop);
+#endif
 
 	assert(thread.IsInside());
 }
+
+#ifdef USE_EPOLL
+
+void
+EventLoop::AddCall(std::function<void()> &&f)
+{
+	mutex.lock();
+	calls.push_back(f);
+	mutex.unlock();
+
+	wake_fd.Write();
+}
+
+bool
+EventLoop::OnSocketReady(gcc_unused unsigned flags)
+{
+	assert(!quit);
+
+	wake_fd.Read();
+
+	mutex.lock();
+
+	while (!calls.empty() && !quit) {
+		auto f = std::move(calls.front());
+		calls.pop_front();
+
+		mutex.unlock();
+		f();
+		mutex.lock();
+	}
+
+	mutex.unlock();
+
+	return true;
+}
+
+#else
 
 guint
 EventLoop::AddIdle(GSourceFunc function, gpointer data)
@@ -60,3 +254,5 @@ EventLoop::AddTimeoutSeconds(guint interval_s,
 	g_source_attach(source, GetContext());
 	return source;
 }
+
+#endif

src/event/Loop.hxx
@@ -24,13 +24,76 @@
 #include "thread/Id.hxx"
 #include "gcc.h"
 
+#ifdef USE_EPOLL
+#include "system/EPollFD.hxx"
+#include "thread/Mutex.hxx"
+#include "WakeFD.hxx"
+#include "SocketMonitor.hxx"
+
+#include <functional>
+#include <list>
+#include <set>
+#else
 #include <glib.h>
+#endif
+
+#ifdef USE_EPOLL
+class TimeoutMonitor;
+class IdleMonitor;
+class SocketMonitor;
+#endif
 
 #include <assert.h>
 
-class EventLoop {
+class EventLoop final
+#ifdef USE_EPOLL
+	: private SocketMonitor
+#endif
+{
+#ifdef USE_EPOLL
+	struct TimerRecord {
+		/**
+		 * Projected monotonic_clock_ms() value when this
+		 * timer is due.
+		 */
+		const unsigned due_ms;
+
+		TimeoutMonitor &timer;
+
+		constexpr TimerRecord(TimeoutMonitor &_timer,
+				      unsigned _due_ms)
+			:due_ms(_due_ms), timer(_timer) {}
+
+		bool operator<(const TimerRecord &other) const {
+			return due_ms < other.due_ms;
+		}
+
+		bool IsDue(unsigned _now_ms) const {
+			return _now_ms >= due_ms;
+		}
+	};
+
+	EPollFD epoll;
+
+	WakeFD wake_fd;
+
+	std::multiset<TimerRecord> timers;
+	std::list<IdleMonitor *> idle;
+
+	Mutex mutex;
+	std::list<std::function<void()>> calls;
+
+	unsigned now_ms;
+
+	bool quit;
+
+	static constexpr unsigned MAX_EVENTS = 16;
+	unsigned n_events;
+	epoll_event events[MAX_EVENTS];
+#else
 	GMainContext *context;
 	GMainLoop *loop;
+#endif
 
 	/**
 	 * A reference to the thread that is currently inside Run().
@@ -38,6 +101,43 @@ class EventLoop {
 	ThreadId thread;
 
 public:
+#ifdef USE_EPOLL
+	struct Default {};
+
+	EventLoop(Default dummy=Default());
+	~EventLoop();
+
+	unsigned GetTimeMS() const {
+		return now_ms;
+	}
+
+	void Break();
+
+	bool AddFD(int _fd, unsigned flags, SocketMonitor &m) {
+		return epoll.Add(_fd, flags, &m);
+	}
+
+	bool ModifyFD(int _fd, unsigned flags, SocketMonitor &m) {
+		return epoll.Modify(_fd, flags, &m);
+	}
+
+	bool RemoveFD(int fd, SocketMonitor &m);
+
+	void AddIdle(IdleMonitor &i);
+	void RemoveIdle(IdleMonitor &i);
+
+	void AddTimer(TimeoutMonitor &t, unsigned ms);
+	void CancelTimer(TimeoutMonitor &t);
+
+	void AddCall(std::function<void()> &&f);
+
+	void Run();
+
+private:
+	virtual bool OnSocketReady(unsigned flags) override;
+
+public:
+#else
 	EventLoop()
 		:context(g_main_context_new()),
 		 loop(g_main_loop_new(context, false)),
@@ -52,16 +152,6 @@ public:
 	~EventLoop() {
 		g_main_loop_unref(loop);
 		g_main_context_unref(context);
-	}
-
-	/**
-	 * Are we currently running inside this EventLoop's thread?
-	 */
-	gcc_pure
-	bool IsInside() const {
-		assert(!thread.IsNull());
-
-		return thread.IsInside();
 	}
 
 	GMainContext *GetContext() {
@@ -85,6 +175,17 @@ public:
 
 	GSource *AddTimeoutSeconds(guint interval_s,
 				   GSourceFunc function, gpointer data);
+#endif
+
+	/**
+	 * Are we currently running inside this EventLoop's thread?
+	 */
+	gcc_pure
+	bool IsInside() const {
+		assert(!thread.IsNull());
+
+		return thread.IsInside();
+	}
 };
 
 #endif /* MAIN_NOTIFY_H */

commit d1c401fe7a637cdea728c8c2860d47c3d1e0efa0

ZeroconfAvahi: integrate into the EventLoop
Finally fixes the regression by commit c1f4f1fd.

Makefile.am
@@ -724,7 +724,9 @@ src_mpd_SOURCES += \
 	src/ZeroconfGlue.cxx src/ZeroconfGlue.hxx
 
 if HAVE_AVAHI
-src_mpd_SOURCES += src/ZeroconfAvahi.cxx src/ZeroconfAvahi.hxx
+src_mpd_SOURCES += \
+	src/AvahiPoll.cxx src/AvahiPoll.hxx \
+	src/ZeroconfAvahi.cxx src/ZeroconfAvahi.hxx
 endif
 
 if HAVE_BONJOUR
@@ -1346,7 +1348,7 @@ test_software_volume_LDADD = \
 
 test_run_avahi_SOURCES = \
 	src/Log.cxx \
-	src/ZeroconfAvahi.cxx \
+	src/ZeroconfAvahi.cxx src/AvahiPoll.cxx \
 	test/ShutdownHandler.cxx test/ShutdownHandler.hxx \
 	test/run_avahi.cxx
 test_run_avahi_CPPFLAGS = $(AM_CPPFLAGS) \
@@ -1354,6 +1356,7 @@ test_run_avahi_CPPFLAGS = $(AM_CPPFLAGS) \
 test_run_avahi_LDADD = \
 	libevent.a \
 	libsystem.a \
+	$(GLIB_LIBS) \
 	$(AVAHI_LIBS)
 
 test_run_normalize_SOURCES = test/run_normalize.cxx \

configure.ac
@@ -628,8 +628,8 @@ avahi)
 	;;
 esac
 
-MPD_AUTO_PKG(avahi, AVAHI, [avahi-client avahi-glib],
-	[avahi client library], [avahi client+glib not found])
+MPD_AUTO_PKG(avahi, AVAHI, [avahi-client],
+	[avahi client library], [avahi-client not found])
 if test x$enable_avahi = xyes; then
 	AC_DEFINE([HAVE_AVAHI], 1, [Define to enable Avahi Zeroconf support])
 	with_zeroconf=avahi

src/AvahiPoll.cxx
@@ -1,0 +1,147 @@
+/*
+ * Copyright (C) 2003-2013 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "config.h"
+#include "AvahiPoll.hxx"
+#include "event/Loop.hxx"
+#include "event/SocketMonitor.hxx"
+#include "event/TimeoutMonitor.hxx"
+
+static unsigned
+FromAvahiWatchEvent(AvahiWatchEvent e)
+{
+	return (e & AVAHI_WATCH_IN ? SocketMonitor::READ : 0) |
+		(e & AVAHI_WATCH_OUT ? SocketMonitor::WRITE : 0) |
+		(e & AVAHI_WATCH_ERR ? SocketMonitor::ERROR : 0) |
+		(e & AVAHI_WATCH_HUP ? SocketMonitor::HANGUP : 0);
+}
+
+static AvahiWatchEvent
+ToAvahiWatchEvent(unsigned e)
+{
+	return AvahiWatchEvent((e & SocketMonitor::READ ? AVAHI_WATCH_IN : 0) |
+			       (e & SocketMonitor::WRITE ? AVAHI_WATCH_OUT : 0) |
+			       (e & SocketMonitor::ERROR ? AVAHI_WATCH_ERR : 0) |
+			       (e & SocketMonitor::HANGUP ? AVAHI_WATCH_HUP : 0));
+}
+
+struct AvahiWatch final : private SocketMonitor {
+private:
+	const AvahiWatchCallback callback;
+	void *const userdata;
+
+	AvahiWatchEvent received;
+
+public:
+	AvahiWatch(int _fd, AvahiWatchEvent _event,
+		   AvahiWatchCallback _callback, void *_userdata,
+		   EventLoop &_loop)
+		:SocketMonitor(_fd, _loop),
+		 callback(_callback), userdata(_userdata),
+		 received(AvahiWatchEvent(0)) {
+		Schedule(FromAvahiWatchEvent(_event));
+	}
+
+	static void WatchUpdate(AvahiWatch *w, AvahiWatchEvent event) {
+		w->Schedule(FromAvahiWatchEvent(event));
+	}
+
+	static AvahiWatchEvent WatchGetEvents(AvahiWatch *w) {
+		return w->received;
+	}
+
+	static void WatchFree(AvahiWatch *w) {
+		delete w;
+	}
+
+protected:
+	virtual bool OnSocketReady(unsigned flags) {
+		received = ToAvahiWatchEvent(flags);
+		callback(this, Get(), received, userdata);
+		received = AvahiWatchEvent(0);
+		return true;
+	}
+};
+
+static constexpr unsigned
+TimevalToMS(const timeval &tv)
+{
+	return tv.tv_sec * 1000 + (tv.tv_usec + 500) / 1000;
+}
+
+struct AvahiTimeout final : private TimeoutMonitor {
+private:
+	const AvahiTimeoutCallback callback;
+	void *const userdata;
+
+public:
+	AvahiTimeout(const struct timeval *tv,
+		     AvahiTimeoutCallback _callback, void *_userdata,
+		     EventLoop &_loop)
+		:TimeoutMonitor(_loop),
+		 callback(_callback), userdata(_userdata) {
+		if (tv != nullptr)
+			Schedule(TimevalToMS(*tv));
+	}
+
+	static void TimeoutUpdate(AvahiTimeout *t, const struct timeval *tv) {
+		if (tv != nullptr)
+			t->Schedule(TimevalToMS(*tv));
+		else
+			t->Cancel();
+	}
+
+	static void TimeoutFree(AvahiTimeout *t) {
+		delete t;
+	}
+
+protected:
+	virtual void OnTimeout() {
+		callback(this, userdata);
+	}
+};
+
+MyAvahiPoll::MyAvahiPoll(EventLoop &_loop):event_loop(_loop)
+{
+	watch_new = WatchNew;
+	watch_update = AvahiWatch::WatchUpdate;
+	watch_get_events = AvahiWatch::WatchGetEvents;
+	watch_free = AvahiWatch::WatchFree;
+	timeout_new = TimeoutNew;
+	timeout_update = AvahiTimeout::TimeoutUpdate;
+	timeout_free = AvahiTimeout::TimeoutFree;
+}
+
+AvahiWatch *
+MyAvahiPoll::WatchNew(const AvahiPoll *api, int fd, AvahiWatchEvent event,
+		      AvahiWatchCallback callback, void *userdata) {
+	const MyAvahiPoll &poll = *(const MyAvahiPoll *)api;
+
+	return new AvahiWatch(fd, event, callback, userdata,
+			      poll.event_loop);
+}
+
+AvahiTimeout *
+MyAvahiPoll::TimeoutNew(const AvahiPoll *api, const struct timeval *tv,
+			AvahiTimeoutCallback callback, void *userdata) {
+	const MyAvahiPoll &poll = *(const MyAvahiPoll *)api;
+
+	return new AvahiTimeout(tv, callback, userdata,
+				poll.event_loop);
+}

src/AvahiPoll.hxx
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2003-2013 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MPD_AVAHI_POLL_HXX
+#define MPD_AVAHI_POLL_HXX
+
+#include "check.h"
+#include "Compiler.h"
+
+#include <avahi-common/watch.h>
+
+class EventLoop;
+
+class MyAvahiPoll final : public AvahiPoll {
+	EventLoop &event_loop;
+
+public:
+	MyAvahiPoll(EventLoop &_loop);
+
+private:
+	static AvahiWatch *WatchNew(const AvahiPoll *api, int fd,
+				    AvahiWatchEvent event,
+				    AvahiWatchCallback callback,
+				    void *userdata);
+
+	static AvahiTimeout *TimeoutNew(const AvahiPoll *api,
+					const struct timeval *tv,
+					AvahiTimeoutCallback callback,
+					void *userdata);
+};
+
+#endif

src/ZeroconfAvahi.cxx
@@ -19,33 +19,29 @@
 
 #include "config.h"
 #include "ZeroconfAvahi.hxx"
+#include "AvahiPoll.hxx"
 #include "ZeroconfInternal.hxx"
 #include "Listen.hxx"
-#include "event/Loop.hxx"
 #include "system/FatalError.hxx"
 #include "util/Domain.hxx"
 #include "Log.hxx"
 
-#include <glib.h>
-
 #include <avahi-client/client.h>
 #include <avahi-client/publish.h>
 
+#include <avahi-common/watch.h>
 #include <avahi-common/alternative.h>
 #include <avahi-common/domain.h>
 #include <avahi-common/malloc.h>
 #include <avahi-common/error.h>
 
-#include <avahi-glib/glib-watch.h>
+#include <stddef.h>
 
 static constexpr Domain avahi_domain("avahi");
 
 static char *avahiName;
 static int avahiRunning;
-#ifndef USE_EPOLL
-static AvahiGLibPoll *avahi_glib_poll;
-#endif
-static const AvahiPoll *avahi_poll;
+static MyAvahiPoll *avahi_poll;
 static AvahiClient *avahiClient;
 static AvahiEntryGroup *avahiGroup;
 
@@ -246,15 +242,7 @@ AvahiInit(EventLoop &loop, const char *serviceName)
 
 	avahiRunning = 1;
 
-#ifdef USE_EPOLL
-	// TODO
-	(void)loop;
-	if (1==1) return;
-#else
-	avahi_glib_poll = avahi_glib_poll_new(loop.GetContext(),
-					      G_PRIORITY_DEFAULT);
-	avahi_poll = avahi_glib_poll_get(avahi_glib_poll);
-#endif
+	avahi_poll = new MyAvahiPoll(loop);
 
 	int error;
 	avahiClient = avahi_client_new(avahi_poll, AVAHI_CLIENT_NO_FAIL,
@@ -282,14 +270,8 @@ AvahiDeinit(void)
 		avahiClient = NULL;
 	}
 
-#ifdef USE_EPOLL
-	// TODO
-#else
-	if (avahi_glib_poll != NULL) {
-		avahi_glib_poll_free(avahi_glib_poll);
-		avahi_glib_poll = NULL;
-	}
-#endif
+	delete avahi_poll;
+	avahi_poll = nullptr;
 
 	avahi_free(avahiName);
 	avahiName = NULL;