964aa42b0479efac9a811ff071f27f28a27a6ede
Max Kellermann <max@duempel.org>
2013-10-18 08:37:28 +0200

    test/run_avahi: new debug program

---
+++ test/run_avahi.cxx
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2003-2013 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "config.h"
+#include "event/Loop.hxx"
+#include "ShutdownHandler.hxx"
+#include "ZeroconfAvahi.hxx"
+
+#include <stdlib.h>
+
+unsigned listen_port = 1234;
+
+int
+main(gcc_unused int argc, gcc_unused char **argv)
+{
+	EventLoop event_loop((EventLoop::Default()));
+	const ShutdownHandler shutdown_handler(event_loop);
+
+	AvahiInit(event_loop, "test");
+
+	event_loop.Run();
+
+	AvahiDeinit();
+
+	return EXIT_SUCCESS;
+}
