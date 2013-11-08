MusicPlayerDaemon/MPD

commit 77c63511d8809f7785328138e7e3a50303302730

PlayerThread: reduce the number of DecoderThread wakeups
After the number of decoded chunks has fallen below the threshold, the
PlayerThread woke up the DecoderThread over and over.  This commit
adds a boolean flag that avoids these duplicate wakeups, and thus
reduces the number of system calls.

src/PlayerThread.cxx
@@ -69,6 +69,12 @@ class Player {
 	bool decoder_starting;
 
 	/**
+	 * Did we wake up the DecoderThread recently?  This avoids
+	 * duplicate wakeup calls.
+	 */
+	bool decoder_woken;
+
+	/**
 	 * is the player paused?
 	 */
 	bool paused;
@@ -133,6 +139,7 @@ public:
 		:pc(_pc), dc(_dc), buffer(_buffer),
 		 buffering(true),
 		 decoder_starting(false),
+		 decoder_woken(false),
 		 paused(false),
 		 queued(true),
 		 output_open(false),
@@ -861,8 +868,13 @@ Player::PlayNextChunk()
 	pc.Lock();
 	if (!dc.IsIdle() &&
 	    dc.pipe->GetSize() <= (pc.buffered_before_play +
-				   buffer.GetSize() * 3) / 4)
-		dc.Signal();
+				   buffer.GetSize() * 3) / 4) {
+		if (!decoder_woken) {
+			decoder_woken = true;
+			dc.Signal();
+		}
+	} else
+		decoder_woken = false;
 	pc.Unlock();
 
 	return true;

commit 2789493a5f6bdc8239c321c9d15987bf596f0b09

PlayerThread: fix stuck MPD after song change (0.18.2 regression)
Commit 77c63511 caused MPD to become stuck right after a song change.
The problem was that at some point, the MusicBuffer became full, and
the DecoderThread working on the next song waits for the PlayerThread.
However, the PlayerThread was stuck in a loop of g_usleep() calls, and
never bothered to tell the DecoderThread that the MusicBuffer is not
full anymore.  This bug is very old, but its chance to occur went from
nearly 0% to nearly 100%.

The fix is to wake up the DecoderThread before waiting for it.  As a
side effect, I replaced the g_usleep() call with a Cond::Wait() call.

src/PlayerThread.cxx
@@ -36,8 +36,6 @@
 #include "util/Domain.hxx"
 #include "Log.hxx"
 
-#include <glib.h>
-
 #include <string.h>
 
 static constexpr Domain player_domain("player");
@@ -1043,8 +1041,14 @@ Player::Run()
 			   output thread is still busy, so it's
 			   okay */
 
-			/* XXX synchronize in a better way */
-			g_usleep(10000);
+			pc.Lock();
+
+			/* wake up the decoder (just in case it's
+			   waiting for space in the MusicBuffer) and
+			   wait for it */
+			dc.Signal();
+			dc.WaitForDecoder();
+			continue;
 		} else if (IsDecoderAtNextSong()) {
 			/* at the beginning of a new song */
 