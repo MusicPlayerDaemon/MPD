69867015e97183fb36e5bb85ea190583f7f60082
Max Kellermann <max@duempel.org>
2013-12-03 11:31:55 +0100

    DespotifyUtils: use class TagBuilder

--- src/DespotifyUtils.cxx
+++ src/DespotifyUtils.cxx
@@ -19,6 +19,7 @@
 
 #include "DespotifyUtils.hxx"
 #include "tag/Tag.hxx"
+#include "tag/TagBuilder.hxx"
 #include "ConfigGlobal.hxx"
 #include "ConfigOption.hxx"
 #include "util/Domain.hxx"
@@ -90,25 +91,24 @@ mpd_despotify_tag_from_track(struct ds_track *track)
 	char comment[80];
 	char date[20];
 
-	Tag *tag = new Tag();
-
 	if (!track->has_meta_data)
-		return tag;
+		return new Tag();
 
+	TagBuilder tag;
 	snprintf(tracknum, sizeof(tracknum), "%d", track->tracknumber);
 	snprintf(date, sizeof(date), "%d", track->year);
 	snprintf(comment, sizeof(comment), "Bitrate %d Kbps, %sgeo restricted",
 		 track->file_bitrate / 1000,
 		 track->geo_restricted ? "" : "not ");
-	tag->AddItem(TAG_TITLE, track->title);
-	tag->AddItem(TAG_ARTIST, track->artist->name);
-	tag->AddItem(TAG_TRACK, tracknum);
-	tag->AddItem(TAG_ALBUM, track->album);
-	tag->AddItem(TAG_DATE, date);
-	tag->AddItem(TAG_COMMENT, comment);
-	tag->time = track->length / 1000;
+	tag.AddItem(TAG_TITLE, track->title);
+	tag.AddItem(TAG_ARTIST, track->artist->name);
+	tag.AddItem(TAG_TRACK, tracknum);
+	tag.AddItem(TAG_ALBUM, track->album);
+	tag.AddItem(TAG_DATE, date);
+	tag.AddItem(TAG_COMMENT, comment);
+	tag.SetTime(track->length / 1000);
 
-	return tag;
+	return tag.Commit();
 }
 
 struct despotify_session *mpd_despotify_get_session(void)
