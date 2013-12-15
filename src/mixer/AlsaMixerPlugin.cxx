MusicPlayerDaemon/MPD

commit 8e38b4f83c98ae61e9f843c3d770dcbac699c98c

mixer/alsa: use BlockingCall() instead of EventLoop::AddCall()
This is safer, and works without epoll().  Fixes a build failure with
uClibc, which does not support epoll().

src/mixer/AlsaMixerPlugin.cxx
@@ -24,6 +24,7 @@
 #include "Main.hxx"
 #include "event/MultiSocketMonitor.hxx"
 #include "event/Loop.hxx"
+#include "event/Call.hxx"
 #include "util/ASCII.hxx"
 #include "util/ReusableArray.hxx"
 #include "util/Error.hxx"
@@ -46,7 +47,7 @@ class AlsaMixerMonitor final : private MultiSocketMonitor {
 public:
 	AlsaMixerMonitor(EventLoop &_loop, snd_mixer_t *_mixer)
 		:MultiSocketMonitor(_loop), mixer(_mixer) {
-		_loop.AddCall([this](){ InvalidateSockets(); });
+		BlockingCall(_loop, [this](){ InvalidateSockets(); });
 	}
 
 private:

commit 42a09ff17a47ec670b9a6d3ecd0aae14e0d52cde

mixer/alsa: fix deadlock
This deadlock was a regression by commit 8e38b4f8.  Since we currently
can't resolve this, let's revert the commit, and add a GLib specific
workaround for the build failure.

src/mixer/AlsaMixerPlugin.cxx
@@ -47,10 +47,22 @@ class AlsaMixerMonitor final : private MultiSocketMonitor {
 public:
 	AlsaMixerMonitor(EventLoop &_loop, snd_mixer_t *_mixer)
 		:MultiSocketMonitor(_loop), mixer(_mixer) {
-		BlockingCall(_loop, [this](){ InvalidateSockets(); });
+#ifdef USE_EPOLL
+		_loop.AddCall([this](){ InvalidateSockets(); });
+#else
+		_loop.AddIdle(InitAlsaMixerMonitor, this);
+#endif
 	}
 
 private:
+#ifndef USE_EPOLL
+	static gboolean InitAlsaMixerMonitor(gpointer data) {
+		AlsaMixerMonitor &amm = *(AlsaMixerMonitor *)data;
+		amm.InvalidateSockets();
+		return false;
+	}
+#endif
+
 	virtual int PrepareSockets() override;
 	virtual void DispatchSockets() override;
 };