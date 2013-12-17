MusicPlayerDaemon/MPD

commit 3b0fea5fae8ce52d2d4aa9ae7accc1f7f33592e1

input/cdio_paranoia: support libcdio-paranoia 0.90

src/input/CdioParanoiaInputPlugin.cxx
@@ -41,7 +41,12 @@
 #include <glib.h>
 #include <assert.h>
 
+#ifdef HAVE_CDIO_PARANOIA_PARANOIA_H
+#include <cdio/parannoia/paranoia.h>
+#else
 #include <cdio/paranoia.h>
+#endif
+
 #include <cdio/cd_types.h>
 
 struct CdioParanoiaInputStream {

configure.ac
@@ -749,6 +749,7 @@ MPD_AUTO_PKG(cdio_paranoia, CDIO_PARANOIA, [libcdio_paranoia],
 if test x$enable_cdio_paranoia = xyes; then
 	AC_DEFINE([ENABLE_CDIO_PARANOIA], 1,
 		[Define to enable libcdio_paranoia support])
+	AC_CHECK_HEADERS(cdio/paranoia/paranoia.h)
 fi
 
 AM_CONDITIONAL(ENABLE_CDIO_PARANOIA, test x$enable_cdio_paranoia = xyes)

commit 97fc001180e13fb6eea678d6e2c3926418a1b6bb

input/cdio: fix typo in #include path
Broken by commit 3b0fea5f

src/input/CdioParanoiaInputPlugin.cxx
@@ -42,7 +42,7 @@
 #include <assert.h>
 
 #ifdef HAVE_CDIO_PARANOIA_PARANOIA_H
-#include <cdio/parannoia/paranoia.h>
+#include <cdio/paranoia/paranoia.h>
 #else
 #include <cdio/paranoia.h>
 #endif