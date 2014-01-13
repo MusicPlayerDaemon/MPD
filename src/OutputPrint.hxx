4f83c60296de90933dc898193bd212c8d3b3a869
Max Kellermann <max@duempel.org>
2014-01-13 22:30:36 +0100

    copyright year 2014

--- src/OutputPrint.hxx
+++ src/OutputPrint.hxx
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2003-2013 The Music Player Daemon Project
+ * Copyright (C) 2003-2014 The Music Player Daemon Project
  * http://www.musicpd.org
  *
  * This program is free software; you can redistribute it and/or modify
