19a982cf693e660ce296a29a9830f1e94d9f1b4c
Max Kellermann <max@duempel.org>
2014-02-07 00:29:59 +0100

    Mapper: convert IsInDatabase() check to assertion in map_song_detach()

--- src/Mapper.cxx
+++ src/Mapper.cxx
@@ -238,8 +238,9 @@ DetachedSong
 map_song_detach(const LightSong &song)
 {
 	DetachedSong detached(song);
+	assert(detached.IsInDatabase());
 
-	if (detached.IsInDatabase() && !detached.HasRealURI()) {
+	if (!detached.HasRealURI()) {
 		const auto uri = song.GetURI();
 		detached.SetRealURI(PathTraitsUTF8::Build(music_dir_utf8.c_str(),
 							  uri.c_str()));
