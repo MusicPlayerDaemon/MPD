MusicPlayerDaemon/MPD

commit df5f9f4a158c244006d1965a328779f284cae7d9

Listen: add Partition reference

src/Main.cxx
@@ -432,7 +432,8 @@ int mpd_main(int argc, char *argv[])
 	const unsigned max_clients = config_get_positive(CONF_MAX_CONN, 10);
 	instance->client_list = new ClientList(max_clients);
 
-	if (!listen_global_init(*instance->event_loop, error)) {
+	if (!listen_global_init(*instance->event_loop, *instance->partition,
+				error)) {
 		LogError(error);
 		return EXIT_FAILURE;
 	}

src/Listen.cxx
@@ -19,8 +19,6 @@
 
 #include "config.h"
 #include "Listen.hxx"
-#include "Main.hxx"
-#include "Instance.hxx"
 #include "client/Client.hxx"
 #include "config/ConfigData.hxx"
 #include "config/ConfigGlobal.hxx"
@@ -43,13 +41,16 @@ static constexpr Domain listen_domain("listen");
 #define DEFAULT_PORT	6600
 
 class ClientListener final : public ServerSocket {
+	Partition &partition;
+
 public:
-	ClientListener(EventLoop &_loop):ServerSocket(_loop) {}
+	ClientListener(EventLoop &_loop, Partition &_partition)
+		:ServerSocket(_loop), partition(_partition) {}
 
 private:
 	virtual void OnAccept(int fd, const sockaddr &address,
 			      size_t address_length, int uid) {
-		client_new(GetEventLoop(), *instance->partition,
+		client_new(GetEventLoop(), partition,
 			   fd, &address, address_length, uid);
 	}
 };
@@ -101,14 +102,14 @@ listen_systemd_activation(Error &error_r)
 }
 
 bool
-listen_global_init(EventLoop &loop, Error &error)
+listen_global_init(EventLoop &loop, Partition &partition, Error &error)
 {
 	int port = config_get_positive(CONF_PORT, DEFAULT_PORT);
 	const struct config_param *param =
 		config_get_next_param(CONF_BIND_TO_ADDRESS, nullptr);
 	bool success;
 
-	listen_socket = new ClientListener(loop);
+	listen_socket = new ClientListener(loop, partition);
 
 	if (listen_systemd_activation(error))
 		return true;

src/Listen.hxx
@@ -22,11 +22,12 @@
 
 class EventLoop;
 class Error;
+struct Partition;
 
 extern int listen_port;
 
 bool
-listen_global_init(EventLoop &loop, Error &error);
+listen_global_init(EventLoop &loop, Partition &partition, Error &error);
 
 void listen_global_finish(void);
 

commit 04ba433ca631f0c3f51b067a6b55dc1046bd287e

Main: initialize Partition before Listener
Fixes nullptr dereference (regression by commit df5f9f4a).

src/Main.cxx
@@ -432,6 +432,8 @@ int mpd_main(int argc, char *argv[])
 	const unsigned max_clients = config_get_positive(CONF_MAX_CONN, 10);
 	instance->client_list = new ClientList(max_clients);
 
+	initialize_decoder_and_player();
+
 	if (!listen_global_init(*instance->event_loop, *instance->partition,
 				error)) {
 		LogError(error);
@@ -475,7 +477,6 @@ int mpd_main(int argc, char *argv[])
 	glue_sticker_init();
 
 	command_init();
-	initialize_decoder_and_player();
 	initAudioConfig();
 	instance->partition->outputs.Configure(*instance->event_loop,
 					       instance->partition->pc);