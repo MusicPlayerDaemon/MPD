MusicPlayerDaemon/MPD

commit f5a923b9d16e4c63942a033d1bdb2ab150aae342

OutputAll: convert to class, move instance to class Partition
Another big chunk of code for multi-player support.

src/output/MultipleOutputs.cxx
@@ -1,0 +1,477 @@
+/*
+ * Copyright (C) 2003-2014 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "config.h"
+#include "MultipleOutputs.hxx"
+#include "PlayerControl.hxx"
+#include "OutputInternal.hxx"
+#include "OutputControl.hxx"
+#include "OutputError.hxx"
+#include "MusicBuffer.hxx"
+#include "MusicPipe.hxx"
+#include "MusicChunk.hxx"
+#include "system/FatalError.hxx"
+#include "util/Error.hxx"
+#include "config/ConfigData.hxx"
+#include "config/ConfigGlobal.hxx"
+#include "config/ConfigOption.hxx"
+#include "notify.hxx"
+
+#include <assert.h>
+#include <string.h>
+
+MultipleOutputs::MultipleOutputs()
+	:buffer(nullptr), pipe(nullptr),
+	 elapsed_time(-1)
+{
+}
+
+MultipleOutputs::~MultipleOutputs()
+{
+	for (auto i : outputs) {
+		audio_output_disable(i);
+		audio_output_finish(i);
+	}
+}
+
+static audio_output *
+LoadOutput(PlayerControl &pc, const config_param &param)
+{
+	Error error;
+	audio_output *output = audio_output_new(param, pc, error);
+	if (output == nullptr) {
+		if (param.line > 0)
+			FormatFatalError("line %i: %s",
+					 param.line,
+					 error.GetMessage());
+		else
+			FatalError(error);
+	}
+
+	return output;
+}
+
+void
+MultipleOutputs::Configure(PlayerControl &pc)
+{
+	const config_param *param = nullptr;
+	while ((param = config_get_next_param(CONF_AUDIO_OUTPUT,
+					      param)) != nullptr) {
+		auto output = LoadOutput(pc, *param);
+		if (FindByName(output->name) != nullptr)
+			FormatFatalError("output devices with identical "
+					 "names: %s", output->name);
+
+		outputs.push_back(output);
+	}
+
+	if (outputs.empty()) {
+		/* auto-detect device */
+		const config_param empty;
+		auto output = LoadOutput(pc, empty);
+		outputs.push_back(output);
+	}
+}
+
+audio_output *
+MultipleOutputs::FindByName(const char *name) const
+{
+	for (auto i : outputs)
+		if (strcmp(i->name, name) == 0)
+			return i;
+
+	return nullptr;
+}
+
+void
+MultipleOutputs::EnableDisable()
+{
+	for (auto ao : outputs) {
+		bool enabled;
+
+		ao->mutex.lock();
+		enabled = ao->really_enabled;
+		ao->mutex.unlock();
+
+		if (ao->enabled != enabled) {
+			if (ao->enabled)
+				audio_output_enable(ao);
+			else
+				audio_output_disable(ao);
+		}
+	}
+}
+
+bool
+MultipleOutputs::AllFinished() const
+{
+	for (auto ao : outputs) {
+		const ScopeLock protect(ao->mutex);
+		if (audio_output_is_open(ao) &&
+		    !audio_output_command_is_finished(ao))
+			return false;
+	}
+
+	return true;
+}
+
+void
+MultipleOutputs::WaitAll()
+{
+	while (!AllFinished())
+		audio_output_client_notify.Wait();
+}
+
+void
+MultipleOutputs::AllowPlay()
+{
+	for (auto ao : outputs)
+		audio_output_allow_play(ao);
+}
+
+static void
+audio_output_reset_reopen(struct audio_output *ao)
+{
+	const ScopeLock protect(ao->mutex);
+
+	ao->fail_timer.Reset();
+}
+
+void
+MultipleOutputs::ResetReopen()
+{
+	for (auto ao : outputs)
+		audio_output_reset_reopen(ao);
+}
+
+bool
+MultipleOutputs::Update()
+{
+	bool ret = false;
+
+	if (!input_audio_format.IsDefined())
+		return false;
+
+	for (auto ao : outputs)
+		ret = audio_output_update(ao, input_audio_format, *pipe)
+			|| ret;
+
+	return ret;
+}
+
+void
+MultipleOutputs::SetReplayGainMode(ReplayGainMode mode)
+{
+	for (auto ao : outputs)
+		audio_output_set_replay_gain_mode(ao, mode);
+}
+
+bool
+MultipleOutputs::Play(music_chunk *chunk, Error &error)
+{
+	assert(buffer != nullptr);
+	assert(pipe != nullptr);
+	assert(chunk != nullptr);
+	assert(chunk->CheckFormat(input_audio_format));
+
+	if (!Update()) {
+		/* TODO: obtain real error */
+		error.Set(output_domain, "Failed to open audio output");
+		return false;
+	}
+
+	pipe->Push(chunk);
+
+	for (auto ao : outputs)
+		audio_output_play(ao);
+
+	return true;
+}
+
+bool
+MultipleOutputs::Open(const AudioFormat audio_format,
+		      MusicBuffer &_buffer,
+		      Error &error)
+{
+	bool ret = false, enabled = false;
+
+	assert(buffer == nullptr || buffer == &_buffer);
+	assert((pipe == nullptr) == (buffer == nullptr));
+
+	buffer = &_buffer;
+
+	/* the audio format must be the same as existing chunks in the
+	   pipe */
+	assert(pipe == nullptr || pipe->CheckFormat(audio_format));
+
+	if (pipe == nullptr)
+		pipe = new MusicPipe();
+	else
+		/* if the pipe hasn't been cleared, the the audio
+		   format must not have changed */
+		assert(pipe->IsEmpty() || audio_format == input_audio_format);
+
+	input_audio_format = audio_format;
+
+	ResetReopen();
+	EnableDisable();
+	Update();
+
+	for (auto ao : outputs) {
+		if (ao->enabled)
+			enabled = true;
+
+		if (ao->open)
+			ret = true;
+	}
+
+	if (!enabled)
+		error.Set(output_domain, "All audio outputs are disabled");
+	else if (!ret)
+		/* TODO: obtain real error */
+		error.Set(output_domain, "Failed to open audio output");
+
+	if (!ret)
+		/* close all devices if there was an error */
+		Close();
+
+	return ret;
+}
+
+/**
+ * Has the specified audio output already consumed this chunk?
+ */
+gcc_pure
+static bool
+chunk_is_consumed_in(const struct audio_output *ao,
+		     gcc_unused const MusicPipe *pipe,
+		     const struct music_chunk *chunk)
+{
+	if (!ao->open)
+		return true;
+
+	if (ao->chunk == nullptr)
+		return false;
+
+	assert(chunk == ao->chunk || pipe->Contains(ao->chunk));
+
+	if (chunk != ao->chunk) {
+		assert(chunk->next != nullptr);
+		return true;
+	}
+
+	return ao->chunk_finished && chunk->next == nullptr;
+}
+
+bool
+MultipleOutputs::IsChunkConsumed(const music_chunk *chunk) const
+{
+	for (auto ao : outputs) {
+		const ScopeLock protect(ao->mutex);
+		if (!chunk_is_consumed_in(ao, pipe, chunk))
+			return false;
+	}
+
+	return true;
+}
+
+inline void
+MultipleOutputs::ClearTailChunk(gcc_unused const struct music_chunk *chunk,
+				bool *locked)
+{
+	assert(chunk->next == nullptr);
+	assert(pipe->Contains(chunk));
+
+	for (unsigned i = 0, n = outputs.size(); i != n; ++i) {
+		audio_output *ao = outputs[i];
+
+		/* this mutex will be unlocked by the caller when it's
+		   ready */
+		ao->mutex.lock();
+		locked[i] = ao->open;
+
+		if (!locked[i]) {
+			ao->mutex.unlock();
+			continue;
+		}
+
+		assert(ao->chunk == chunk);
+		assert(ao->chunk_finished);
+		ao->chunk = nullptr;
+	}
+}
+
+unsigned
+MultipleOutputs::Check()
+{
+	const struct music_chunk *chunk;
+	bool is_tail;
+	struct music_chunk *shifted;
+	bool locked[outputs.size()];
+
+	assert(buffer != nullptr);
+	assert(pipe != nullptr);
+
+	while ((chunk = pipe->Peek()) != nullptr) {
+		assert(!pipe->IsEmpty());
+
+		if (!IsChunkConsumed(chunk))
+			/* at least one output is not finished playing
+			   this chunk */
+			return pipe->GetSize();
+
+		if (chunk->length > 0 && chunk->times >= 0.0)
+			/* only update elapsed_time if the chunk
+			   provides a defined value */
+			elapsed_time = chunk->times;
+
+		is_tail = chunk->next == nullptr;
+		if (is_tail)
+			/* this is the tail of the pipe - clear the
+			   chunk reference in all outputs */
+			ClearTailChunk(chunk, locked);
+
+		/* remove the chunk from the pipe */
+		shifted = pipe->Shift();
+		assert(shifted == chunk);
+
+		if (is_tail)
+			/* unlock all audio outputs which were locked
+			   by clear_tail_chunk() */
+			for (unsigned i = 0, n = outputs.size(); i != n; ++i)
+				if (locked[i])
+					outputs[i]->mutex.unlock();
+
+		/* return the chunk to the buffer */
+		buffer->Return(shifted);
+	}
+
+	return 0;
+}
+
+bool
+MultipleOutputs::Wait(PlayerControl &pc, unsigned threshold)
+{
+	pc.Lock();
+
+	if (Check() < threshold) {
+		pc.Unlock();
+		return true;
+	}
+
+	pc.Wait();
+	pc.Unlock();
+
+	return Check() < threshold;
+}
+
+void
+MultipleOutputs::Pause()
+{
+	Update();
+
+	for (auto ao : outputs)
+		audio_output_pause(ao);
+
+	WaitAll();
+}
+
+void
+MultipleOutputs::Drain()
+{
+	for (auto ao : outputs)
+		audio_output_drain_async(ao);
+
+	WaitAll();
+}
+
+void
+MultipleOutputs::Cancel()
+{
+	/* send the cancel() command to all audio outputs */
+
+	for (auto ao : outputs)
+		audio_output_cancel(ao);
+
+	WaitAll();
+
+	/* clear the music pipe and return all chunks to the buffer */
+
+	if (pipe != nullptr)
+		pipe->Clear(*buffer);
+
+	/* the audio outputs are now waiting for a signal, to
+	   synchronize the cleared music pipe */
+
+	AllowPlay();
+
+	/* invalidate elapsed_time */
+
+	elapsed_time = -1.0;
+}
+
+void
+MultipleOutputs::Close()
+{
+	for (auto ao : outputs)
+		audio_output_close(ao);
+
+	if (pipe != nullptr) {
+		assert(buffer != nullptr);
+
+		pipe->Clear(*buffer);
+		delete pipe;
+		pipe = nullptr;
+	}
+
+	buffer = nullptr;
+
+	input_audio_format.Clear();
+
+	elapsed_time = -1.0;
+}
+
+void
+MultipleOutputs::Release()
+{
+	for (auto ao : outputs)
+		audio_output_release(ao);
+
+	if (pipe != nullptr) {
+		assert(buffer != nullptr);
+
+		pipe->Clear(*buffer);
+		delete pipe;
+		pipe = nullptr;
+	}
+
+	buffer = nullptr;
+
+	input_audio_format.Clear();
+
+	elapsed_time = -1.0;
+}
+
+void
+MultipleOutputs::SongBorder()
+{
+	/* clear the elapsed_time pointer at the beginning of a new
+	   song */
+	elapsed_time = 0.0;
+}

Makefile.am
@@ -961,7 +961,7 @@ OUTPUT_API_SRC = \
 	src/output/OutputAPI.hxx \
 	src/output/OutputInternal.hxx \
 	src/output/OutputList.cxx src/output/OutputList.hxx \
-	src/output/OutputAll.cxx src/output/OutputAll.hxx \
+	src/output/MultipleOutputs.cxx src/output/MultipleOutputs.hxx \
 	src/output/OutputThread.cxx src/output/OutputThread.hxx \
 	src/output/OutputError.cxx src/output/OutputError.hxx \
 	src/output/OutputControl.cxx src/output/OutputControl.hxx \
@@ -985,7 +985,7 @@ MIXER_API_SRC = \
 	src/mixer/MixerList.hxx \
 	src/mixer/MixerControl.cxx src/mixer/MixerControl.hxx \
 	src/mixer/MixerType.cxx src/mixer/MixerType.hxx \
-	src/mixer/MixerAll.cxx src/mixer/MixerAll.hxx \
+	src/mixer/MixerAll.cxx \
 	src/mixer/MixerInternal.hxx
 
 libmixer_plugins_a_SOURCES = \

src/Main.cxx
@@ -37,7 +37,6 @@
 #include "command/AllCommands.hxx"
 #include "Partition.hxx"
 #include "mixer/Volume.hxx"
-#include "output/OutputAll.hxx"
 #include "tag/TagConfig.hxx"
 #include "ReplayGainConfig.hxx"
 #include "Idle.hxx"
@@ -458,7 +457,7 @@ int mpd_main(int argc, char *argv[])
 	initialize_decoder_and_player();
 	volume_init();
 	initAudioConfig();
-	audio_output_all_init(instance->partition->pc);
+	instance->partition->outputs.Configure(instance->partition->pc);
 	client_manager_init();
 	replay_gain_global_init();
 
@@ -500,7 +499,7 @@ int mpd_main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	audio_output_all_set_replay_gain_mode(replay_gain_get_real_mode(instance->partition->playlist.queue.random));
+	instance->partition->outputs.SetReplayGainMode(replay_gain_get_real_mode(instance->partition->playlist.queue.random));
 
 	if (config_get_bool(CONF_AUTO_UPDATE, false)) {
 #ifdef ENABLE_INOTIFY
@@ -567,7 +566,6 @@ int mpd_main(int argc, char *argv[])
 
 	playlist_list_global_finish();
 	input_stream_global_finish();
-	audio_output_all_finish();
 	mapper_finish();
 	delete instance->partition;
 	command_finish();

src/Partition.cxx
@@ -20,6 +20,7 @@
 #include "config.h"
 #include "Partition.hxx"
 #include "DetachedSong.hxx"
+#include "output/MultipleOutputs.hxx"
 
 void
 Partition::DatabaseModified()

src/Partition.hxx
@@ -21,9 +21,11 @@
 #define MPD_PARTITION_HXX
 
 #include "Playlist.hxx"
+#include "output/MultipleOutputs.hxx"
 #include "PlayerControl.hxx"
 
 struct Instance;
+class MultipleOutputs;
 
 /**
  * A partition of the Music Player Daemon.  It is a separate unit with
@@ -34,6 +36,8 @@ struct Partition {
 
 	struct playlist playlist;
 
+	MultipleOutputs outputs;
+
 	PlayerControl pc;
 
 	Partition(Instance &_instance,
@@ -41,8 +45,7 @@ struct Partition {
 		  unsigned buffer_chunks,
 		  unsigned buffered_before_play)
 		:instance(_instance), playlist(max_length),
-		 pc(buffer_chunks, buffered_before_play) {
-	}
+		 pc(outputs, buffer_chunks, buffered_before_play) {}
 
 	void ClearQueue() {
 		playlist.Clear(pc);

src/PlayerControl.cxx
@@ -26,9 +26,11 @@
 
 #include <assert.h>
 
-PlayerControl::PlayerControl(unsigned _buffer_chunks,
+PlayerControl::PlayerControl(MultipleOutputs &_outputs,
+			     unsigned _buffer_chunks,
 			     unsigned _buffered_before_play)
-	:buffer_chunks(_buffer_chunks),
+	:outputs(_outputs),
+	 buffer_chunks(_buffer_chunks),
 	 buffered_before_play(_buffered_before_play),
 	 command(PlayerCommand::NONE),
 	 state(PlayerState::STOP),

src/PlayerControl.hxx
@@ -29,6 +29,7 @@
 
 #include <stdint.h>
 
+class MultipleOutputs;
 class DetachedSong;
 
 enum class PlayerState : uint8_t {
@@ -91,6 +92,8 @@ struct player_status {
 };
 
 struct PlayerControl {
+	MultipleOutputs &outputs;
+
 	unsigned buffer_chunks;
 
 	unsigned int buffered_before_play;
@@ -170,7 +173,8 @@ struct PlayerControl {
 	 */
 	bool border_pause;
 
-	PlayerControl(unsigned buffer_chunks,
+	PlayerControl(MultipleOutputs &_outputs,
+		      unsigned buffer_chunks,
 		      unsigned buffered_before_play);
 	~PlayerControl();
 

src/PlayerThread.cxx
@@ -28,7 +28,7 @@
 #include "system/FatalError.hxx"
 #include "CrossFade.hxx"
 #include "PlayerControl.hxx"
-#include "output/OutputAll.hxx"
+#include "output/MultipleOutputs.hxx"
 #include "tag/Tag.hxx"
 #include "Idle.hxx"
 #include "GlobalEvents.hxx"
@@ -125,7 +125,7 @@ class Player {
 	/**
 	 * The time stamp of the chunk most recently sent to the
 	 * output thread.  This attribute is only used if
-	 * audio_output_all_get_elapsed_time() didn't return a usable
+	 * MultipleOutputs::GetElapsedTime() didn't return a usable
 	 * value; the output thread can estimate the elapsed time more
 	 * precisely.
 	 */
@@ -228,8 +228,8 @@ private:
 	bool WaitForDecoder();
 
 	/**
-	 * Wrapper for audio_output_all_open().  Upon failure, it pauses the
-	 * player.
+	 * Wrapper for MultipleOutputs::Open().  Upon failure, it
+	 * pauses the player.
 	 *
 	 * @return true on success
 	 */
@@ -393,7 +393,7 @@ Player::OpenOutput()
 	       pc.state == PlayerState::PAUSE);
 
 	Error error;
-	if (audio_output_all_open(play_audio_format, buffer, error)) {
+	if (pc.outputs.Open(play_audio_format, buffer, error)) {
 		output_open = true;
 		paused = false;
 
@@ -444,7 +444,7 @@ Player::CheckDecoderStartup()
 		pc.Unlock();
 
 		if (output_open &&
-		    !audio_output_all_wait(pc, 1))
+		    !pc.outputs.Wait(pc, 1))
 			/* the output devices havn't finished playing
 			   all chunks yet - wait for that */
 			return true;
@@ -504,7 +504,7 @@ Player::SendSilence()
 	memset(chunk->data, 0, chunk->length);
 
 	Error error;
-	if (!audio_output_all_play(chunk, error)) {
+	if (!pc.outputs.Play(chunk, error)) {
 		LogError(error);
 		buffer.Return(chunk);
 		return false;
@@ -582,7 +582,7 @@ Player::SeekDecoder()
 	/* re-fill the buffer after seeking */
 	buffering = true;
 
-	audio_output_all_cancel();
+	pc.outputs.Cancel();
 
 	return true;
 }
@@ -599,7 +599,7 @@ Player::ProcessCommand()
 
 	case PlayerCommand::UPDATE_AUDIO:
 		pc.Unlock();
-		audio_output_all_enable_disable();
+		pc.outputs.EnableDisable();
 		pc.Lock();
 		pc.CommandFinished();
 		break;
@@ -618,7 +618,7 @@ Player::ProcessCommand()
 
 		paused = !paused;
 		if (paused) {
-			audio_output_all_pause();
+			pc.outputs.Pause();
 			pc.Lock();
 
 			pc.state = PlayerState::PAUSE;
@@ -669,11 +669,11 @@ Player::ProcessCommand()
 	case PlayerCommand::REFRESH:
 		if (output_open && !paused) {
 			pc.Unlock();
-			audio_output_all_check();
+			pc.outputs.Check();
 			pc.Lock();
 		}
 
-		pc.elapsed_time = audio_output_all_get_elapsed_time();
+		pc.elapsed_time = pc.outputs.GetElapsedTime();
 		if (pc.elapsed_time < 0.0)
 			pc.elapsed_time = elapsed_time;
 
@@ -733,7 +733,7 @@ play_chunk(PlayerControl &pc,
 
 	/* send the chunk to the audio outputs */
 
-	if (!audio_output_all_play(chunk, error))
+	if (!pc.outputs.Play(chunk, error))
 		return false;
 
 	pc.total_play_time += (double)chunk->length /
@@ -744,7 +744,7 @@ play_chunk(PlayerControl &pc,
 inline bool
 Player::PlayNextChunk()
 {
-	if (!audio_output_all_wait(pc, 64))
+	if (!pc.outputs.Wait(pc, 64))
 		/* the output pipe is still large enough, don't send
 		   another chunk */
 		return true;
@@ -883,7 +883,7 @@ Player::SongBorder()
 
 	ReplacePipe(dc.pipe);
 
-	audio_output_all_song_border();
+	pc.outputs.SongBorder();
 
 	if (!WaitForDecoder())
 		return false;
@@ -933,7 +933,7 @@ Player::Run()
 		    pc.command == PlayerCommand::EXIT ||
 		    pc.command == PlayerCommand::CLOSE_AUDIO) {
 			pc.Unlock();
-			audio_output_all_cancel();
+			pc.outputs.Cancel();
 			break;
 		}
 
@@ -949,7 +949,7 @@ Player::Run()
 				/* not enough decoded buffer space yet */
 
 				if (!paused && output_open &&
-				    audio_output_all_check() < 4 &&
+				    pc.outputs.Check() < 4 &&
 				    !SendSilence())
 					break;
 
@@ -1029,7 +1029,7 @@ Player::Run()
 			   to the audio output */
 
 			PlayNextChunk();
-		} else if (audio_output_all_check() > 0) {
+		} else if (pc.outputs.Check() > 0) {
 			/* not enough data from decoder, but the
 			   output thread is still busy, so it's
 			   okay */
@@ -1054,7 +1054,7 @@ Player::Run()
 			if (pipe->IsEmpty()) {
 				/* wait for the hardware to finish
 				   playback */
-				audio_output_all_drain();
+				pc.outputs.Drain();
 				break;
 			}
 		} else if (output_open) {
@@ -1130,7 +1130,7 @@ player_task(void *arg)
 
 		case PlayerCommand::STOP:
 			pc.Unlock();
-			audio_output_all_cancel();
+			pc.outputs.Cancel();
 			pc.Lock();
 
 			/* fall through */
@@ -1145,7 +1145,7 @@ player_task(void *arg)
 		case PlayerCommand::CLOSE_AUDIO:
 			pc.Unlock();
 
-			audio_output_all_release();
+			pc.outputs.Release();
 
 			pc.Lock();
 			pc.CommandFinished();
@@ -1156,7 +1156,7 @@ player_task(void *arg)
 
 		case PlayerCommand::UPDATE_AUDIO:
 			pc.Unlock();
-			audio_output_all_enable_disable();
+			pc.outputs.EnableDisable();
 			pc.Lock();
 			pc.CommandFinished();
 			break;
@@ -1166,7 +1166,7 @@ player_task(void *arg)
 
 			dc.Quit();
 
-			audio_output_all_close();
+			pc.outputs.Close();
 
 			player_command_finished(pc);
 			return;

commit d0cd98a63aa40ba16946148590d3cf0648a1dd19

MultipleOutputs: ensure input_audio_format is zero-initialised
-fixes SIGABRT when mpd is started from a previously paused state
-regression introduced by commit: f5a923b9d16e4c63942a033d1bdb2ab150aae342:
'OutputAll: convert to class, move instance to class Partition'

-input_audio_format was previously declared using the static modifier
ensuring it was zero-initialised by default
-the current default-initialised input_audio_format contains garbage at
runtime which allows the AudioFormat.IsDefined() 'fail fast' test in
MultipleOutputs::Update to pass and the SIGABRT follows in
AudioOutput::Open when passed the invalid input_audio_format struct
-switching AudioFormat.IsDefined() for AudioFormat.IsValid() is an
alternative workaround

src/output/MultipleOutputs.cxx
@@ -37,6 +37,7 @@
 
 MultipleOutputs::MultipleOutputs(MixerListener &_mixer_listener)
 	:mixer_listener(_mixer_listener),
+	 input_audio_format(AudioFormat::Undefined()),
 	 buffer(nullptr), pipe(nullptr),
 	 elapsed_time(-1)
 {