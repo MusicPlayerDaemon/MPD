MusicPlayerDaemon/MPD

commit 0b4fa41aff35ac8d190e5daaeed12f12614a0e9c

InputStream: "protect" attributes

src/input/plugins/RewindInputPlugin.cxx
@@ -93,7 +93,7 @@ private:
 	 * buffer contain more data for the next read operation?
 	 */
 	bool ReadingFromBuffer() const {
-		return tail > 0 && offset < input->offset;
+		return tail > 0 && offset < input->GetOffset();
 	}
 
 	/**
@@ -116,7 +116,7 @@ private:
 			SetReady();
 		}
 
-		offset = src->offset;
+		offset = src->GetOffset();
 	}
 };
 
@@ -127,7 +127,7 @@ RewindInputStream::Read(void *ptr, size_t read_size, Error &error)
 		/* buffered read */
 
 		assert(head == (size_t)offset);
-		assert(tail == (size_t)input->offset);
+		assert(tail == (size_t)input->GetOffset());
 
 		if (read_size > tail - head)
 			read_size = tail - head;
@@ -142,7 +142,7 @@ RewindInputStream::Read(void *ptr, size_t read_size, Error &error)
 
 		size_t nbytes = input->Read(ptr, read_size, error);
 
-		if (input->offset > (InputPlugin::offset_type)sizeof(buffer))
+		if (input->GetOffset() > (offset_type)sizeof(buffer))
 			/* disable buffering */
 			tail = 0;
 		else if (tail == (size_t)offset) {
@@ -151,7 +151,7 @@ RewindInputStream::Read(void *ptr, size_t read_size, Error &error)
 			memcpy(buffer + tail, ptr, nbytes);
 			tail += nbytes;
 
-			assert(tail == (size_t)input->offset);
+			assert(tail == (size_t)input->GetOffset());
 		}
 
 		CopyAttributes();
@@ -172,7 +172,7 @@ RewindInputStream::Seek(InputPlugin::offset_type new_offset, int whence,
 
 		assert(!ReadingFromBuffer() ||
 		       head == (size_t)offset);
-		assert(tail == (size_t)input->offset);
+		assert(tail == (size_t)input->GetOffset());
 
 		head = (size_t)new_offset;
 		offset = new_offset;
@@ -194,7 +194,7 @@ InputStream *
 input_rewind_open(InputStream *is)
 {
 	assert(is != nullptr);
-	assert(is->offset == 0);
+	assert(is->GetOffset() == 0);
 
 	if (is->IsReady() && is->IsSeekable())
 		/* seekable resources don't need this plugin */

src/decoder/plugins/FfmpegDecoderPlugin.cxx
@@ -121,12 +121,12 @@ mpd_ffmpeg_stream_seek(void *opaque, int64_t pos, int whence)
 	AvioStream *stream = (AvioStream *)opaque;
 
 	if (whence == AVSEEK_SIZE)
-		return stream->input.size;
+		return stream->input.GetSize();
 
 	if (!stream->input.LockSeek(pos, whence, IgnoreError()))
 		return -1;
 
-	return stream->input.offset;
+	return stream->input.GetOffset();
 }
 
 bool
@@ -135,7 +135,7 @@ AvioStream::Open()
 	io = avio_alloc_context(buffer, sizeof(buffer),
 				false, this,
 				mpd_ffmpeg_stream_read, nullptr,
-				input.seekable
+				input.IsSeekable()
 				? mpd_ffmpeg_stream_seek : nullptr);
 	return io != nullptr;
 }
@@ -481,7 +481,7 @@ ffmpeg_decode(Decoder &decoder, InputStream &input)
 		: 0;
 
 	decoder_initialized(decoder, audio_format,
-			    input.seekable, total_time);
+			    input.IsSeekable(), total_time);
 
 #if LIBAVUTIL_VERSION_MAJOR >= 53
 	AVFrame *frame = av_frame_alloc();

src/decoder/plugins/FlacCommon.cxx
@@ -137,7 +137,7 @@ flac_got_first_frame(struct flac_data *data, const FLAC__FrameHeader *header)
 	data->frame_size = data->audio_format.GetFrameSize();
 
 	decoder_initialized(data->decoder, data->audio_format,
-			    data->input_stream.seekable,
+			    data->input_stream.IsSeekable(),
 			    (float)data->total_frames /
 			    (float)data->audio_format.sample_rate);
 

src/decoder/plugins/FlacDecoderPlugin.cxx
@@ -145,13 +145,13 @@ flac_decoder_initialize(struct flac_data *data, FLAC__StreamDecoder *sd,
 	if (data->initialized) {
 		/* done */
 		decoder_initialized(data->decoder, data->audio_format,
-				    data->input_stream.seekable,
+				    data->input_stream.IsSeekable(),
 				    (float)data->total_frames /
 				    (float)data->audio_format.sample_rate);
 		return true;
 	}
 
-	if (data->input_stream.seekable)
+	if (data->input_stream.IsSeekable())
 		/* allow the workaround below only for nonseekable
 		   streams*/
 		return false;

src/decoder/plugins/FlacIOHandle.cxx
@@ -75,7 +75,7 @@ FlacIOTell(FLAC__IOHandle handle)
 {
 	InputStream *is = (InputStream *)handle;
 
-	return is->offset;
+	return is->GetOffset();
 }
 
 static int

src/decoder/plugins/FlacIOHandle.hxx
@@ -37,7 +37,7 @@ ToFlacIOHandle(InputStream &is)
 static inline const FLAC__IOCallbacks &
 GetFlacIOCallbacks(const InputStream &is)
 {
-	return is.seekable
+	return is.IsSeekable()
 		? flac_io_callbacks_seekable
 		: flac_io_callbacks;
 }

src/decoder/plugins/FlacInput.cxx
@@ -47,7 +47,7 @@ FlacInput::Read(FLAC__byte buffer[], size_t *bytes)
 FLAC__StreamDecoderSeekStatus
 FlacInput::Seek(FLAC__uint64 absolute_byte_offset)
 {
-	if (!input_stream.seekable)
+	if (!input_stream.IsSeekable())
 		return FLAC__STREAM_DECODER_SEEK_STATUS_UNSUPPORTED;
 
 	::Error error;
@@ -62,20 +62,20 @@ FlacInput::Seek(FLAC__uint64 absolute_byte_offset)
 FLAC__StreamDecoderTellStatus
 FlacInput::Tell(FLAC__uint64 *absolute_byte_offset)
 {
-	if (!input_stream.seekable)
+	if (!input_stream.IsSeekable())
 		return FLAC__STREAM_DECODER_TELL_STATUS_UNSUPPORTED;
 
-	*absolute_byte_offset = (FLAC__uint64)input_stream.offset;
+	*absolute_byte_offset = (FLAC__uint64)input_stream.GetOffset();
 	return FLAC__STREAM_DECODER_TELL_STATUS_OK;
 }
 
 FLAC__StreamDecoderLengthStatus
 FlacInput::Length(FLAC__uint64 *stream_length)
 {
-	if (input_stream.size < 0)
+	if (!input_stream.KnownSize())
 		return FLAC__STREAM_DECODER_LENGTH_STATUS_UNSUPPORTED;
 
-	*stream_length = (FLAC__uint64)input_stream.size;
+	*stream_length = (FLAC__uint64)input_stream.GetSize();
 	return FLAC__STREAM_DECODER_LENGTH_STATUS_OK;
 }
 

src/decoder/plugins/OggFind.cxx
@@ -57,7 +57,7 @@ bool
 OggSeekFindEOS(OggSyncState &oy, ogg_stream_state &os, ogg_packet &packet,
 	       InputStream &is)
 {
-	if (is.size > 0 && is.size - is.offset < 65536)
+	if (is.KnownSize() && is.GetRest() < 65536)
 		return OggFindEOS(oy, os, packet);
 
 	if (!is.CheapSeeking())

commit dc085e0bd202932c08505196eedd768c87b34d33

input/rewind: work around assertion failure
Caused by commit 0b4fa41a

src/input/plugins/RewindInputPlugin.cxx
@@ -194,7 +194,7 @@ InputStream *
 input_rewind_open(InputStream *is)
 {
 	assert(is != nullptr);
-	assert(is->GetOffset() == 0);
+	assert(!is->IsReady() || is->GetOffset() == 0);
 
 	if (is->IsReady() && is->IsSeekable())
 		/* seekable resources don't need this plugin */