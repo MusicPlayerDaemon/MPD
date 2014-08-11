2ccfb71d12d0ae1a8b4177c95aca45100982e685
Max Kellermann <max@duempel.org>
2014-08-11 18:42:05 +0200

    util/CharUtil: add ToLowerASCII()

--- src/util/CharUtil.hxx
+++ src/util/CharUtil.hxx
@@ -126,4 +126,17 @@ ToUpperASCII(char ch)
 		: ch;
 }
 
+/**
+ * Convert the specified ASCII character (0x00..0x7f) to lower case.
+ * Unlike toupper(), it ignores the system locale.
+ */
+constexpr
+static inline char
+ToLowerASCII(char ch)
+{
+	return ch >= 'A' && ch <= 'Z'
+		? (ch + ('a' - 'A'))
+		: ch;
+}
+
 #endif
