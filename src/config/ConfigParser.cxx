bb39fffab9734c9d1ee88ac465bbb47bda4f64bb
Max Kellermann <max@duempel.org>
2014-12-09 21:57:07 +0100

    ConfigParser: make the static arrays "const"

--- src/config/ConfigParser.cxx
+++ src/config/ConfigParser.cxx
@@ -23,8 +23,8 @@
 bool
 get_bool(const char *value, bool *value_r)
 {
-	static const char *t[] = { "yes", "true", "1", nullptr };
-	static const char *f[] = { "no", "false", "0", nullptr };
+	static const char *const t[] = { "yes", "true", "1", nullptr };
+	static const char *const f[] = { "no", "false", "0", nullptr };
 
 	if (string_array_contains(t, value)) {
 		*value_r = true;
