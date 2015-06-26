4d33b089dae0d97f0144239fcf1d254f51a5fd8f
Max Kellermann <max@duempel.org>
2015-06-26 16:32:52 +0200

    CommandLine: update copyright year

--- src/CommandLine.cxx
+++ src/CommandLine.cxx
@@ -106,7 +106,7 @@ static void version(void)
 	       "\n"
 	       "\n"
 	       "Copyright (C) 2003-2007 Warren Dukes <warren.dukes@gmail.com>\n"
-	       "Copyright (C) 2008-2014 Max Kellermann <max@duempel.org>\n"
+	       "Copyright (C) 2008-2015 Max Kellermann <max@duempel.org>\n"
 	       "This is free software; see the source for copying conditions.  There is NO\n"
 	       "warranty; not even MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
 
