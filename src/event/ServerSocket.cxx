MusicPlayerDaemon/MPD

commit cbb595ba83b91df1bf54b2b6cfa451d37dbd0e0c

event/ServerSocket: use AllocatedSocketAddress to build local socket address

src/event/ServerSocket.cxx
@@ -429,21 +429,12 @@ bool
 ServerSocket::AddPath(AllocatedPath &&path, Error &error)
 {
 #ifdef HAVE_UN
-	struct sockaddr_un s_un;
+	(void)error;
 
-	const size_t path_length = path.length();
-	if (path_length >= sizeof(s_un.sun_path)) {
-		error.Set(server_socket_domain,
-			  "UNIX socket path is too long");
-		return false;
-	}
+	AllocatedSocketAddress address;
+	address.SetLocal(path.c_str());
 
-	RemoveFile(path);
-
-	s_un.sun_family = AF_UNIX;
-	memcpy(s_un.sun_path, path.c_str(), path_length + 1);
-
-	OneServerSocket &s = AddAddress({(const sockaddr *)&s_un, sizeof(s_un)});
+	OneServerSocket &s = AddAddress(std::move(address));
 	s.SetPath(std::move(path));
 
 	return true;

commit de332a16d17a3e23438ba4dd05f27ad6bdd4a04d

event/ServerSocket: fix AF_UNIX address in use error
bind fails with an "address already in use" error if the socket path
already exists.

This was broken by cbb595ba.

src/event/ServerSocket.cxx
@@ -431,6 +431,8 @@ ServerSocket::AddPath(AllocatedPath &&path, Error &error)
 #ifdef HAVE_UN
 	(void)error;
 
+	RemoveFile(path);
+
 	AllocatedSocketAddress address;
 	address.SetLocal(path.c_str());
 