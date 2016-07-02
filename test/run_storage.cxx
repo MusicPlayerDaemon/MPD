4b25c5e8d6936110e2a03e1b43d67bf24f89e0d9
Max Kellermann <max@duempel.org>
2016-07-02 14:19:47 +0200

    test/*: add missing includes for fprintf()

--- test/run_storage.cxx
+++ test/run_storage.cxx
@@ -28,6 +28,7 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <time.h>
 
--- test/visit_archive.cxx
+++ test/visit_archive.cxx
@@ -31,6 +31,7 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 class MyArchiveVisitor final : public ArchiveVisitor {
  public:
