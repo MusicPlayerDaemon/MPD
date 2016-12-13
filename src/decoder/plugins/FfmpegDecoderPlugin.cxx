7146f825b279b6882b5372d7a5bec38d69848259
Max Kellermann <max@musicpd.org>
2016-12-13 08:34:05 +0100

    decoder/ffmpeg: fix double free bug
    From the avformat_open_input() API documentation:
    
     "Note that a user-supplied AVFormatContext will be freed on failure."
    
    https://bugs.musicpd.org/view.php?id=4607

--- src/decoder/plugins/FfmpegDecoderPlugin.cxx
+++ src/decoder/plugins/FfmpegDecoderPlugin.cxx
@@ -71,7 +71,6 @@ FfmpegOpenInput(AVIOContext *pb,
 
 	int err = avformat_open_input(&context, filename, fmt, nullptr);
 	if (err < 0) {
-		avformat_free_context(context);
 		SetFfmpegError(error, err, "avformat_open_input() failed");
 		return nullptr;
 	}
