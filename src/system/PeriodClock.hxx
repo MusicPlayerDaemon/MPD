e786207cc22a9455dc4ce8c1cd31147a52d29559
Max Kellermann <max@musicpd.org>
2016-12-27 22:49:13 +0100

    system/PeriodClock: indent with tabs

--- src/system/PeriodClock.hxx
+++ src/system/PeriodClock.hxx
@@ -28,122 +28,122 @@
  */
 class PeriodClock {
 protected:
-  typedef unsigned Stamp;
+	typedef unsigned Stamp;
 
 private:
-  Stamp last;
+	Stamp last;
 
 public:
-  /**
-   * Initializes the object, setting the last time stamp to "0",
-   * i.e. a Check() will always succeed.  If you do not want this
-   * default behaviour, call Update() immediately after creating the
-   * object.
-   */
-  constexpr
-  PeriodClock():last(0) {}
+	/**
+	 * Initializes the object, setting the last time stamp to "0",
+	 * i.e. a Check() will always succeed.  If you do not want this
+	 * default behaviour, call Update() immediately after creating the
+	 * object.
+	 */
+	constexpr
+	PeriodClock():last(0) {}
 
 protected:
-  static Stamp GetNow() {
-    return MonotonicClockMS();
-  }
+	static Stamp GetNow() {
+		return MonotonicClockMS();
+	}
 
-  constexpr int Elapsed(Stamp now) const {
-    return last == 0
-      ? -1
-      : now - last;
-  }
+	constexpr int Elapsed(Stamp now) const {
+		return last == 0
+			? -1
+			: now - last;
+	}
 
-  constexpr bool Check(Stamp now, unsigned duration) const {
-    return now >= last + duration;
-  }
+	constexpr bool Check(Stamp now, unsigned duration) const {
+		return now >= last + duration;
+	}
 
-  void Update(Stamp now) {
-    last = now;
-  }
+	void Update(Stamp now) {
+		last = now;
+	}
 
 public:
-  constexpr bool IsDefined() const {
-    return last != 0;
-  }
+	constexpr bool IsDefined() const {
+		return last != 0;
+	}
 
-  /**
-   * Resets the clock.
-   */
-  void Reset() {
-    last = 0;
-  }
+	/**
+	 * Resets the clock.
+	 */
+	void Reset() {
+		last = 0;
+	}
 
-  /**
-   * Returns the number of milliseconds elapsed since the last
-   * update().  Returns -1 if update() was never called.
-   */
-  int Elapsed() const {
-    return Elapsed(GetNow());
-  }
+	/**
+	 * Returns the number of milliseconds elapsed since the last
+	 * update().  Returns -1 if update() was never called.
+	 */
+	int Elapsed() const {
+		return Elapsed(GetNow());
+	}
 
-  /**
-   * Combines a call to Elapsed() and Update().
-   */
-  int ElapsedUpdate() {
-    const auto now = GetNow();
-    int result = Elapsed(now);
-    Update(now);
-    return result;
-  }
+	/**
+	 * Combines a call to Elapsed() and Update().
+	 */
+	int ElapsedUpdate() {
+		const auto now = GetNow();
+		int result = Elapsed(now);
+		Update(now);
+		return result;
+	}
 
-  /**
-   * Checks whether the specified duration has passed since the last
-   * update.
-   *
-   * @param duration the duration in milliseconds
-   */
-  bool Check(unsigned duration) const {
-    return Check(GetNow(), duration);
-  }
+	/**
+	 * Checks whether the specified duration has passed since the last
+	 * update.
+	 *
+	 * @param duration the duration in milliseconds
+	 */
+	bool Check(unsigned duration) const {
+		return Check(GetNow(), duration);
+	}
 
-  /**
-   * Updates the time stamp, setting it to the current clock.
-   */
-  void Update() {
-    Update(GetNow());
-  }
+	/**
+	 * Updates the time stamp, setting it to the current clock.
+	 */
+	void Update() {
+		Update(GetNow());
+	}
 
-  /**
-   * Updates the time stamp, setting it to the current clock plus the
-   * specified offset.
-   */
-  void UpdateWithOffset(int offset) {
-    Update(GetNow() + offset);
-  }
+	/**
+	 * Updates the time stamp, setting it to the current clock plus the
+	 * specified offset.
+	 */
+	void UpdateWithOffset(int offset) {
+		Update(GetNow() + offset);
+	}
 
-  /**
-   * Checks whether the specified duration has passed since the last
-   * update.  If yes, it updates the time stamp.
-   *
-   * @param duration the duration in milliseconds
-   */
-  bool CheckUpdate(unsigned duration) {
-    Stamp now = GetNow();
-    if (Check(now, duration)) {
-      Update(now);
-      return true;
-    } else
-      return false;
-  }
+	/**
+	 * Checks whether the specified duration has passed since the last
+	 * update.  If yes, it updates the time stamp.
+	 *
+	 * @param duration the duration in milliseconds
+	 */
+	bool CheckUpdate(unsigned duration) {
+		Stamp now = GetNow();
+		if (Check(now, duration)) {
+			Update(now);
+			return true;
+		} else
+			return false;
+	}
 
-  /**
-   * Checks whether the specified duration has passed since the last
-   * update.  After that, it updates the time stamp.
-   *
-   * @param duration the duration in milliseconds
-   */
-  bool CheckAlwaysUpdate(unsigned duration) {
-    Stamp now = GetNow();
-    bool ret = Check(now, duration);
-    Update(now);
-    return ret;
-  }
+	/**
+	 * Checks whether the specified duration has passed since the last
+	 * update.  After that, it updates the time stamp.
+	 *
+	 * @param duration the duration in milliseconds
+	 */
+	bool CheckAlwaysUpdate(unsigned duration) {
+		Stamp now = GetNow();
+		bool ret = Check(now, duration);
+		Update(now);
+		return ret;
+	}
 };
 
 #endif
