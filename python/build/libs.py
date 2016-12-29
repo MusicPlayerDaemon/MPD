a0393e9ddf752f6425756cbaf6fb699b3992c195
Max Kellermann <max@musicpd.org>
2016-12-29 21:49:28 +0100

    python/libs: upgrade Boost to 1.63

--- python/build/libs.py
+++ python/build/libs.py
@@ -103,7 +103,7 @@ curl = AutotoolsProject(
 )
 
 boost = BoostProject(
-    'http://downloads.sourceforge.net/project/boost/boost/1.62.0/boost_1_62_0.tar.bz2',
-    '5fb94629535c19e48703bdb2b2e9490f',
+    'http://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.bz2',
+    'beae2529f759f6b3bf3f4969a19c2e9d6f0c503edcb2de4a61d1428519fcb3b0',
     'include/boost/version.hpp',
 )
