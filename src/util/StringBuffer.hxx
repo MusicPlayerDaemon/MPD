4f01387edfc522908758ca8f3ad0eb96cf841e02
Max Kellermann <max@musicpd.org>
2017-01-17 10:58:07 +0100

    util/StringBuffer: new utility class

---
+++ src/util/StringBuffer.hxx
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2010-2017 Max Kellermann <max.kellermann@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef STRING_BUFFER_HPP
+#define STRING_BUFFER_HPP
+
+#include <array>
+
+/**
+ * A statically allocated string buffer.
+ */
+template<typename T, size_t CAPACITY>
+class BasicStringBuffer {
+public:
+	typedef T value_type;
+	typedef T &reference;
+	typedef T *pointer;
+	typedef const T *const_pointer;
+	typedef const_pointer const_iterator;
+	typedef size_t size_type;
+
+	static constexpr value_type SENTINEL = '\0';
+
+protected:
+	std::array<value_type, CAPACITY> the_data;
+
+public:
+	constexpr size_type capacity() const {
+		return CAPACITY;
+	}
+
+	constexpr bool empty() const {
+		return front() == SENTINEL;
+	}
+
+	void clear() {
+		the_data[0] = SENTINEL;
+	}
+
+	constexpr const_pointer c_str() const {
+		return &the_data.front();
+	}
+
+	pointer data() {
+		return &the_data.front();
+	}
+
+	constexpr value_type front() const {
+		return c_str()[0];
+	}
+
+	/**
+	 * Returns one character.  No bounds checking.
+	 */
+	value_type operator[](size_type i) const {
+		return the_data[i];
+	}
+
+	/**
+	 * Returns one writable character.  No bounds checking.
+	 */
+	reference operator[](size_type i) {
+		return the_data[i];
+	}
+
+	constexpr const_iterator begin() const {
+		return the_data;
+	}
+
+	constexpr const_iterator end() const {
+		return the_data + capacity();
+	}
+
+	constexpr operator const_pointer() const {
+		return c_str();
+	}
+};
+
+template<size_t CAPACITY>
+class StringBuffer : public BasicStringBuffer<char, CAPACITY> {};
+
+#endif
