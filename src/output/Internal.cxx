8f42ba13ef7ff9524ef2aeafe8d8423ade3a2912
Max Kellermann <max@musicpd.org>
2017-08-07 17:40:13 +0200

    output/Internal: simplify "return" statements in IteratePause()

--- src/output/Internal.cxx
+++ src/output/Internal.cxx
@@ -127,16 +127,12 @@ AudioOutput::BeginPause() noexcept
 bool
 AudioOutput::IteratePause() noexcept
 {
-	bool success;
-
 	try {
 		const ScopeUnlock unlock(mutex);
-		success = ao_plugin_pause(*this);
+		return ao_plugin_pause(*this);
 	} catch (const std::runtime_error &e) {
 		FormatError(e, "\"%s\" [%s] failed to pause",
 			    name, plugin.name);
-		success = false;
+		return false;
 	}
-
-	return success;
 }
