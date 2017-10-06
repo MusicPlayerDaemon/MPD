8217d75ca159db470846e28e9215fe08a61bc7f6
Felix Hädicke <felixhaedicke@web.de>
2017-10-06 23:06:28 +0200

    build/python: refactoring: introduce new class MakeProject
    This introduces a the new class MakeProject, which is used as a base
    class for all Makefile based thirdparty libraries.

--- python/build/autotools.py
+++ python/build/autotools.py
@@ -1,24 +1,22 @@
 import os.path, subprocess, sys
 
-from build.project import Project
+from build.makeproject import MakeProject
 
-class AutotoolsProject(Project):
+class AutotoolsProject(MakeProject):
     def __init__(self, url, md5, installed, configure_args=[],
                  autogen=False,
                  cppflags='',
                  ldflags='',
                  libs='',
-                 install_target='install',
                  **kwargs):
-        Project.__init__(self, url, md5, installed, **kwargs)
+        MakeProject.__init__(self, url, md5, installed, **kwargs)
         self.configure_args = configure_args
         self.autogen = autogen
         self.cppflags = cppflags
         self.ldflags = ldflags
         self.libs = libs
-        self.install_target = install_target
 
-    def build(self, toolchain):
+    def configure(self, toolchain):
         src = self.unpack(toolchain)
         if self.autogen:
             if sys.platform == 'darwin':
@@ -49,7 +47,8 @@ class AutotoolsProject(Project):
         ] + self.configure_args
 
         subprocess.check_call(configure, cwd=build, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', '-j12'],
-                              cwd=build, env=toolchain.env)
-        subprocess.check_call(['/usr/bin/make', '--quiet', self.install_target],
-                              cwd=build, env=toolchain.env)
+        return build
+
+    def build(self, toolchain):
+        build = self.configure(toolchain)
+        MakeProject.build(self, toolchain, build)
