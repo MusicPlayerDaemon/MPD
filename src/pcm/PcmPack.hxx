fee9f1482c0f593b43f88b2fd589c10d5176e639
Max Kellermann <max@musicpd.org>
2017-10-26 12:28:21 +0200

    pcm/Pack: add "noexcept"

--- src/pcm/PcmPack.hxx
+++ src/pcm/PcmPack.hxx
@@ -37,7 +37,8 @@
  * @param src the source buffer
  */
 void
-pcm_pack_24(uint8_t *dest, const int32_t *src, const int32_t *src_end);
+pcm_pack_24(uint8_t *dest,
+	    const int32_t *src, const int32_t *src_end) noexcept;
 
 /**
  * Converts packed 24 bit samples (3 bytes per sample) to padded 24
@@ -47,13 +48,15 @@ pcm_pack_24(uint8_t *dest, const int32_t *src, const int32_t *src_end);
  * @param src the source buffer (array of triples)
  */
 void
-pcm_unpack_24(int32_t *dest, const uint8_t *src, const uint8_t *src_end);
+pcm_unpack_24(int32_t *dest,
+	      const uint8_t *src, const uint8_t *src_end) noexcept;
 
 /**
  * Like pcm_unpack_24(), but assume the source byte order is
  * big-endian.  The destination byte order ia always native.
  */
 void
-pcm_unpack_24be(int32_t *dest, const uint8_t *src, const uint8_t *src_end);
+pcm_unpack_24be(int32_t *dest,
+		const uint8_t *src, const uint8_t *src_end) noexcept;
 
 #endif
