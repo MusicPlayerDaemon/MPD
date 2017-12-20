36899dba0bb0100f84c2a4644bf1e2c27627a44b
Max Kellermann <max@musicpd.org>
2017-12-20 10:50:36 +0100

    event/Call: document exceptions

--- src/event/Call.hxx
+++ src/event/Call.hxx
@@ -29,6 +29,8 @@ class EventLoop;
 /**
  * Call the given function in the context of the #EventLoop, and wait
  * for it to finish.
+ *
+ * Exceptions thrown by the given function will be rethrown.
  */
 void
 BlockingCall(EventLoop &loop, std::function<void()> &&f);
