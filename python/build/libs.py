272167b4fcc0c76dc3870b33c4246a00ab724968
Max Kellermann <max@musicpd.org>
2018-01-18 22:07:28 +0100

    python/build/libs.py: update LAME to 3.100

--- python/build/libs.py
+++ python/build/libs.py
@@ -83,8 +83,8 @@ libmad = AutotoolsProject(
 )
 
 liblame = AutotoolsProject(
-    'http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz',
-    '24346b4158e4af3bd9f2e194bb23eb473c75fb7377011523353196b19b9a23ff',
+    'http://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz',
+    'ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e',
     'lib/libmp3lame.a',
     [
         '--disable-shared', '--enable-static',
