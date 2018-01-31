dead4615427c675dac48dabdb9672aaa602f17b8
Max Kellermann <max@musicpd.org>
2018-01-31 18:15:46 +0100

    lib/upnp/Init: enable IPv6

--- src/lib/upnp/Init.cxx
+++ src/lib/upnp/Init.cxx
@@ -34,7 +34,11 @@ static unsigned upnp_ref;
 static void
 DoInit()
 {
+#ifdef UPNP_ENABLE_IPV6
+	auto code = UpnpInit2(nullptr, 0);
+#else
 	auto code = UpnpInit(nullptr, 0);
+#endif
 	if (code != UPNP_E_SUCCESS)
 		throw FormatRuntimeError("UpnpInit() failed: %s",
 					 UpnpGetErrorMessage(code));
