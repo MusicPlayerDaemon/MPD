MusicPlayerDaemon/MPD

commit 386688b87ac87200ed6c68b0b5273ad7151e8a0a

SongFilter: use std::string instead of AllocatedString

src/SongFilter.cxx
@@ -59,7 +59,7 @@ locate_parse_type(const char *str) noexcept
 
 SongFilter::Item::Item(unsigned _tag, const char *_value, bool _fold_case)
 	:tag(_tag),
-	 value(AllocatedString<>::Duplicate(_value)),
+	 value(_value),
 	 fold_case(_fold_case ? IcuCompare(value.c_str()) : IcuCompare())
 {
 }

src/SongFilter.hxx
@@ -21,9 +21,9 @@
 #define MPD_SONG_FILTER_HXX
 
 #include "lib/icu/Compare.hxx"
-#include "util/AllocatedString.hxx"
 #include "Compiler.h"
 
+#include <string>
 #include <list>
 
 #include <stdint.h>
@@ -49,7 +49,7 @@ public:
 	class Item {
 		uint8_t tag;
 
-		AllocatedString<> value;
+		std::string value;
 
 		/**
 		 * This value is only set if case folding is enabled.

commit 5147654f6c6e452132be6bd4b8d525d1c1856878

SongFilter: fix "modified-since" filter
Error message sent to client was "basic_string::_M_construct null not
valid" due to passing nullptr to the std::string constructor.

Regression caused by commit 386688b87ac

src/SongFilter.cxx
@@ -69,7 +69,7 @@ SongFilter::Item::Item(unsigned _tag, const char *_value, bool _fold_case)
 
 SongFilter::Item::Item(unsigned _tag,
 		       std::chrono::system_clock::time_point _time)
-	:tag(_tag), value(nullptr), time(_time)
+	:tag(_tag), time(_time)
 {
 }
 