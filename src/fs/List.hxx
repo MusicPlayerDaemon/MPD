fcbcec6856a0e574b5ebc6d990c0afc0e25f132b
Max Kellermann <max@musicpd.org>
2018-07-18 13:32:25 +0200

    fs/List: new library exporting ListWildcard()

---
+++ src/fs/List.hxx
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2003-2017 The Music Player Daemon Project
+ * http://www.musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MPD_FS_LIST_XX
+#define MPD_FS_LIST_XX
+
+#include "check.h"
+
+#include <forward_list>
+
+class Path;
+class AllocatedPath;
+
+/**
+ * Returns a sorted list of files matching the given pattern.
+ *
+ * Throws on error.
+ */
+std::forward_list<AllocatedPath>
+ListWildcard(Path pattern);
+
+#endif
