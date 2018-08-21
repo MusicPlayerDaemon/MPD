eee91aa4eadd5dae7438e3c7abfa32c546290d6b
Max Kellermann <max@musicpd.org>
2018-08-21 08:20:52 +0200

    util/Exception: update copyright

--- src/util/StringBuffer.hxx
+++ src/util/StringBuffer.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2018 Max Kellermann <max.kellermann@gmail.com>
+ * Copyright 2010-2018 Max Kellermann <max.kellermann@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
