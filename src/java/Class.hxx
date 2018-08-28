fbeb5eefdc43c82394ba564f743128bb1f3116c8
Max Kellermann <max@musicpd.org>
2018-08-28 13:01:01 +0200

    java/Class: drop unnecessary namespace spec

--- src/java/Class.hxx
+++ src/java/Class.hxx
@@ -39,7 +39,7 @@ namespace Java {
 	/**
 	 * Wrapper for a local "jclass" reference.
 	 */
-	class Class : public Java::LocalRef<jclass> {
+	class Class : public LocalRef<jclass> {
 	public:
 		Class(JNIEnv *env, jclass cls)
 			:LocalRef<jclass>(env, cls) {}
