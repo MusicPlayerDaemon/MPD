2148d4bb31cde4751d7be42e2d8c7cd9ac4e0ff4
Max Kellermann <max@musicpd.org>
2018-10-31 17:18:44 +0100

    fs/io/FileOutputStream: fix -Wcomment

--- src/fs/io/FileOutputStream.hxx
+++ src/fs/io/FileOutputStream.hxx
@@ -50,7 +50,7 @@
 /* we don't use O_TMPFILE on Android because Android's braindead
    SELinux policy disallows hardlinks
    (https://android.googlesource.com/platform/external/sepolicy/+/85ce2c7),
-   even hardlinks from /proc/self/fd/*, which however is required to
+   even hardlinks from /proc/self/fd/N, which however is required to
    use O_TMPFILE */
 #define HAVE_O_TMPFILE
 #endif
