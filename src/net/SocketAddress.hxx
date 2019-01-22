d77b0c7dcdf46b627034fd22c6f9af9171f43bd3
Max Kellermann <max@musicpd.org>
2019-01-22 08:42:35 +0100

    net/SocketAddress: add `constexpr`

--- src/net/SocketAddress.hxx
+++ src/net/SocketAddress.hxx
@@ -93,7 +93,7 @@ public:
 	 * Does the object have a well-defined address?  Check !IsNull()
 	 * before calling this method.
 	 */
-	bool IsDefined() const noexcept {
+	constexpr bool IsDefined() const noexcept {
 		return GetFamily() != AF_UNSPEC;
 	}
 
