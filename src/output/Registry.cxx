e147cf00df25b5ed7cb42c6af39a901b07749089
Richard Patel <richxpatel@gmail.com>
2019-02-23 00:32:05 +0100

    Registry

--- src/output/Registry.cxx
+++ src/output/Registry.cxx
@@ -23,6 +23,7 @@
 #include "plugins/AlsaOutputPlugin.hxx"
 #include "plugins/AoOutputPlugin.hxx"
 #include "plugins/FifoOutputPlugin.hxx"
+#include "plugins/SockOutputPlugin.hxx"
 #include "plugins/SndioOutputPlugin.hxx"
 #include "plugins/httpd/HttpdOutputPlugin.hxx"
 #include "plugins/HaikuOutputPlugin.hxx"
@@ -51,6 +52,9 @@ const AudioOutputPlugin *const audio_output_plugins[] = {
 #endif
 #ifdef HAVE_FIFO
 	&fifo_output_plugin,
+#endif
+#ifdef HAVE_SOCK
+  &sock_output_plugin,
 #endif
 #ifdef ENABLE_SNDIO
 	&sndio_output_plugin,
