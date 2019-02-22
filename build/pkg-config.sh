c672b60d07003d7017c051314bdbdc1f13afffee
Max Kellermann <max@musicpd.org>
2019-02-22 12:39:59 +0100

    build/pkg-config.sh: add comment

--- build/pkg-config.sh
+++ build/pkg-config.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# This is a wrapper for pkg-config which helps with cross-compiling;
+# it sets up environment variables to pkg-config searches for
+# libraries in the sysroot where a copy of this script is located.
+
 BIN=`dirname $0`
 ROOT=`dirname "$BIN"`
 
