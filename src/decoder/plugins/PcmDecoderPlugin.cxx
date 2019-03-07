c5df879cf97b24caac1d34272066ae46caa98df9
borine <32966433+borine@users.noreply.github.com>
2019-03-07 12:31:43 +0000

    decooder/plugins/PcmDecoderPlugin: add missing config for preproceesor macro definitions

--- src/decoder/plugins/PcmDecoderPlugin.cxx
+++ src/decoder/plugins/PcmDecoderPlugin.cxx
@@ -17,6 +17,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include "config.h"
+
 #include "PcmDecoderPlugin.hxx"
 #include "../DecoderAPI.hxx"
 #include "CheckAudioFormat.hxx"
