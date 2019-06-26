MusicPlayerDaemon/MPD

commit ea639269d8b0ae3db06c46d546d54896a8d8c89e

decoder/wildmidi: throw PluginUnavailable on WildMidi_Init() error
Closes https://github.com/MusicPlayerDaemon/MPD/issues/589

src/decoder/plugins/WildmidiDecoderPlugin.cxx
@@ -21,6 +21,7 @@
 #include "../DecoderAPI.hxx"
 #include "tag/Handler.hxx"
 #include "util/Domain.hxx"
+#include "util/ScopeExit.hxx"
 #include "util/StringFormat.hxx"
 #include "fs/AllocatedPath.hxx"
 #include "fs/FileSystem.hxx"
@@ -49,8 +50,14 @@ wildmidi_init(const ConfigBlock &block)
 							   utf8.c_str()));
 	}
 
-	return WildMidi_Init(path.c_str(), wildmidi_audio_format.sample_rate,
-			     0) == 0;
+	WildMidi_ClearError();
+	AtScopeExit() { WildMidi_ClearError(); };
+
+	if (WildMidi_Init(path.c_str(), wildmidi_audio_format.sample_rate,
+			  0) != 0)
+		throw PluginUnavailable(WildMidi_GetError());
+
+	return true;
 }
 
 static void

commit f07f8f7d882757e77cf1e0829daf425dbcc15d58

decoder/wildmidi: add fallbacks for libwildmidi<0.4
Fix build breakage from commit ea639269d8b0ae3db06c46d546d54896a8d8c89e

src/decoder/plugins/WildmidiDecoderPlugin.cxx
@@ -47,12 +47,21 @@ wildmidi_init(const ConfigBlock &block)
 							   utf8.c_str()));
 	}
 
+#ifdef LIBWILDMIDI_VERSION
+	/* WildMidi_ClearError() requires libwildmidi 0.4 */
 	WildMidi_ClearError();
 	AtScopeExit() { WildMidi_ClearError(); };
+#endif
 
 	if (WildMidi_Init(path.c_str(), wildmidi_audio_format.sample_rate,
-			  0) != 0)
+			  0) != 0) {
+#ifdef LIBWILDMIDI_VERSION
+		/* WildMidi_GetError() requires libwildmidi 0.4 */
 		throw PluginUnavailable(WildMidi_GetError());
+#else
+		throw PluginUnavailable("WildMidi_Init() failed");
+#endif
+	}
 
 	return true;
 }