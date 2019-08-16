0c4a7c8004c602ff7ccaffce9f97704c5c46287f
Max Kellermann <max@musicpd.org>
2019-08-16 13:20:23 +0200

    util/AllocatedString: update copyright

--- src/util/AllocatedString.hxx
+++ src/util/AllocatedString.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Max Kellermann <max.kellermann@gmail.com>
+ * Copyright 2015-2019 Max Kellermann <max.kellermann@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
