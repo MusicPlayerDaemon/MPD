995783bb2f41b25b22c7409d17250988b00b2d9d
Max Kellermann <max@musicpd.org>
2019-08-19 22:33:21 +0200

    test/TestISO8601: unit test for time/ISO8601

---
+++ test/TestISO8601.cxx
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Content Management AG
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "time/ISO8601.hxx"
+
+#include <gtest/gtest.h>
+
+static constexpr struct {
+	const char *s;
+	time_t t;
+	std::chrono::system_clock::duration d;
+} parse_tests[] = {
+	/* full ISO8601 */
+	{ "1970-01-01T00:00:00Z", 0, std::chrono::seconds(1) },
+	{ "1970-01-01T00:00:01Z", 1, std::chrono::seconds(1) },
+	{ "2019-02-04T16:46:41Z", 1549298801, std::chrono::seconds(1) },
+	{ "2018-12-31T23:59:59Z", 1546300799, std::chrono::seconds(1) },
+	{ "2019-01-01T00:00:00Z", 1546300800, std::chrono::seconds(1) },
+};
+
+TEST(ISO8601, Parse)
+{
+	for (const auto &i : parse_tests) {
+		const auto result = ParseISO8601(i.s);
+		EXPECT_EQ(std::chrono::system_clock::to_time_t(result.first), i.t);
+		EXPECT_EQ(result.second, i.d);
+	}
+}
