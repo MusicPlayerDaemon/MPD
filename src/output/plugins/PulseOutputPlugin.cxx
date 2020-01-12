af20a1c994fc829d1fe93da144fac8451e2295f6
Max Kellermann <max@musicpd.org>
2020-01-12 07:57:52 +0100

    pulse: obey Pulse's maximum sample rate (fixes DSD128 playback)
    Closes https://github.com/MusicPlayerDaemon/MPD/issues/708

--- src/output/plugins/PulseOutputPlugin.cxx
+++ src/output/plugins/PulseOutputPlugin.cxx
@@ -650,7 +650,7 @@ PulseOutput::Open(AudioFormat &audio_format)
 		break;
 	}
 
-	ss.rate = audio_format.sample_rate;
+	ss.rate = std::min(audio_format.sample_rate, PA_RATE_MAX);
 	ss.channels = audio_format.channels;
 
 	/* create a stream .. */
