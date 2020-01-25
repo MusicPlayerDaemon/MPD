f6f30d6d64946eda043bce2c7025ef8c5074444f
Max Kellermann <max@musicpd.org>
2020-01-25 20:06:58 +0100

    increment version number to 0.21.20

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ author = 'Max Kellermann'
 # built documents.
 #
 # The short X.Y version.
-version = '0.21.19'
+version = '0.21.20'
 # The full version, including alpha/beta/rc tags.
 release = version
 
