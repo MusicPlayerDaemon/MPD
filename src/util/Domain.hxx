0896f444552a383a0da1b5056a361bef9596f414
Max Kellermann <max@musicpd.org>
2020-02-01 13:25:57 +0100

    util/Domain: add `noexcept`

--- src/util/Domain.hxx
+++ src/util/Domain.hxx
@@ -34,21 +34,21 @@ class Domain {
 	const char *const name;
 
 public:
-	constexpr explicit Domain(const char *_name)
+	constexpr explicit Domain(const char *_name) noexcept
 		:name(_name) {}
 
 	Domain(const Domain &) = delete;
 	Domain &operator=(const Domain &) = delete;
 
-	constexpr const char *GetName() const {
+	constexpr const char *GetName() const noexcept {
 		return name;
 	}
 
-	bool operator==(const Domain &other) const {
+	bool operator==(const Domain &other) const noexcept {
 		return this == &other;
 	}
 
-	bool operator!=(const Domain &other) const {
+	bool operator!=(const Domain &other) const noexcept {
 		return !(*this == other);
 	}
 };
