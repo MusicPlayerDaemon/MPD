MusicPlayerDaemon/MPD

commit cc72ceb368185381851bec2d7d4fe1c4d69b3f46

tag/FixString: use IsPrintableASCII()

src/tag/FixString.cxx
@@ -19,6 +19,7 @@
 
 #include "FixString.hxx"
 #include "util/Alloc.hxx"
+#include "util/CharUtil.hxx"
 #include "util/WritableBuffer.hxx"
 #include "util/StringView.hxx"
 #include "util/UTF8.hxx"
@@ -84,17 +85,11 @@ fix_utf8(StringView p)
 	return patch_utf8(p, invalid);
 }
 
-static bool
-char_is_non_printable(unsigned char ch)
-{
-	return ch < 0x20;
-}
-
 static const char *
 find_non_printable(StringView p)
 {
 	for (const char &ch : p)
-		if (char_is_non_printable(ch))
+		if (!IsPrintableASCII(ch))
 			return &ch;
 
 	return nullptr;
@@ -114,7 +109,7 @@ clear_non_printable(StringView src)
 	char *dest = (char *)xmemdup(src.data, src.size);
 
 	for (size_t i = first - src.data; i < src.size; ++i)
-		if (char_is_non_printable(dest[i]))
+		if (!IsPrintableASCII(dest[i]))
 			dest[i] = ' ';
 
 	return { dest, src.size };

commit bca9678683eb8dbb31e7b5f348d9a3436639de22

tag/FixString: use IsNonPrintableASCII()
Fixes breakage of non-ASCII characters, regression from commit
cc72ceb368185381851bec2d7d4fe1c4d69b3f46

Fixes https://github.com/MusicPlayerDaemon/MPD/issues/842

src/tag/FixString.cxx
@@ -90,7 +90,7 @@ static const char *
 find_non_printable(StringView p)
 {
 	for (const char &ch : p)
-		if (!IsPrintableASCII(ch))
+		if (IsNonPrintableASCII(ch))
 			return &ch;
 
 	return nullptr;
@@ -110,7 +110,7 @@ clear_non_printable(StringView src)
 	char *dest = (char *)xmemdup(src.data, src.size);
 
 	for (size_t i = first - src.data; i < src.size; ++i)
-		if (!IsPrintableASCII(dest[i]))
+		if (IsNonPrintableASCII(dest[i]))
 			dest[i] = ' ';
 
 	return { dest, src.size };