61aed60f6d26560203d9d501138d9bf938ec3323
Max Kellermann <max@musicpd.org>
2020-05-07 14:18:54 +0200

    python/build/libs.py: update CURL to 7.70.0

--- python/build/libs.py
+++ python/build/libs.py
@@ -341,8 +341,8 @@ ffmpeg = FfmpegProject(
 )
 
 curl = AutotoolsProject(
-    'http://curl.haxx.se/download/curl-7.69.1.tar.xz',
-    '03c7d5e6697f7b7e40ada1b2256e565a555657398e6c1fcfa4cb251ccd819d4f',
+    'http://curl.haxx.se/download/curl-7.70.0.tar.xz',
+    '032f43f2674008c761af19bf536374128c16241fb234699a55f9fb603fcfbae7',
     'lib/libcurl.a',
     [
         '--disable-shared', '--enable-static',
