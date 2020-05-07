2cc323c9fe1cd8f39553cc0f1cc8894718e068d8
Max Kellermann <max@musicpd.org>
2020-05-07 14:18:20 +0200

    python/build/libs.py: update Boost to 1.73.0

--- python/build/libs.py
+++ python/build/libs.py
@@ -397,7 +397,7 @@ libnfs = AutotoolsProject(
 )
 
 boost = BoostProject(
-    'https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.bz2',
-    '59c9b274bc451cf91a9ba1dd2c7fdcaf5d60b1b3aa83f2c9fa143417cc660722',
+    'https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.bz2',
+    '4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402',
     'include/boost/version.hpp',
 )
