749ad7cd83f4061fc8714202bcc51b3ec065139d
Max Kellermann <max@musicpd.org>
2020-07-06 20:40:07 +0200

    PluginUnavailable: inherit the base class constructor

--- src/PluginUnavailable.hxx
+++ src/PluginUnavailable.hxx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2018 The Music Player Daemon Project
+ * Copyright 2003-2020 The Music Player Daemon Project
  * http://www.musicpd.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -29,8 +29,7 @@
  */
 class PluginUnavailable final : public std::runtime_error {
 public:
-	explicit PluginUnavailable(const char *msg)
-		:std::runtime_error(msg) {}
+	using std::runtime_error::runtime_error;
 };
 
 #endif
