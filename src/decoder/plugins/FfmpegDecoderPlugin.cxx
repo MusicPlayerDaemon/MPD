6eba621045dd181472b9cda1043d5b8af10e2de5
Max Kellermann <max@musicpd.org>
2021-02-22 13:36:08 +0100

    decoder/ffmpeg: fix build problem with FFmpeg 3.4
    Regression by commit a22d1c88d7e11bfdc553f38a86d416783421c7e4
    
    Closes https://github.com/MusicPlayerDaemon/MPD/issues/1097

--- src/decoder/plugins/FfmpegDecoderPlugin.cxx
+++ src/decoder/plugins/FfmpegDecoderPlugin.cxx
@@ -467,7 +467,11 @@ FfmpegCheckTag(DecoderClient &client, InputStream *is,
 static bool
 IsSeekable(const AVFormatContext &format_context) noexcept
 {
+#if LIBAVFORMAT_VERSION_INT >= AV_VERSION_INT(58, 6, 100)
 	return (format_context.ctx_flags & AVFMTCTX_UNSEEKABLE) != 0;
+#else
+	return false;
+#endif
 }
 
 static void
