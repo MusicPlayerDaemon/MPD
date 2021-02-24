8fe8f0902707c7e6f5a91ab299841019ca4682cf
Max Kellermann <max@musicpd.org>
2021-02-24 20:17:26 +0100

    util/IntrusiveList: add `noexcept`

--- src/util/IntrusiveList.hxx
+++ src/util/IntrusiveList.hxx
@@ -226,7 +226,7 @@ public:
 			:cursor(_cursor) {}
 
 	public:
-		iterator() = default;
+		iterator() noexcept = default;
 
 		constexpr bool operator==(const iterator &other) const noexcept {
 			return cursor == other.cursor;
@@ -273,7 +273,7 @@ public:
 			:cursor(_cursor) {}
 
 	public:
-		const_iterator() = default;
+		const_iterator() noexcept = default;
 
 		const_iterator(iterator src) noexcept
 			:cursor(src.cursor) {}
