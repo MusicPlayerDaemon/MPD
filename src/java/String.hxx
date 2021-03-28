285ba54fe58e630c150deb1cb216fde41738b695
Max Kellermann <max.kellermann@gmail.com>
2021-03-28 13:33:07 +0200

    java/String: remove unnecessary `env` parameter

--- src/java/String.hxx
+++ src/java/String.hxx
@@ -66,9 +66,8 @@ public:
 	 * @return a pointer to the terminating null byte,
 	 * nullptr on error
 	 */
-	char *CopyTo(JNIEnv *env,
-		     char *buffer, size_t max_size) const noexcept {
-		return CopyTo(env, Get(), buffer, max_size);
+	char *CopyTo(char *buffer, size_t max_size) const noexcept {
+		return CopyTo(GetEnv(), Get(), buffer, max_size);
 	}
 
 	static std::string ToString(JNIEnv *env, jstring s) noexcept;
