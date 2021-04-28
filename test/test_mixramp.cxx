8c51440057a52436322f7ba538d88b18f73490c8
Max Kellermann <max@musicpd.org>
2021-04-28 09:15:06 +0200

    test/test_mixramp: workaround for -Wdouble-promotion

--- test/test_mixramp.cxx
+++ test/test_mixramp.cxx
@@ -13,25 +13,25 @@ TEST(MixRamp, Interpolate)
 	const char *input = "1.0 0.00;3.0 0.10;6.0 2.50;";
 
 	char *foo = strdup(input);
-	EXPECT_NEAR(double(0),
+	EXPECT_NEAR(0.,
 		    mixramp_interpolate(foo, 0).count(),
 		    0.05);
 	free(foo);
 
 	foo = strdup(input);
-	EXPECT_NEAR(float(0),
+	EXPECT_NEAR(0.,
 		    mixramp_interpolate(foo, 1).count(),
 		    0.005);
 	free(foo);
 
 	foo = strdup(input);
-	EXPECT_NEAR(float(0.1),
+	EXPECT_NEAR(0.1,
 		    mixramp_interpolate(foo, 3).count(),
 		    0.005);
 	free(foo);
 
 	foo = strdup(input);
-	EXPECT_NEAR(float(2.5),
+	EXPECT_NEAR(2.5,
 		    mixramp_interpolate(foo, 6).count(),
 		    0.01);
 	free(foo);
@@ -41,25 +41,25 @@ TEST(MixRamp, Interpolate)
 	free(foo);
 
 	foo = strdup(input);
-	EXPECT_NEAR(float(0.05),
+	EXPECT_NEAR(0.05,
 		    mixramp_interpolate(foo, 2).count(),
 		    0.05);
 	free(foo);
 
 	foo = strdup(input);
-	EXPECT_NEAR(float(1.3),
+	EXPECT_NEAR(1.3,
 		    mixramp_interpolate(foo, 4.5).count(),
 		    0.05);
 	free(foo);
 
 	foo = strdup(input);
-	EXPECT_NEAR(float(0.9),
+	EXPECT_NEAR(0.9,
 		    mixramp_interpolate(foo, 4).count(),
 		    0.05);
 	free(foo);
 
 	foo = strdup(input);
-	EXPECT_NEAR(float(1.7),
+	EXPECT_NEAR(1.7,
 		    mixramp_interpolate(foo, 5).count(),
 		    0.05);
 	free(foo);
