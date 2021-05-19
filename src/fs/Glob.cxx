c64a3b5dbb25fceabfa30828462bf8a855141e21
Max Kellermann <max@musicpd.org>
2021-05-19 17:39:03 +0200

    fs/Glob: un-inline the Windows version to reduce header dependencies

--- src/fs/Glob.cxx
+++ src/fs/Glob.cxx
@@ -17,51 +17,15 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef MPD_FS_GLOB_XX
-#define MPD_FS_GLOB_XX
+#include "Glob.hxx"
 
-#include "config.h"
+#ifdef _WIN32
+#include <shlwapi.h>
 
-#ifdef HAVE_FNMATCH
-#define HAVE_CLASS_GLOB
-#include <fnmatch.h>
-#elif defined(_WIN32)
-#define HAVE_CLASS_GLOB
-#endif
-
-#ifdef HAVE_CLASS_GLOB
-#include "util/Compiler.h"
-
-#include <string>
-
-/**
- * A pattern that matches file names.  It may contain shell wildcards
- * (asterisk and question mark).
- */
-class Glob {
-	std::string pattern;
-
-public:
-	explicit Glob(const char *_pattern)
-		:pattern(_pattern) {}
-
-	Glob(Glob &&other) noexcept = default;
-	Glob &operator=(Glob &&other) noexcept = default;
-
-	gcc_pure
-	bool Check(const char *name_fs) const noexcept;
-};
-
-#ifdef HAVE_FNMATCH
-
-inline bool
+bool
 Glob::Check(const char *name_fs) const noexcept
 {
-	return fnmatch(pattern.c_str(), name_fs, 0) == 0;
+	return PathMatchSpecA(name_fs, pattern.c_str());
 }
-
-#endif
-
-#endif /* HAVE_CLASS_GLOB */
 
 #endif
