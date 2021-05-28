MusicPlayerDaemon/MPD

commit 0440c41cbac0857eaab1220e3b0852c61bfc794a

client/Response: add method Fmt() based on libfmt

src/command/PlayerCommands.cxx
@@ -38,6 +38,8 @@
 #include "db/update/Service.hxx"
 #endif
 
+#include <fmt/format.h>
+
 #define COMMAND_STATUS_STATE            "state"
 #define COMMAND_STATUS_REPEAT           "repeat"
 #define COMMAND_STATUS_SINGLE           "single"
@@ -131,60 +133,60 @@ handle_status(Client &client, [[maybe_unused]] Request args, Response &r)
 
 	const auto volume = volume_level_get(partition.outputs);
 	if (volume >= 0)
-		r.Format("volume: %i\n", volume);
+		r.Fmt(FMT_STRING("volume: {}\n"), volume);
 
-	r.Format(COMMAND_STATUS_REPEAT ": %i\n"
-		 COMMAND_STATUS_RANDOM ": %i\n"
-		 COMMAND_STATUS_SINGLE ": %s\n"
-		 COMMAND_STATUS_CONSUME ": %i\n"
-		 "partition: %s\n"
-		 COMMAND_STATUS_PLAYLIST ": %li\n"
-		 COMMAND_STATUS_PLAYLIST_LENGTH ": %i\n"
-		 COMMAND_STATUS_MIXRAMPDB ": %f\n"
-		 COMMAND_STATUS_STATE ": %s\n",
-		 playlist.GetRepeat(),
-		 playlist.GetRandom(),
-		 SingleToString(playlist.GetSingle()),
-		 playlist.GetConsume(),
-		 partition.name.c_str(),
-		 (unsigned long)playlist.GetVersion(),
-		 playlist.GetLength(),
-		 (double)pc.GetMixRampDb(),
-		 state);
+	r.Fmt(FMT_STRING(COMMAND_STATUS_REPEAT ": {}\n"
+			 COMMAND_STATUS_RANDOM ": {}\n"
+			 COMMAND_STATUS_SINGLE ": {}\n"
+			 COMMAND_STATUS_CONSUME ": {}\n"
+			 "partition: {}\n"
+			 COMMAND_STATUS_PLAYLIST ": {}\n"
+			 COMMAND_STATUS_PLAYLIST_LENGTH ": {}\n"
+			 COMMAND_STATUS_MIXRAMPDB ": {}\n"
+			 COMMAND_STATUS_STATE ": {}\n"),
+	      playlist.GetRepeat(),
+	      playlist.GetRandom(),
+	      SingleToString(playlist.GetSingle()),
+	      playlist.GetConsume(),
+	      partition.name.c_str(),
+	      playlist.GetVersion(),
+	      playlist.GetLength(),
+	      pc.GetMixRampDb(),
+	      state);
 
 	if (pc.GetCrossFade() > FloatDuration::zero())
-		r.Format(COMMAND_STATUS_CROSSFADE ": %lu\n",
-			 lround(pc.GetCrossFade().count()));
+		r.Fmt(FMT_STRING(COMMAND_STATUS_CROSSFADE ": {}\n"),
+		      lround(pc.GetCrossFade().count()));
 
 	if (pc.GetMixRampDelay() > FloatDuration::zero())
-		r.Format(COMMAND_STATUS_MIXRAMPDELAY ": %f\n",
-			 pc.GetMixRampDelay().count());
+		r.Fmt(FMT_STRING(COMMAND_STATUS_MIXRAMPDELAY ": {}\n"),
+		      pc.GetMixRampDelay().count());
 
 	song = playlist.GetCurrentPosition();
 	if (song >= 0) {
-		r.Format(COMMAND_STATUS_SONG ": %i\n"
-			 COMMAND_STATUS_SONGID ": %u\n",
-			 song, playlist.PositionToId(song));
+		r.Fmt(FMT_STRING(COMMAND_STATUS_SONG ": {}\n"
+				 COMMAND_STATUS_SONGID ": {}\n"),
+		      song, playlist.PositionToId(song));
 	}
 
 	if (player_status.state != PlayerState::STOP) {
-		r.Format(COMMAND_STATUS_TIME ": %i:%i\n"
-			 "elapsed: %1.3f\n"
-			 COMMAND_STATUS_BITRATE ": %u\n",
-			 player_status.elapsed_time.RoundS(),
-			 player_status.total_time.IsNegative()
-			 ? 0U
-			 : unsigned(player_status.total_time.RoundS()),
-			 player_status.elapsed_time.ToDoubleS(),
-			 player_status.bit_rate);
+		r.Fmt(FMT_STRING(COMMAND_STATUS_TIME ": {}:{}\n"
+				 "elapsed: {:1.3}\n"
+				 COMMAND_STATUS_BITRATE ": {}\n"),
+		      player_status.elapsed_time.RoundS(),
+		      player_status.total_time.IsNegative()
+		      ? 0U
+		      : unsigned(player_status.total_time.RoundS()),
+		      player_status.elapsed_time.ToDoubleS(),
+		      player_status.bit_rate);
 
 		if (!player_status.total_time.IsNegative())
-			r.Format("duration: %1.3f\n",
+			r.Fmt(FMT_STRING("duration: {:1.3}\n"),
 				 player_status.total_time.ToDoubleS());
 
 		if (player_status.audio_format.IsDefined())
-			r.Format(COMMAND_STATUS_AUDIO ": %s\n",
-				 ToString(player_status.audio_format).c_str());
+			r.Fmt(FMT_STRING(COMMAND_STATUS_AUDIO ": {}\n"),
+			      ToString(player_status.audio_format));
 	}
 
 #ifdef ENABLE_DATABASE
@@ -193,23 +195,23 @@ handle_status(Client &client, [[maybe_unused]] Request args, Response &r)
 		? update_service->GetId()
 		: 0;
 	if (updateJobId != 0) {
-		r.Format(COMMAND_STATUS_UPDATING_DB ": %i\n",
-			 updateJobId);
+		r.Fmt(FMT_STRING(COMMAND_STATUS_UPDATING_DB ": {}\n"),
+		      updateJobId);
 	}
 #endif
 
 	try {
 		pc.LockCheckRethrowError();
 	} catch (...) {
-		r.Format(COMMAND_STATUS_ERROR ": %s\n",
-			 GetFullMessage(std::current_exception()).c_str());
+		r.Fmt(FMT_STRING(COMMAND_STATUS_ERROR ": {}\n"),
+		      GetFullMessage(std::current_exception()));
 	}
 
 	song = playlist.GetNextPosition();
 	if (song >= 0)
-		r.Format(COMMAND_STATUS_NEXTSONG ": %i\n"
-			 COMMAND_STATUS_NEXTSONGID ": %u\n",
-			 song, playlist.PositionToId(song));
+		r.Fmt(FMT_STRING(COMMAND_STATUS_NEXTSONG ": {}\n"
+				 COMMAND_STATUS_NEXTSONGID ": {}\n"),
+		      song, playlist.PositionToId(song));
 
 	return CommandResult::OK;
 }
@@ -351,7 +353,7 @@ CommandResult
 handle_replay_gain_status(Client &client, [[maybe_unused]] Request args,
 			  Response &r)
 {
-	r.Format("replay_gain_mode: %s\n",
-		 ToString(client.GetPartition().replay_gain_mode));
+	r.Fmt(FMT_STRING("replay_gain_mode: {}\n"),
+	      ToString(client.GetPartition().replay_gain_mode));
 	return CommandResult::OK;
 }

src/output/Print.cxx
@@ -26,21 +26,24 @@
 #include "MultipleOutputs.hxx"
 #include "client/Response.hxx"
 
+#include <fmt/format.h>
+
 void
 printAudioDevices(Response &r, const MultipleOutputs &outputs)
 {
 	for (unsigned i = 0, n = outputs.Size(); i != n; ++i) {
 		const auto &ao = outputs.Get(i);
 
-		r.Format("outputid: %u\n"
-			 "outputname: %s\n"
-			 "plugin: %s\n"
-			 "outputenabled: %i\n",
-			 i,
-			 ao.GetName(), ao.GetPluginName(),
-			 ao.IsEnabled());
+		r.Fmt(FMT_STRING("outputid: {}\n"
+				 "outputname: {}\n"
+				 "plugin: {}\n"
+				 "outputenabled: {}\n"),
+		      i,
+		      ao.GetName(), ao.GetPluginName(),
+		      ao.IsEnabled());
 
 		for (const auto &[attribute, value] : ao.GetAttributes())
-			r.Format("attribute: %s=%s\n", attribute.c_str(), value.c_str());
+			r.Fmt(FMT_STRING("attribute: {}={}\n"),
+			      attribute, value);
 	}
 }

src/sticker/Print.cxx
@@ -21,11 +21,13 @@
 #include "Sticker.hxx"
 #include "client/Response.hxx"
 
+#include <fmt/format.h>
+
 void
 sticker_print_value(Response &r,
 		    const char *name, const char *value)
 {
-	r.Format("sticker: %s=%s\n", name, value);
+	r.Fmt(FMT_STRING("sticker: {}={}\n"), name, value);
 }
 
 void

src/SongPrint.cxx
@@ -27,6 +27,8 @@
 #include "time/ChronoUtil.hxx"
 #include "util/UriUtil.hxx"
 
+#include <fmt/format.h>
+
 #define SONG_FILE "file: "
 
 static void
@@ -42,14 +44,15 @@ song_print_uri(Response &r, const char *uri, bool base) noexcept
 			uri = allocated.c_str();
 	}
 
-	r.Format(SONG_FILE "%s\n", uri);
+	r.Fmt(FMT_STRING(SONG_FILE "{}\n"), uri);
 }
 
 void
 song_print_uri(Response &r, const LightSong &song, bool base) noexcept
 {
 	if (!base && song.directory != nullptr)
-		r.Format(SONG_FILE "%s/%s\n", song.directory, song.uri);
+		r.Fmt(FMT_STRING(SONG_FILE "{}/{}\n"),
+		      song.directory, song.uri);
 	else
 		song_print_uri(r, song.uri, base);
 }
@@ -67,15 +70,15 @@ PrintRange(Response &r, SongTime start_time, SongTime end_time) noexcept
 	const unsigned end_ms = end_time.ToMS();
 
 	if (end_ms > 0)
-		r.Format("Range: %u.%03u-%u.%03u\n",
-			 start_ms / 1000,
-			 start_ms % 1000,
-			 end_ms / 1000,
-			 end_ms % 1000);
+		r.Fmt(FMT_STRING("Range: {}.{:03}-{}.{:03}\n"),
+		      start_ms / 1000,
+		      start_ms % 1000,
+		      end_ms / 1000,
+		      end_ms % 1000);
 	else if (start_ms > 0)
-		r.Format("Range: %u.%03u-\n",
-			 start_ms / 1000,
-			 start_ms % 1000);
+		r.Fmt(FMT_STRING("Range: {}.{:03}-\n"),
+		      start_ms / 1000,
+		      start_ms % 1000);
 }
 
 void
@@ -89,16 +92,16 @@ song_print_info(Response &r, const LightSong &song, bool base) noexcept
 		time_print(r, "Last-Modified", song.mtime);
 
 	if (song.audio_format.IsDefined())
-		r.Format("Format: %s\n", ToString(song.audio_format).c_str());
+		r.Fmt(FMT_STRING("Format: {}\n"), ToString(song.audio_format));
 
 	tag_print_values(r, song.tag);
 
 	const auto duration = song.GetDuration();
 	if (!duration.IsNegative())
-		r.Format("Time: %i\n"
-			 "duration: %1.3f\n",
-			 duration.RoundS(),
-			 duration.ToDoubleS());
+		r.Fmt(FMT_STRING("Time: {}\n"
+				 "duration: {:1.3}\n"),
+		      duration.RoundS(),
+		      duration.ToDoubleS());
 }
 
 void
@@ -112,14 +115,14 @@ song_print_info(Response &r, const DetachedSong &song, bool base) noexcept
 		time_print(r, "Last-Modified", song.GetLastModified());
 
 	if (const auto &f = song.GetAudioFormat(); f.IsDefined())
-		r.Format("Format: %s\n", ToString(f).c_str());
+		r.Fmt(FMT_STRING("Format: {}\n"), ToString(f));
 
 	tag_print_values(r, song.GetTag());
 
 	const auto duration = song.GetDuration();
 	if (!duration.IsNegative())
-		r.Format("Time: %i\n"
-			 "duration: %1.3f\n",
-			 duration.RoundS(),
-			 duration.ToDoubleS());
+		r.Fmt(FMT_STRING("Time: {}\n"
+				 "duration: {:1.3}\n"),
+		      duration.RoundS(),
+		      duration.ToDoubleS());
 }

src/Stats.cxx
@@ -34,6 +34,8 @@
 #include "system/Clock.hxx"
 #endif
 
+#include <fmt/format.h>
+
 #include <chrono>
 
 #ifndef _WIN32
@@ -97,19 +99,19 @@ db_stats_print(Response &r, const Database &db)
 	unsigned total_duration_s =
 		std::chrono::duration_cast<std::chrono::seconds>(stats.total_duration).count();
 
-	r.Format("artists: %u\n"
-		 "albums: %u\n"
-		 "songs: %u\n"
-		 "db_playtime: %u\n",
-		 stats.artist_count,
-		 stats.album_count,
-		 stats.song_count,
-		 total_duration_s);
+	r.Fmt(FMT_STRING("artists: {}\n"
+			 "albums: {}\n"
+			 "songs: {}\n"
+			 "db_playtime: {}\n"),
+	      stats.artist_count,
+	      stats.album_count,
+	      stats.song_count,
+	      total_duration_s);
 
 	const auto update_stamp = db.GetUpdateStamp();
 	if (!IsNegative(update_stamp))
-		r.Format("db_update: %lu\n",
-			 (unsigned long)std::chrono::system_clock::to_time_t(update_stamp));
+		r.Fmt(FMT_STRING("db_update: {}\n"),
+		      std::chrono::system_clock::to_time_t(update_stamp));
 }
 
 #endif
@@ -123,10 +125,10 @@ stats_print(Response &r, const Partition &partition)
 	const auto uptime = std::chrono::steady_clock::now() - start_time;
 #endif
 
-	r.Format("uptime: %u\n"
-		 "playtime: %lu\n",
-		 (unsigned)std::chrono::duration_cast<std::chrono::seconds>(uptime).count(),
-		 lround(partition.pc.GetTotalPlayTime().count()));
+	r.Fmt(FMT_STRING("uptime: {}\n"
+			 "playtime: {}\n"),
+	      std::chrono::duration_cast<std::chrono::seconds>(uptime).count(),
+	      lround(partition.pc.GetTotalPlayTime().count()));
 
 #ifdef ENABLE_DATABASE
 	const Database *db = partition.instance.GetDatabase();

src/TagPrint.cxx
@@ -23,26 +23,27 @@
 #include "client/Response.hxx"
 #include "util/StringView.hxx"
 
+#include <fmt/format.h>
+
 void
 tag_print_types(Response &r) noexcept
 {
 	const auto tag_mask = global_tag_mask & r.GetTagMask();
 	for (unsigned i = 0; i < TAG_NUM_OF_ITEM_TYPES; i++)
 		if (tag_mask.Test(TagType(i)))
-			r.Format("tagtype: %s\n", tag_item_names[i]);
+			r.Fmt(FMT_STRING("tagtype: {}\n"), tag_item_names[i]);
 }
 
 void
 tag_print(Response &r, TagType type, StringView value) noexcept
 {
-	r.Format("%s: %.*s\n", tag_item_names[type],
-		 int(value.size), value.data);
+	r.Fmt(FMT_STRING("{}: {}\n"), tag_item_names[type], value);
 }
 
 void
 tag_print(Response &r, TagType type, const char *value) noexcept
 {
-	r.Format("%s: %s\n", tag_item_names[type], value);
+	r.Fmt(FMT_STRING("{}: {}\n"), tag_item_names[type], value);
 }
 
 void
@@ -58,10 +59,10 @@ void
 tag_print(Response &r, const Tag &tag) noexcept
 {
 	if (!tag.duration.IsNegative())
-		r.Format("Time: %i\n"
-			 "duration: %1.3f\n",
-			 tag.duration.RoundS(),
-			 tag.duration.ToDoubleS());
+		r.Fmt(FMT_STRING("Time: {}\n"
+				 "duration: {:1.3}\n"),
+		      tag.duration.RoundS(),
+		      tag.duration.ToDoubleS());
 
 	tag_print_values(r, tag);
 }

src/TimePrint.cxx
@@ -21,6 +21,8 @@
 #include "client/Response.hxx"
 #include "time/ISO8601.hxx"
 
+#include <fmt/format.h>
+
 void
 time_print(Response &r, const char *name,
 	   std::chrono::system_clock::time_point t)
@@ -33,5 +35,5 @@ time_print(Response &r, const char *name,
 		return;
 	}
 
-	r.Format("%s: %s\n", name, s.c_str());
+	r.Fmt(FMT_STRING("{}: {}\n"), name, s);
 }

src/client/Idle.cxx
@@ -22,6 +22,8 @@
 #include "Response.hxx"
 #include "Idle.hxx"
 
+#include <fmt/format.h>
+
 #include <cassert>
 
 static void
@@ -30,7 +32,7 @@ WriteIdleResponse(Response &r, unsigned flags) noexcept
 	const char *const*idle_names = idle_get_names();
 	for (unsigned i = 0; idle_names[i]; ++i) {
 		if (flags & (1 << i))
-			r.Format("changed: %s\n", idle_names[i]);
+			r.Fmt(FMT_STRING("changed: {}\n"), idle_names[i]);
 	}
 
 	r.Write("OK\n");

commit 679b3bc00f851c1511eed7f944209cf096f87657

output/print, command/player: print bool as integer
Fixes protocol breakage after commit 0440c41cbac0857eaab1220e3b0852c61bfc794a

libfmt is too clever for the MPD protocol!

Closes https://github.com/MusicPlayerDaemon/MPD/issues/1175

src/command/PlayerCommands.cxx
@@ -144,10 +144,10 @@ handle_status(Client &client, [[maybe_unused]] Request args, Response &r)
 			 COMMAND_STATUS_PLAYLIST_LENGTH ": {}\n"
 			 COMMAND_STATUS_MIXRAMPDB ": {}\n"
 			 COMMAND_STATUS_STATE ": {}\n"),
-	      playlist.GetRepeat(),
-	      playlist.GetRandom(),
+	      (unsigned)playlist.GetRepeat(),
+	      (unsigned)playlist.GetRandom(),
 	      SingleToString(playlist.GetSingle()),
-	      playlist.GetConsume(),
+	      (unsigned)playlist.GetConsume(),
 	      partition.name.c_str(),
 	      playlist.GetVersion(),
 	      playlist.GetLength(),

src/output/Print.cxx
@@ -40,7 +40,7 @@ printAudioDevices(Response &r, const MultipleOutputs &outputs)
 				 "outputenabled: {}\n"),
 		      i,
 		      ao.GetName(), ao.GetPluginName(),
-		      ao.IsEnabled());
+		      (unsigned)ao.IsEnabled());
 
 		for (const auto &[attribute, value] : ao.GetAttributes())
 			r.Fmt(FMT_STRING("attribute: {}={}\n"),