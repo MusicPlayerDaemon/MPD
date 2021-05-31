82da57b7cea06d461aa60b322ee217627041613b
Max Kellermann <max@musicpd.org>
2021-05-31 16:49:45 +0200

    decoder/ffmpeg: suppress -Wunused with libavformat<58.6.100

--- src/decoder/plugins/FfmpegDecoderPlugin.cxx
+++ src/decoder/plugins/FfmpegDecoderPlugin.cxx
@@ -470,6 +470,7 @@ IsSeekable(const AVFormatContext &format_context) noexcept
 #if LIBAVFORMAT_VERSION_INT >= AV_VERSION_INT(58, 6, 100)
 	return (format_context.ctx_flags & AVFMTCTX_UNSEEKABLE) != 0;
 #else
+	(void)format_context;
 	return false;
 #endif
 }
