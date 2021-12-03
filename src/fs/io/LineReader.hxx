f9c4d88b12f681ac2eed8fedeb009975067a6f4b
Max Kellermann <max@musicpd.org>
2021-12-03 14:13:17 +0100

    fs/io/TextFile: add interface LineReader

--- src/fs/io/LineReader.hxx
+++ src/fs/io/LineReader.hxx
@@ -17,37 +17,17 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef MPD_TEXT_FILE_HXX
-#define MPD_TEXT_FILE_HXX
+#pragma once
 
-#include "LineReader.hxx"
-#include "config.h"
-
-#include <memory>
-
-class Path;
-class FileReader;
-class AutoGunzipReader;
-class BufferedReader;
-
-class TextFile final : public LineReader {
-	const std::unique_ptr<FileReader> file_reader;
-
-#ifdef ENABLE_ZLIB
-	const std::unique_ptr<AutoGunzipReader> gunzip_reader;
-#endif
-
-	const std::unique_ptr<BufferedReader> buffered_reader;
-
+class LineReader
+{
 public:
-	explicit TextFile(Path path_fs);
-
-	TextFile(const TextFile &other) = delete;
-
-	~TextFile() noexcept;
-
-	/* virtual methods from class LineReader */
-	char *ReadLine() override;
+	/**
+	 * Reads a line from the input file, and strips trailing
+	 * space.  There is a reasonable maximum line length, only to
+	 * prevent denial of service.
+	 *
+	 * @return a pointer to the line, or nullptr on end-of-file
+	 */
+	virtual char *ReadLine() = 0;
 };
-
-#endif
