dda0dfc1408e137e11055fec43a7022a1c028aae
Max Kellermann <max@musicpd.org>
2021-12-08 19:56:20 +0100

    lib/curl/Request: move code from SetupEasy() to Setup.cxx

--- src/lib/curl/Setup.hxx
+++ src/lib/curl/Setup.hxx
@@ -27,103 +27,13 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "Request.hxx"
-#include "Setup.hxx"
-#include "Global.hxx"
-#include "event/Call.hxx"
+#pragma once
 
-#include <curl/curl.h>
+class CurlEasy;
 
-#include <cassert>
-
-CurlRequest::CurlRequest(CurlGlobal &_global, CurlEasy _easy,
-			 CurlResponseHandler &_handler)
-	:global(_global), handler(_handler), easy(std::move(_easy))
-{
-	SetupEasy();
-}
-
-CurlRequest::CurlRequest(CurlGlobal &_global,
-			 CurlResponseHandler &_handler)
-	:global(_global), handler(_handler)
-{
-	SetupEasy();
-}
-
-CurlRequest::~CurlRequest() noexcept
-{
-	FreeEasy();
-}
+namespace Curl {
 
 void
-CurlRequest::SetupEasy()
-{
-	easy.SetPrivate((void *)this);
+Setup(CurlEasy &easy);
 
-	handler.Install(easy);
-
-	Curl::Setup(easy);
-}
-
-void
-CurlRequest::Start()
-{
-	assert(!registered);
-
-	global.Add(*this);
-	registered = true;
-}
-
-void
-CurlRequest::StartIndirect()
-{
-	BlockingCall(global.GetEventLoop(), [this](){
-			Start();
-		});
-}
-
-void
-CurlRequest::Stop() noexcept
-{
-	if (!registered)
-		return;
-
-	global.Remove(*this);
-	registered = false;
-}
-
-void
-CurlRequest::StopIndirect()
-{
-	BlockingCall(global.GetEventLoop(), [this](){
-			Stop();
-		});
-}
-
-void
-CurlRequest::FreeEasy() noexcept
-{
-	if (!easy)
-		return;
-
-	Stop();
-	easy = nullptr;
-}
-
-void
-CurlRequest::Resume() noexcept
-{
-	assert(registered);
-
-	easy.Unpause();
-
-	global.InvalidateSockets();
-}
-
-void
-CurlRequest::Done(CURLcode result) noexcept
-{
-	Stop();
-
-	handler.Done(result);
-}
+} // namespace Curl
