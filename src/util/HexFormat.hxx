7293b32025e2b14fcb2d2db54c472a3a9fa12488
Max Kellermann <max@musicpd.org>
2022-03-14 12:08:06 +0100

    util/HexFormat: faster implementation without snprintf()

--- src/util/HexFormat.hxx
+++ src/util/HexFormat.hxx
@@ -1,5 +1,8 @@
 /*
- * Copyright (C) 2018 Max Kellermann <max.kellermann@gmail.com>
+ * Copyright 2007-2022 CM4all GmbH
+ * All rights reserved.
+ *
+ * author: Max Kellermann <mk@cm4all.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,8 +30,7 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef HEX_FORMAT_HXX
-#define HEX_FORMAT_HXX
+#pragma once
 
 #include "ConstBuffer.hxx"
 #include "StringBuffer.hxx"
@@ -36,16 +38,67 @@
 #include <cstddef>
 #include <cstdint>
 
+constexpr char hex_digits[] = "0123456789abcdef";
+
+[[gnu::always_inline]]
+static constexpr char *
+HexFormatUint8Fixed(char dest[2], uint8_t number) noexcept
+{
+	dest[0] = hex_digits[(number >> 4) & 0xf];
+	dest[1] = hex_digits[number & 0xf];
+	return dest + 2;
+}
+
+[[gnu::always_inline]]
+static constexpr char *
+HexFormatUint16Fixed(char dest[4], uint16_t number) noexcept
+{
+	dest[0] = hex_digits[(number >> 12) & 0xf];
+	dest[1] = hex_digits[(number >> 8) & 0xf];
+	dest[2] = hex_digits[(number >> 4) & 0xf];
+	dest[3] = hex_digits[number & 0xf];
+	return dest + 4;
+}
+
+[[gnu::always_inline]]
+static constexpr char *
+HexFormatUint32Fixed(char dest[8], uint32_t number) noexcept
+{
+	dest[0] = hex_digits[(number >> 28) & 0xf];
+	dest[1] = hex_digits[(number >> 24) & 0xf];
+	dest[2] = hex_digits[(number >> 20) & 0xf];
+	dest[3] = hex_digits[(number >> 16) & 0xf];
+	dest[4] = hex_digits[(number >> 12) & 0xf];
+	dest[5] = hex_digits[(number >> 8) & 0xf];
+	dest[6] = hex_digits[(number >> 4) & 0xf];
+	dest[7] = hex_digits[number & 0xf];
+	return dest + 8;
+}
+
+[[gnu::always_inline]]
+static constexpr char *
+HexFormatUint64Fixed(char dest[16], uint64_t number) noexcept
+{
+	dest = HexFormatUint32Fixed(dest, number >> 32);
+	dest = HexFormatUint32Fixed(dest, number);
+	return dest;
+}
+
 /**
- * Format the given byte sequence into a null-terminated hexadecimal
- * string.
+ * Format the given input buffer of bytes to hex.  The caller ensures
+ * that the output buffer is at least twice as large as the input.
+ * Does not null-terminate the output buffer.
  *
- * @param dest the destination buffer; must be large enough to hold
- * all hex digits plus the null terminator
- * @return a pointer to the generated null terminator
+ * @return a pointer to one after the last written character
  */
-char *
-HexFormat(char *dest, ConstBuffer<uint8_t> src) noexcept;
+constexpr char *
+HexFormat(char *output, ConstBuffer<uint8_t> input) noexcept
+{
+	for (const auto &i : input)
+		output = HexFormatUint8Fixed(output, i);
+
+	return output;
+}
 
 /**
  * Like HexFormat(), but return a #StringBuffer with exactly the
@@ -53,12 +106,10 @@ HexFormat(char *dest, ConstBuffer<uint8_t> src) noexcept;
  */
 template<size_t size>
 [[gnu::pure]]
-auto
+constexpr auto
 HexFormatBuffer(const uint8_t *src) noexcept
 {
 	StringBuffer<size * 2 + 1> dest;
-	HexFormat(dest.data(), {src, size});
+	*HexFormat(dest.data(), {src, size}) = 0;
 	return dest;
 }
-
-#endif
