83572701f45e57610c3fdfb1d30f644b4f78474e
Max Kellermann <max@musicpd.org>
2022-04-26 18:26:04 +0200

    python/build/libs.py: update Boost to 1.79.0

--- python/build/libs.py
+++ python/build/libs.py
@@ -444,7 +444,7 @@ jack = JackProject(
 )
 
 boost = BoostProject(
-    'https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2',
-    '8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc',
+    'https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.tar.bz2',
+    '475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39',
     'include/boost/version.hpp',
 )
