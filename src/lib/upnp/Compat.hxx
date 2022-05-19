8bd1b5228c0b7c708d64b3020767763c56bbf19a
Max Kellermann <max@musicpd.org>
2022-05-19 20:08:29 +0200

    lib/upnp/Compat: suppress -Wunused-but-set-parameter

--- src/lib/upnp/Compat.hxx
+++ src/lib/upnp/Compat.hxx
@@ -24,6 +24,10 @@
 /* libupnp versions until 1.10.1 redefine "bool" and "true" */
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wkeyword-macro"
+
+/* libupnp 1.8.4 uses a flawed kludge to suppress this warning in
+   inline function __list_add_valid() */
+#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
 #endif
 
 #include <upnp.h>
