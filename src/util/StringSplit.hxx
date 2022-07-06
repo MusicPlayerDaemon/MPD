988f5d1b5daaf4ce2a8c3b607529bee58c1cae11
Max Kellermann <mk@cm4all.com>
2022-07-06 11:45:03 +0200

    util/StringSplit: add SplitWhile()

--- src/util/StringSplit.hxx
+++ src/util/StringSplit.hxx
@@ -29,7 +29,9 @@
 
 #pragma once
 
+#include <algorithm>
 #include <string_view>
+#include <type_traits>
 
 template<typename T>
 constexpr std::pair<std::basic_string_view<T>, std::basic_string_view<T>>
@@ -48,6 +50,16 @@ Partition(const std::basic_string_view<T> haystack,
 	  const typename std::basic_string_view<T>::const_pointer position) noexcept
 {
 	return Partition(haystack, position - haystack.data());
+}
+
+template<typename T>
+requires(!std::is_same_v<typename std::basic_string_view<T>::const_pointer,
+	 typename std::basic_string_view<T>::const_iterator>)
+constexpr std::pair<std::basic_string_view<T>, std::basic_string_view<T>>
+Partition(const std::basic_string_view<T> haystack,
+	  const typename std::basic_string_view<T>::const_iterator i) noexcept
+{
+	return Partition(haystack, i - haystack.begin());
 }
 
 template<typename T>
@@ -91,4 +103,17 @@ SplitLast(const std::basic_string_view<T> haystack, const T ch) noexcept
 		return {haystack, {}};
 
 	return PartitionWithout(haystack, i);
+}
+
+/**
+ * Find the first character that does not match the given predicate
+ * and split at this boundary.
+ */
+template<typename T, typename P>
+constexpr std::pair<std::basic_string_view<T>, std::basic_string_view<T>>
+SplitWhile(const std::basic_string_view<T> haystack, P &&predicate) noexcept
+{
+	const auto i = std::find_if_not(haystack.begin(), haystack.end(),
+					std::forward<P>(predicate));
+	return Partition(haystack, i);
 }
