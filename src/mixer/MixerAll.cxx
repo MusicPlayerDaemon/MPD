MusicPlayerDaemon/MPD

commit 35dbc1a90c5110fbf6ab18e98c43463d1b4ea4f3

mixer,output: prevent setting volume before outputs are really enabled
Previous versions of MPD would call SetVolume on enabled outputs before
they are ready, causing all of MPD to crash. Checking the really_enabled
flag prevents this, though it also prevents setting volume before the
player starts.

Before (with the PipeWire output):
  [i] ~$ mpc clear
  volume: 81%   repeat: off   random: off   single: off   consume: off
  [i] ~$ systemctl --user restart mpd.service
  [i] ~$ mpc volume 100
  MPD error: Connection closed by the server
  [i] ~ 1 $

After:
  [i] ~$ # mpd is freshly started w/o anything in the queue
  [i] ~$ mpc
  volume:100%   repeat: off   random: off   single: off   consume: off
  [i] ~$ mpc volume 80
  MPD error: problems setting volume
  [i] ~ 1 $ mpc
  volume:100%   repeat: off   random: off   single: off   consume: off
  [i] ~$

src/mixer/MixerAll.cxx
@@ -84,7 +84,7 @@ output_mixer_set_volume(AudioOutputControl &ao, unsigned volume) noexcept
 
 	/* software mixers are always updated, even if they are
 	   disabled */
-	if (!ao.IsEnabled() && !mixer->IsPlugin(software_mixer_plugin))
+	if (!ao.IsReallyEnabled() && !mixer->IsPlugin(software_mixer_plugin))
 		return false;
 
 	try {

src/output/Control.hxx
@@ -291,6 +291,13 @@ public:
 	/**
 	 * Caller must lock the mutex.
 	 */
+	bool IsReallyEnabled() const noexcept {
+		return really_enabled;
+	}
+
+	/**
+	 * Caller must lock the mutex.
+	 */
 	bool IsEnabled() const noexcept {
 		return enabled;
 	}

commit 47680f936be878281e65bc11834ec90c63bac17d

mixer/All: auto-open "global" mixers
If a mixer is "global", it is available even if the output isn't
open.  However, since the check was changed from IsEnabled() to
IsReallyEnabled(), enabled outputs have not yet been used have not
been "really" enabled yet, preventing using the mixer.

Fixes a regression by commit 35dbc1a90c5110fbf6ab18e98c43463d1b4ea4f3
(part of https://github.com/MusicPlayerDaemon/MPD/pull/1480).

Closes https://github.com/MusicPlayerDaemon/MPD/issues/1563

src/mixer/MixerAll.cxx
@@ -91,7 +91,10 @@ output_mixer_set_volume(AudioOutputControl &ao, unsigned volume)
 
 	/* software mixers are always updated, even if they are
 	   disabled */
-	if (!ao.IsReallyEnabled() && !mixer->IsPlugin(software_mixer_plugin))
+	if (!mixer->IsPlugin(software_mixer_plugin) &&
+	    /* "global" mixers can be used even if the output hasn't
+	       been used yet */
+	    !(mixer->IsGlobal() ? ao.IsEnabled() : ao.IsReallyEnabled()))
 		return SetVolumeResult::DISABLED;
 
 	try {

src/mixer/MixerInternal.hxx
@@ -63,6 +63,10 @@ public:
 		return &plugin == &other;
 	}
 
+	bool IsGlobal() const noexcept {
+		return plugin.global;
+	}
+
 	/**
 	 * Open mixer device
 	 *