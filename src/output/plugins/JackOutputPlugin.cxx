MusicPlayerDaemon/MPD

commit 45071607aa277dbbf3814e9a06c8371be75f27c5

output/Interface: pass std::span to Play()

src/output/plugins/JackOutputPlugin.cxx
@@ -26,6 +26,7 @@
 #include "util/ScopeExit.hxx"
 #include "util/IterableSplitString.hxx"
 #include "util/RuntimeError.hxx"
+#include "util/SpanCast.hxx"
 #include "util/Domain.hxx"
 #include "Log.hxx"
 
@@ -178,7 +179,7 @@ public:
 			: std::chrono::steady_clock::duration::zero();
 	}
 
-	size_t Play(const void *chunk, size_t size) override;
+	std::size_t Play(std::span<const std::byte> src) override;
 
 	void Cancel() noexcept override;
 	bool Pause() override;
@@ -689,14 +690,17 @@ JackOutput::WriteSamples(const float *src, size_t n_frames)
 	return result;
 }
 
-inline size_t
-JackOutput::Play(const void *chunk, size_t size)
+std::size_t
+JackOutput::Play(std::span<const std::byte> _src)
 {
+	const auto src = FromBytesStrict<const float>(_src);
+
 	pause = false;
 
 	const size_t frame_size = audio_format.GetFrameSize();
-	assert(size % frame_size == 0);
-	size /= frame_size;
+	assert(src.size() % frame_size == 0);
+
+	const std::size_t n_frames = src.size() / audio_format.channels;
 
 	while (true) {
 		{
@@ -709,7 +713,7 @@ JackOutput::Play(const void *chunk, size_t size)
 		}
 
 		size_t frames_written =
-			WriteSamples((const float *)chunk, size);
+			WriteSamples(src.data(), n_frames);
 		if (frames_written > 0)
 			return frames_written * frame_size;
 

src/output/Filtered.cxx
@@ -173,10 +173,10 @@ FilteredAudioOutput::SendTag(const Tag &tag)
 	output->SendTag(tag);
 }
 
-size_t
-FilteredAudioOutput::Play(const void *data, size_t size)
+std::size_t
+FilteredAudioOutput::Play(std::span<const std::byte> src)
 {
-	return output->Play(data, size);
+	return output->Play(src);
 }
 
 void

src/output/Filtered.hxx
@@ -23,10 +23,11 @@
 #include "pcm/AudioFormat.hxx"
 #include "filter/Observer.hxx"
 
-#include <memory>
-#include <string>
-#include <map>
 #include <chrono>
+#include <map>
+#include <memory>
+#include <span>
+#include <string>
 
 class FilterFactory;
 class PreparedFilter;
@@ -224,7 +225,7 @@ public:
 
 	void SendTag(const Tag &tag);
 
-	size_t Play(const void *data, size_t size);
+	std::size_t Play(std::span<const std::byte> src);
 
 	void Drain();
 	void Cancel() noexcept;

src/output/Interface.hxx
@@ -21,8 +21,9 @@
 #define MPD_AUDIO_OUTPUT_INTERFACE_HXX
 
 #include <map>
-#include <string>
 #include <chrono>
+#include <span>
+#include <string>
 
 struct AudioFormat;
 struct Tag;
@@ -180,7 +181,7 @@ public:
 	 * @return the number of bytes played (must be a multiple of
 	 * the frame size)
 	 */
-	virtual size_t Play(const void *chunk, size_t size) = 0;
+	virtual std::size_t Play(std::span<const std::byte> src) = 0;
 
 	/**
 	 * Wait until the device has finished playing.

src/output/Thread.cxx
@@ -265,7 +265,7 @@ AudioOutputControl::PlayChunk(std::unique_lock<Mutex> &lock) noexcept
 
 		try {
 			const ScopeUnlock unlock(mutex);
-			nbytes = output->Play(data.data(), data.size());
+			nbytes = output->Play(data);
 			assert(nbytes > 0);
 			assert(nbytes <= data.size());
 		} catch (AudioOutputInterrupted) {
@@ -380,7 +380,7 @@ static void
 PlayFull(FilteredAudioOutput &output, std::span<const std::byte> buffer)
 {
 	while (!buffer.empty()) {
-		size_t nbytes = output.Play(buffer.data(), buffer.size());
+		size_t nbytes = output.Play(buffer);
 		assert(nbytes > 0);
 
 		buffer = buffer.subspan(nbytes);

src/output/plugins/AlsaOutputPlugin.cxx
@@ -283,7 +283,7 @@ private:
 
 	void Interrupt() noexcept override;
 
-	size_t Play(const void *chunk, size_t size) override;
+	std::size_t Play(std::span<const std::byte> src) override;
 	void Drain() override;
 	void Cancel() noexcept override;
 	bool Pause() noexcept override;
@@ -1219,26 +1219,26 @@ AlsaOutput::LockWaitWriteAvailable()
 	}
 }
 
-size_t
-AlsaOutput::Play(const void *chunk, size_t size)
+std::size_t
+AlsaOutput::Play(std::span<const std::byte> src)
 {
-	assert(size > 0);
-	assert(size % in_frame_size == 0);
+	assert(!src.empty());
+	assert(src.size() % in_frame_size == 0);
 
 	const size_t max_frames = LockWaitWriteAvailable();
 	const size_t max_size = max_frames * in_frame_size;
-	if (size > max_size)
-		size = max_size;
+	if (src.size() > max_size)
+		src = src.first(max_size);
 
-	const auto e = pcm_export->Export({(const std::byte *)chunk, size});
+	const auto e = pcm_export->Export(src);
 	if (e.empty())
-		return size;
+		return src.size();
 
 	size_t bytes_written = ring_buffer->push(e.data(), e.size());
 	assert(bytes_written == e.size());
 	(void)bytes_written;
 
-	return size;
+	return src.size();
 }
 
 Event::Duration

src/output/plugins/AoOutputPlugin.cxx
@@ -74,7 +74,7 @@ public:
 	void Open(AudioFormat &audio_format) override;
 	void Close() noexcept override;
 
-	size_t Play(const void *chunk, size_t size) override;
+	std::size_t Play(std::span<const std::byte> src) override;
 };
 
 static constexpr Domain ao_output_domain("ao_output");
@@ -195,23 +195,24 @@ AoOutput::Close() noexcept
 	ao_close(device);
 }
 
-size_t
-AoOutput::Play(const void *chunk, size_t size)
+std::size_t
+AoOutput::Play(std::span<const std::byte> src)
 {
-	assert(size % frame_size == 0);
+	assert(src.size() % frame_size == 0);
 
-	if (size > max_size)
-		size = max_size;
+	if (src.size() > max_size)
+		/* round down to a multiple of the frame size */
+		src = src.first(max_size);
 
 	/* For whatever reason, libao wants a non-const pointer.
 	   Let's hope it does not write to the buffer, and use the
 	   union deconst hack to * work around this API misdesign. */
-	char *data = const_cast<char *>((const char *)chunk);
+	char *data = const_cast<char *>((const char *)src.data());
 
-	if (ao_play(device, data, size) == 0)
+	if (ao_play(device, data, src.size()) == 0)
 		throw MakeAoError();
 
-	return size;
+	return src.size();
 }
 
 const struct AudioOutputPlugin ao_output_plugin = {

src/output/plugins/FifoOutputPlugin.cxx
@@ -69,7 +69,7 @@ private:
 	void Close() noexcept override;
 
 	[[nodiscard]] std::chrono::steady_clock::duration Delay() const noexcept override;
-	size_t Play(const void *chunk, size_t size) override;
+	std::size_t Play(std::span<const std::byte> src) override;
 	void Cancel() noexcept override;
 };
 
@@ -208,17 +208,17 @@ FifoOutput::Delay() const noexcept
 		: std::chrono::steady_clock::duration::zero();
 }
 
-size_t
-FifoOutput::Play(const void *chunk, size_t size)
+std::size_t
+FifoOutput::Play(std::span<const std::byte> src)
 {
 	if (!timer->IsStarted())
 		timer->Start();
-	timer->Add(size);
+	timer->Add(src.size());
 
 	while (true) {
-		ssize_t bytes = write(output, chunk, size);
+		ssize_t bytes = write(output, src.data(), src.size());
 		if (bytes > 0)
-			return (size_t)bytes;
+			return (std::size_t)bytes;
 
 		if (bytes < 0) {
 			switch (errno) {

commit 0279f4fb5778157c25fcbd9189f88b88d8557cad

output/jack: fix bogus assertion failure
Regression from commit 45071607aa277dbbf3814e9a06c8371be75f27c5

Closes https://github.com/MusicPlayerDaemon/MPD/issues/1571

src/output/plugins/JackOutputPlugin.cxx
@@ -693,12 +693,12 @@ JackOutput::WriteSamples(const float *src, size_t n_frames)
 std::size_t
 JackOutput::Play(std::span<const std::byte> _src)
 {
+	const size_t frame_size = audio_format.GetFrameSize();
+	assert(_src.size() % frame_size == 0);
+
 	const auto src = FromBytesStrict<const float>(_src);
 
 	pause = false;
-
-	const size_t frame_size = audio_format.GetFrameSize();
-	assert(src.size() % frame_size == 0);
 
 	const std::size_t n_frames = src.size() / audio_format.channels;
 