db21e20c99c45a4e70ce58b6e30f0cab46bc14e9
Max Kellermann <max.kellermann@gmail.com>
2022-09-28 09:55:34 +0200

    event/Loop: improve API doc wording

--- src/event/Loop.hxx
+++ src/event/Loop.hxx
@@ -96,8 +96,8 @@ class EventLoop final
 	SocketList sockets;
 
 	/**
-	 * A linked list of #SocketEvent instances which have a
-	 * non-zero "ready_flags" field, and need to be dispatched.
+	 * A list of #SocketEvent instances which have a non-zero
+	 * "ready_flags" field, and need to be dispatched.
 	 */
 	SocketList ready_sockets;
 
