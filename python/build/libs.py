568f63100b980cc0c586a946abb08f4f26cb4ded
Max Kellermann <max.kellermann@gmail.com>
2022-10-14 21:53:05 +0200

    python/build/libs.py: update zlib to 1.2.13

--- python/build/libs.py
+++ python/build/libs.py
@@ -57,8 +57,8 @@ flac = AutotoolsProject(
 )
 
 zlib = ZlibProject(
-    'http://zlib.net/zlib-1.2.12.tar.xz',
-    '7db46b8d7726232a621befaab4a1c870f00a90805511c0e0090441dac57def18',
+    'http://zlib.net/zlib-1.2.13.tar.xz',
+    'd14c38e313afc35a9a8760dadf26042f51ea0f5d154b0630a31da0540107fb98',
     'lib/libz.a',
 )
 
