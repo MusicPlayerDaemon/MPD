c549e16ed148cf38ab46cf5363deb3fda2975451
Max Kellermann <max.kellermann@gmail.com>
2022-11-03 10:23:20 +0100

    python/build/libs.py: update CURL to 7.86.0

--- python/build/libs.py
+++ python/build/libs.py
@@ -393,8 +393,8 @@ openssl = OpenSSLProject(
 )
 
 curl = CmakeProject(
-    'https://curl.se/download/curl-7.85.0.tar.xz',
-    '88b54a6d4b9a48cb4d873c7056dcba997ddd5b7be5a2d537a4acb55c20b04be6',
+    'https://curl.se/download/curl-7.86.0.tar.xz',
+    '2d61116e5f485581f6d59865377df4463f2e788677ac43222b496d4e49fb627b',
     'lib/libcurl.a',
     [
         '-DBUILD_CURL_EXE=OFF',
