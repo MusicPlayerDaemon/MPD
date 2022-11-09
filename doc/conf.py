6913148d994f6595e9c5ee67fca3f6fd6b0e46c5
gd <gd@iotide.com>
2022-11-09 09:35:59 +0200

    Docs: changed sphinx theme from classic to sphinx_rtd_theme
    Dependency: pip install sphinx-rtd-theme

--- doc/conf.py
+++ doc/conf.py
@@ -103,14 +103,20 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
-html_theme_options = {"sidebarwidth": "300px"}
+html_theme_options = {
+    'navigation_depth': -1,
+    'sticky_navigation': False,
+    'includehidden': True,
+    'prev_next_buttons_location': 'both',
+    'github_url': "https://github.com/MusicPlayerDaemon/"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
