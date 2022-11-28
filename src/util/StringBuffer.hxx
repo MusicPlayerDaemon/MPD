40d0828ccdc2e86293deee9488e9ef7af0cecd71
Max Kellermann <max.kellermann@gmail.com>
2022-11-28 22:57:59 +0100

    util/StringBuffer: add non-const iterator

--- src/util/StringBuffer.hxx
+++ src/util/StringBuffer.hxx
@@ -51,6 +51,7 @@ protected:
 	Array the_data;
 
 public:
+	using iterator = typename Array::iterator;
 	using const_iterator = typename Array::const_iterator;
 
 	static constexpr size_type capacity() noexcept {
@@ -89,6 +90,14 @@ public:
 	 */
 	reference operator[](size_type i) noexcept {
 		return the_data[i];
+	}
+
+	constexpr iterator begin() noexcept {
+		return the_data.begin();
+	}
+
+	constexpr iterator end() noexcept {
+		return the_data.end();
 	}
 
 	constexpr const_iterator begin() const noexcept {
