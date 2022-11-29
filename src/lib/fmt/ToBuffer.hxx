f62d61f0883951af29f754e36f5cb3bad18c012f
Max Kellermann <max.kellermann@gmail.com>
2022-11-29 06:33:13 +0100

    lib/fmt/ToBuffer: add missing template parameters

--- src/lib/fmt/ToBuffer.hxx
+++ src/lib/fmt/ToBuffer.hxx
@@ -54,11 +54,11 @@ auto
 FmtBuffer(const S &format_str, Args&&... args) noexcept
 {
 #if FMT_VERSION >= 90000
-	return VFmtBuffer(format_str,
-			  fmt::make_format_args(args...));
+	return VFmtBuffer<size>(format_str,
+				fmt::make_format_args(args...));
 #else
-	return VFmtBuffer(fmt::to_string_view(format_str),
-			  fmt::make_args_checked<Args...>(format_str,
-							  args...));
+	return VFmtBuffer<size>(fmt::to_string_view(format_str),
+				fmt::make_args_checked<Args...>(format_str,
+								args...));
 #endif
 }
