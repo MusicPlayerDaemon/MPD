def6fffcf17ac6e5944c6ba5a1622b41db45ff5f
Max Kellermann <max@musicpd.org>
2023-01-29 08:05:40 +0100

    event/Loop: fixup for NO_FINE_TIMER_EVENT

--- src/event/Loop.cxx
+++ src/event/Loop.cxx
@@ -157,6 +157,8 @@ EventLoop::Insert(FineTimerEvent &t) noexcept
 	again = true;
 }
 
+#endif // NO_FINE_TIMER_EVENT
+
 /**
  * Determines which timeout will happen earlier; either one may be
  * negative to specify "no timeout at all".
@@ -168,8 +170,6 @@ GetEarlierTimeout(Event::Duration a, Event::Duration b) noexcept
 		? a
 		: b;
 }
-
-#endif // NO_FINE_TIMER_EVENT
 
 inline Event::Duration
 EventLoop::HandleTimers() noexcept
