f5b22bf2c31fdd2fa4451c1727f03b9157574277
Max Kellermann <max.kellermann@gmail.com>
2023-03-14 20:19:57 +0100

    input/Offset: remove unused macro `PRIoffset`

--- src/input/Offset.hxx
+++ src/input/Offset.hxx
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright The Music Player Daemon Project
 
-#ifndef MPD_OFFSET_HXX
-#define MPD_OFFSET_HXX
+#pragma once
 
 #include <cstdint>
 
@@ -10,11 +9,3 @@
  * A type for absolute offsets in a file.
  */
 typedef uint64_t offset_type;
-
-/**
- * To format an offset_type with printf().  To use this, include
- * <cinttypes>.
- */
-#define PRIoffset PRIu64
-
-#endif
