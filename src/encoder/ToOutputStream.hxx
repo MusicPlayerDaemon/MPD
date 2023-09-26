9a0b3775d8f807696b2361329cbde4f67113a078
Max Kellermann <max.kellermann@gmail.com>
2023-09-26 15:48:26 +0200

    encoder/ToOutputStream: add API docs

--- src/encoder/ToOutputStream.hxx
+++ src/encoder/ToOutputStream.hxx
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright The Music Player Daemon Project
 
-#ifndef MPD_ENCODER_TO_OUTPUT_STREAM_HXX
-#define MPD_ENCODER_TO_OUTPUT_STREAM_HXX
+#pragma once
 
 class OutputStream;
 class Encoder;
 
+/**
+ * Read all available output from the #Encoder and write it to the
+ * #OutputStream.
+ *
+ * Throws on error.
+ */
 void
 EncoderToOutputStream(OutputStream &os, Encoder &encoder);
-
-#endif
