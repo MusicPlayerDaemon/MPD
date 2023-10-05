19c73680cf54999042e6443586aeaae5546c8896
Max Kellermann <max.kellermann@gmail.com>
2023-10-05 17:53:29 +0200

    io/FileReader: use uint_least64_t

--- src/io/FileReader.hxx
+++ src/io/FileReader.hxx
@@ -67,7 +67,7 @@ public:
 	FileInfo GetFileInfo() const;
 
 	[[gnu::pure]]
-	uint64_t GetSize() const noexcept {
+	uint_least64_t GetSize() const noexcept {
 #ifdef _WIN32
 		LARGE_INTEGER size;
 		return GetFileSizeEx(handle, &size)
@@ -79,7 +79,7 @@ public:
 	}
 
 	[[gnu::pure]]
-	uint64_t GetPosition() const noexcept {
+	uint_least64_t GetPosition() const noexcept {
 #ifdef _WIN32
 		LARGE_INTEGER zero;
 		zero.QuadPart = 0;
