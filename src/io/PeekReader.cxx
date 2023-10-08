MusicPlayerDaemon/MPD

commit 7ccc4ddf0de992462de03b10a2450ae7c2cdb588

io/Reader: use std::span

src/io/PeekReader.cxx
@@ -17,8 +17,7 @@ PeekReader::Peek(size_t size)
 	assert(buffer_position == 0);
 
 	do {
-		size_t nbytes = next.Read(buffer + buffer_size,
-					  size - buffer_size);
+		size_t nbytes = next.Read(std::span{buffer}.first(buffer_size).subspan(size));
 		if (nbytes == 0)
 			return nullptr;
 
@@ -29,15 +28,17 @@ PeekReader::Peek(size_t size)
 }
 
 size_t
-PeekReader::Read(void *data, size_t size)
+PeekReader::Read(std::span<std::byte> dest)
 {
-	size_t buffer_remaining = buffer_size - buffer_position;
-	if (buffer_remaining > 0) {
-		size_t nbytes = std::min(buffer_remaining, size);
-		memcpy(data, buffer + buffer_position, nbytes);
-		buffer_position += nbytes;
-		return nbytes;
+	auto src = std::span{buffer}.first(buffer_size).subspan(buffer_position);
+	if (!src.empty()) {
+		if (dest.size() < src.size())
+			src = src.first(dest.size());
+
+		std::copy(src.begin(), src.end(), dest.begin());
+		buffer_position += src.size();
+		return src.size();
 	}
 
-	return next.Read(data, size);
+	return next.Read(dest);
 }

src/decoder/Reader.cxx
@@ -4,8 +4,8 @@
 #include "Reader.hxx"
 #include "DecoderAPI.hxx"
 
-size_t
-DecoderReader::Read(void *data, size_t size)
+std::size_t
+DecoderReader::Read(std::span<std::byte> dest)
 {
-	return decoder_read(client, is, data, size);
+	return decoder_read(client, is, dest.data(), dest.size());
 }

src/decoder/Reader.hxx
@@ -30,7 +30,7 @@ public:
 	}
 
 	/* virtual methods from class Reader */
-	size_t Read(void *data, size_t size) override;
+	std::size_t Read(std::span<std::byte> dest) override;
 };
 
 #endif

src/decoder/plugins/SidplayDecoderPlugin.cxx
@@ -60,7 +60,7 @@ static constexpr unsigned rom_size = 8192;
 static void loadRom(const Path rom_path, uint8_t *dump)
 {
 	FileReader romDump(rom_path);
-	if (romDump.Read(dump, rom_size) != rom_size)
+	if (romDump.Read(std::as_writable_bytes(std::span{dump, rom_size})) != rom_size)
 		throw FmtRuntimeError("Could not load rom dump '{}'", rom_path);
 }
 

src/input/Reader.cxx
@@ -4,10 +4,10 @@
 #include "Reader.hxx"
 #include "InputStream.hxx"
 
-size_t
-InputStreamReader::Read(void *data, size_t size)
+std::size_t
+InputStreamReader::Read(std::span<std::byte> dest)
 {
-	size_t nbytes = is.LockRead(data, size);
+	size_t nbytes = is.LockRead(dest.data(), dest.size());
 	assert(nbytes > 0 || is.IsEOF());
 
 	return nbytes;

src/input/Reader.hxx
@@ -20,7 +20,7 @@ public:
 		:is(_is) {}
 
 	/* virtual methods from class Reader */
-	size_t Read(void *data, size_t size) override;
+	std::size_t Read(std::span<std::byte> dest) override;
 };
 
 #endif

src/input/plugins/FileInputPlugin.cxx
@@ -78,7 +78,7 @@ FileInputStream::Read(std::unique_lock<Mutex> &,
 
 	{
 		const ScopeUnlock unlock(mutex);
-		nbytes = reader.Read(ptr, read_size);
+		nbytes = reader.Read({static_cast<std::byte *>(ptr), read_size});
 	}
 
 	if (nbytes == 0 && !IsEOF())

src/io/BufferedReader.cxx
@@ -26,7 +26,7 @@ BufferedReader::Fill(bool need_more)
 		assert(!w.empty());
 	}
 
-	std::size_t nbytes = reader.Read(w.data(), w.size());
+	std::size_t nbytes = reader.Read(w);
 	if (nbytes == 0) {
 		eof = true;
 		return !need_more;
@@ -95,9 +95,9 @@ BufferedReader::ReadLine()
 	}
 
 	/* terminate the last line */
-	w[0] = 0;
+	w[0] = {};
 
-	char *line = buffer.Read().data();
+	char *line = reinterpret_cast<char *>(buffer.Read().data());
 	buffer.Clear();
 	++line_number;
 	return line;

commit e521b58b01ea973206af81aed041226976885521

io/PeekReader: fix offset/size mixup
Regression by commit 7ccc4ddf0de992462de03b10a2450ae7c2cdb588

src/io/PeekReader.cxx
@@ -17,7 +17,7 @@ PeekReader::Peek(size_t size)
 	assert(buffer_position == 0);
 
 	do {
-		size_t nbytes = next.Read(std::span{buffer}.first(buffer_size).subspan(size));
+		size_t nbytes = next.Read(std::span{buffer}.first(size).subspan(buffer_size));
 		if (nbytes == 0)
 			return nullptr;
 