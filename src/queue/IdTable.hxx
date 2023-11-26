e7b9e8de145b5ced7af62bc458de85ff7906c31c
Max Kellermann <max.kellermann@gmail.com>
2023-11-26 08:42:02 +0100

    queue/IdTable: add API docs

--- src/queue/IdTable.hxx
+++ src/queue/IdTable.hxx
@@ -21,8 +21,16 @@ class IdTable {
 	 */
 	unsigned initialized = 1;
 
+	/**
+	 * An incrementing counter helping GenerateId() to generate
+	 * the next id.
+	 */
 	unsigned next = 1;
 
+	/**
+	 * A lookup table: the index is the id number and the value is
+	 * the position; -1 means this id is unassigned.
+	 */
 	int *const data = new int[size];
 
 public:
