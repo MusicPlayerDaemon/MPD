c4c104442737de2a7f49cfeb8824cc53a3385cd7
Colin Edwards <colin@recursivepenguin.com>
2023-12-21 00:04:17 -0600

    android: Add a text field to display the devices network address

---
+++ android/app/src/main/java/org/musicpd/NetworkUtil.java
@@ -0,0 +1,31 @@
+package org.musicpd;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.LinkAddress;
+import android.net.LinkProperties;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.List;
+
+public class NetworkUtil {
+
+    public static String getDeviceIPV4Address(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        LinkProperties linkProperties = connectivityManager.getLinkProperties(connectivityManager.getActiveNetwork());
+        if (linkProperties != null) {
+            List<LinkAddress> linkAddresses = linkProperties.getLinkAddresses();
+            for (LinkAddress address : linkAddresses) {
+                if (!address.getAddress().isLinkLocalAddress() && !address.getAddress().isLoopbackAddress()) {
+                    InetAddress address1 = address.getAddress();
+                    if (address1 instanceof Inet4Address) {
+                        return address1.getHostAddress();
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
