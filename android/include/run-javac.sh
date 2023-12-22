35c7c005bab26a5bd2c57c3758b19eead9f76b7b
Max Kellermann <max.kellermann@gmail.com>
2023-12-22 16:37:52 +0100

    android/meson.build: move target `org_musicpd_Bridge.h` to include/meson.build

--- android/include/run-javac.sh
+++ android/include/run-javac.sh
@@ -2,11 +2,8 @@
 
 JAVAC=$1
 CLASSPATH=$2
-DIRNAME=$3
+GENINCLUDE=$3
 
-GENINCLUDE="$DIRNAME/include"
-
-mkdir -p "$GENINCLUDE"
 "$JAVAC" -source 1.8 -target 1.8 -Xlint:-options \
 	 -cp "$CLASSPATH" \
 	 -h "$GENINCLUDE"  \
