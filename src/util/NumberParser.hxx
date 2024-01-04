e1eea9d98a8d53d56ce17cb67b320c34b4a8bfc8
Max Kellermann <max.kellermann@gmail.com>
2024-01-04 21:03:25 +0100

    util/NumberParser: new library based on std::from_chars()

---
+++ src/util/NumberParser.hxx
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// author: Max Kellermann <max.kellermann@gmail.com>
+
+#pragma once
+
+#include <charconv>
+#include <concepts>
+#include <optional>
+#include <string_view>
+
+template<std::integral T>
+[[gnu::pure]]
+std::optional<T>
+ParseInteger(const char *first, const char *last, int base=10) noexcept
+{
+	T value;
+	auto [ptr, ec] = std::from_chars(first, last, value, base);
+	if (ptr == last && ec == std::errc{})
+		return value;
+	else
+		return std::nullopt;
+}
+
+template<std::integral T>
+[[gnu::pure]]
+std::optional<T>
+ParseInteger(std::string_view src, int base=10) noexcept
+{
+	return ParseInteger<T>(src.data(), src.data() + src.size(), base);
+}
