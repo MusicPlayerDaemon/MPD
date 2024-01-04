5d122c3bc83b3a17d52ccc01e29e542316d08c33
Colin Edwards <colin@recursivepenguin.com>
2024-01-04 17:36:58 -0600

    android: Add dependencies and new application class for dagger / hilt support
    Dagger and hilt give us dependency injection which makes it easier to split up parts of the app. This lets us easily split out things like logging and paves the way to migrate off preferences to DataStore
    
    This also remove the process name on the service to pull eveything into one process so we don't have to do IPC to pass logs around. This lets us use the same instances of injected classes between the UI and the service side.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -24,7 +26,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
 
     buildTypes {
@@ -63,6 +65,10 @@ dependencies {
     implementation("com.github.alorma:compose-settings-ui-m3:1.0.3")
     implementation("com.github.alorma:compose-settings-storage-preferences:1.0.3")
     implementation("com.google.accompanist:accompanist-permissions:0.33.2-alpha")
+
+    implementation("com.google.dagger:hilt-android:2.49")
+    ksp("com.google.dagger:dagger-compiler:2.49")
+    ksp("com.google.dagger:hilt-compiler:2.49")
 
     // Android Studio Preview support
     implementation("androidx.compose.ui:ui-tooling-preview")
