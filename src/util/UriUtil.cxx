MusicPlayerDaemon/MPD

commit 49ed9dae34f29f499d193490af2c8a869d170e8e

util/UriUtil: New uri_squash_dot_segments.

src/util/UriUtil.cxx
@@ -29,9 +29,12 @@
 
 #include "UriUtil.hxx"
 #include "ASCII.hxx"
+#include "SplitString.hxx"
 
 #include <cassert>
 #include <cstring>
+
+#include <string_view>
 
 static const char *
 verify_uri_segment(const char *p) noexcept
@@ -106,5 +109,39 @@ uri_remove_auth(const char *uri) noexcept
 	   information */
 	std::string result(uri);
 	result.erase(auth - uri, at + 1 - auth);
+	return result;
+}
+
+std::string
+uri_squash_dot_segments(const char *uri) noexcept
+{
+	std::forward_list<std::string_view> path = SplitString(std::string_view(uri), '/');
+	path.remove_if([](const std::string_view &seg) { return seg == "."; });
+	path.reverse();
+
+	std::string result;
+
+	int segskips = 0;
+	auto it = path.begin();
+	while (it != path.end()) {
+		if (*it == "..") {
+			segskips++;
+			it++;
+			continue;
+		} else if (segskips != 0) {
+			segskips--;
+			it++;
+			continue;
+		}
+
+		result.insert(0, *it);
+
+		if (it != path.begin()) {
+			result.insert(it->length(), "/");
+		}
+
+		it++;
+	}
+
 	return result;
 }

src/util/UriUtil.hxx
@@ -56,4 +56,12 @@ gcc_pure
 std::string
 uri_remove_auth(const char *uri) noexcept;
 
+/**
+ * Remove dot segments in the URI.  For example, uri_squash_dot_segments
+ * ("foo/bar/.././")=="foo/".
+ */
+gcc_pure
+std::string
+uri_squash_dot_segments(const char *uri) noexcept;
+
 #endif

commit b6afdf12013a0846f7b7bbff659a6f0046862674

util/UriUtil: disable path segment stripping
This bug introduced by commit 49ed9dae34f29f499d193490af2c8a869d170e8e
and activated by commit acc1bd6297ed4550f2e567aa2d2d27ffc6784629
caused leading spaces to disappear from the beginning of all file
names.

Closes https://github.com/MusicPlayerDaemon/MPD/issues/1961

src/util/UriUtil.cxx
@@ -90,7 +90,7 @@ uri_remove_auth(const char *uri) noexcept
 std::string
 uri_squash_dot_segments(const char *uri) noexcept
 {
-	std::forward_list<std::string_view> path = SplitString(std::string_view(uri), '/');
+	std::forward_list<std::string_view> path = SplitString(std::string_view(uri), '/', false);
 	path.remove_if([](const std::string_view &seg) { return seg == "."; });
 	path.reverse();
 