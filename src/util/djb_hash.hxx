9e3a66d5bf58d01653b57189f86bcf68988a4786
Max Kellermann <mk@cm4all.com>
2024-05-06 23:04:41 +0200

    util/djb_hash: make inline and constexpr
    Allows calculating hashes at compile time.

--- src/util/djb_hash.hxx
+++ src/util/djb_hash.hxx
@@ -14,12 +14,35 @@
 
 static constexpr std::size_t DJB_HASH_INIT = 5381;
 
-[[gnu::pure]]
-std::size_t
-djb_hash(std::span<const std::byte> src,
-	 std::size_t init=DJB_HASH_INIT) noexcept;
+[[nodiscard]] [[gnu::always_inline]] [[gnu::hot]]
+constexpr std::size_t
+djb_hash_update(std::size_t hash, std::byte b) noexcept
+{
+	return (hash * 33) ^ static_cast<std::size_t>(b);
+}
 
-[[gnu::pure]]
-std::size_t
+[[nodiscard]] [[gnu::hot]]
+constexpr std::size_t
+djb_hash(std::span<const std::byte> src,
+	 std::size_t init=DJB_HASH_INIT) noexcept
+{
+	std::size_t hash = init;
+
+	for (const auto i : src)
+		hash = djb_hash_update(hash, i);
+
+	return hash;
+}
+
+[[nodiscard]] [[gnu::hot]]
+constexpr std::size_t
 djb_hash_string(const char *p,
-		std::size_t init=DJB_HASH_INIT) noexcept;
+		std::size_t init=DJB_HASH_INIT) noexcept
+{
+	std::size_t hash = init;
+
+	while (*p != 0)
+		hash = djb_hash_update(hash, static_cast<std::byte>(*p++));
+
+	return hash;
+}
