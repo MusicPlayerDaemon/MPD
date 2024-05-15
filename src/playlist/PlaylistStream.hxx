eceead671e5a5a735de009beacae6b1b55daffd4
Max Kellermann <max.kellermann@gmail.com>
2024-05-15 19:59:09 +0200

    playlist/Stream: add API documentation

--- src/playlist/PlaylistStream.hxx
+++ src/playlist/PlaylistStream.hxx
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright The Music Player Daemon Project
 
-#ifndef MPD_PLAYLIST_STREAM_HXX
-#define MPD_PLAYLIST_STREAM_HXX
+#pragma once
 
 #include "thread/Mutex.hxx"
 
@@ -20,8 +19,12 @@ class Path;
 std::unique_ptr<SongEnumerator>
 playlist_open_path(Path path, Mutex &mutex);
 
+/**
+ * Opens a playlist from a remote file.
+ *
+ * @param uri the absolute URI of the playlist file
+ * @return a playlist, or nullptr on error
+ */
 [[gnu::nonnull]]
 std::unique_ptr<SongEnumerator>
 playlist_open_remote(const char *uri, Mutex &mutex);
-
-#endif
