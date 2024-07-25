MusicPlayerDaemon/MPD

commit 3558317dc9c090a3c779bd7ec0a10dffc4a79814

event/MultiSocketMonitor: remove force-refresh on every iteration
libasound guarantees that the file descriptors never change for a
"prepared" PCM device, thus we can omit this defensive kludge.

src/event/MultiSocketMonitor.cxx
@@ -120,10 +120,6 @@ MultiSocketMonitor::OnIdle() noexcept
 	if (ready) {
 		ready = false;
 		DispatchSockets();
-
-		/* TODO: don't refresh always; require users to call
-		   InvalidateSockets() */
-		refresh = true;
 	}
 
 	if (refresh) {

commit bbbbf5f4bde1403b41785892de1eb08fed7ad65e

event/MultiSocketMonitor: reschedule epoll/EPERM workaround forever
Fixes a regression by commit 3558317dc9c which caused the ALSA null
plugin to cease to work because the workaround timer was never rescheduled.

src/event/MultiSocketMonitor.cxx
@@ -103,7 +103,7 @@ MultiSocketMonitor::Prepare() noexcept
 		constexpr Event::Duration ready_timeout =
 			std::chrono::milliseconds(1);
 		if (timeout < timeout.zero() || timeout > ready_timeout)
-			timeout = ready_timeout;
+			timeout = always_ready_timeout = ready_timeout;
 	}
 #endif
 
@@ -120,6 +120,11 @@ MultiSocketMonitor::OnIdle() noexcept
 	if (ready) {
 		ready = false;
 		DispatchSockets();
+
+#ifdef USE_EPOLL
+		if (!refresh && !always_ready_fds.empty())
+			timeout_event.Schedule(always_ready_timeout);
+#endif
 	}
 
 	if (refresh) {

src/event/MultiSocketMonitor.hxx
@@ -109,6 +109,8 @@ class MultiSocketMonitor
 	 * epoll (epoll_ctl() returns -EPERM).
 	 */
 	std::forward_list<AlwaysReady> always_ready_fds;
+
+	Event::Duration always_ready_timeout;
 #endif
 
 protected: