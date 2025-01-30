d4faaed512b640780b39bca522600cf6ab8688a1
Camille Scholtz <admin@camille.sh>
2025-01-30 16:37:01 +0100

    Revert "remove macOS support"
    This reverts commit 518ce0187a29bea666b9abf71788625262d015be.

---
+++ src/mixer/plugins/OSXMixerPlugin.hxx
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright The Music Player Daemon Project
+
+#pragma once
+
+struct MixerPlugin;
+
+extern const MixerPlugin osx_mixer_plugin;
--- src/zeroconf/Glue.hxx
+++ src/zeroconf/Glue.hxx
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright The Music Player Daemon Project
 
-#pragma once
+#ifndef MPD_ZEROCONF_GLUE_HXX
+#define MPD_ZEROCONF_GLUE_HXX
 
 #include "Helper.hxx"
+#include "config.h"
 
 #include <memory>
 
@@ -11,8 +13,14 @@ struct ConfigData;
 class EventLoop;
 class ZeroconfHelper;
 
+#ifdef HAVE_ZEROCONF
+
 /**
  * Throws on error.
  */
 std::unique_ptr<ZeroconfHelper>
 ZeroconfInit(const ConfigData &config, EventLoop &loop);
+
+#endif /* ! HAVE_ZEROCONF */
+
+#endif
