MusicPlayerDaemon/MPD

commit 0cdc3d160f380b98e0add5bef649f57e56a11448

apple/AppleMain: remove unused includes

src/apple/AppleMain.cxx
@@ -2,9 +2,7 @@
 // Copyright The Music Player Daemon Project
 
 #include "Main.hxx"
-#include "Instance.hxx"
 #include "CommandLine.hxx"
-#include "net/Init.hxx"
 #include "config/Data.hxx"
 
 int apple_main(int argc, char *argv[])

commit 50637ea1d0b609f6961d6794d70ecd163fba195a

apple/AppleMain: add missing include for fork()
This fixes a regression by commit 0cdc3d160f380; that commit was godd,
but fork() was only available through indirect includes.

src/apple/AppleMain.cxx
@@ -5,6 +5,8 @@
 #include "CommandLine.hxx"
 #include "config/Data.hxx"
 
+#include <unistd.h> // for fork()
+
 int apple_main(int argc, char *argv[])
 {
 #ifdef ENABLE_DAEMON