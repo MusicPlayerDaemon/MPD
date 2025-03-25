MusicPlayerDaemon/MPD

commit 0cdc3d160f380b98e0add5bef649f57e56a11448

apple/AppleMain: remove unused includes

src/apple/AppleMain.cxx
@@ -2,9 +2,7 @@
 // Copyright The Music Player Daemon Project
 
 #include "Main.hxx"
-#include "Instance.hxx"
 #include "CommandLine.hxx"
-#include "net/Init.hxx"
 #include "config/Data.hxx"
 
 int apple_main(int argc, char *argv[])

commit e4990afcf168d61862b56bcfbd07cc2b5d245ae6

apple/AppleMain: add missing include for MakeErrno()
Another indirect regression by commit 0cdc3d160f380

src/apple/AppleMain.cxx
@@ -4,6 +4,7 @@
 #include "Main.hxx"
 #include "CommandLine.hxx"
 #include "config/Data.hxx"
+#include "system/Error.hxx"
 
 #include <unistd.h> // for fork()
 