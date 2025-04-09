MusicPlayerDaemon/MPD

commit 7e14f8f8300f059288cf388f21d8e708c6645756

encoder/Interface: pass std::span to Write() and Read()

src/output/plugins/ShoutOutputPlugin.cxx
@@ -326,12 +326,14 @@ static void
 EncoderToShout(shout_t *shout_conn, Encoder &encoder)
 {
 	while (true) {
-		uint8_t buffer[32768];
-		size_t nbytes = encoder.Read(buffer, sizeof(buffer));
-		if (nbytes == 0)
+		std::byte buffer[32768];
+		const auto e = encoder.Read(std::span{buffer});
+		if (e.empty() == 0)
 			return;
 
-		int err = shout_send(shout_conn, buffer, nbytes);
+		int err = shout_send(shout_conn,
+				     (const unsigned char *)e.data(),
+				     e.size());
 		HandleShoutError(shout_conn, err);
 	}
 }
@@ -414,7 +416,7 @@ ShoutOutput::Delay() const noexcept
 size_t
 ShoutOutput::Play(const void *chunk, size_t size)
 {
-	encoder->Write(chunk, size);
+	encoder->Write({(const std::byte *)chunk, size});
 	WritePage();
 	return size;
 }
@@ -422,9 +424,9 @@ ShoutOutput::Play(const void *chunk, size_t size)
 bool
 ShoutOutput::Pause()
 {
-	static char silence[1020];
+	static std::byte silence[1020];
 
-	encoder->Write(silence, sizeof(silence));
+	encoder->Write(std::span{silence});
 	WritePage();
 
 	return true;

src/encoder/EncoderInterface.hxx
@@ -21,6 +21,7 @@
 #define MPD_ENCODER_INTERFACE_HXX
 
 #include <cstddef>
+#include <span>
 
 struct AudioFormat;
 struct Tag;
@@ -91,18 +92,19 @@ public:
 	 * @param data the buffer containing PCM samples
 	 * @param length the length of the buffer in bytes
 	 */
-	virtual void Write(const void *data, std::size_t length) = 0;
+	virtual void Write(std::span<const std::byte> src) = 0;
 
 	/**
 	 * Reads encoded data from the encoder.
 	 *
 	 * Call this repeatedly until no more data is returned.
 	 *
-	 * @param dest the destination buffer to copy to
-	 * @param length the maximum length of the destination buffer
-	 * @return the number of bytes written to #dest
+	 * @param buffer a buffer that can be used to write data into
+	 *
+	 * @return the portion of the buffer that was filled (but may
+	 *  also point to a different buffer, e.g. one owned by this object)
 	 */
-	virtual std::size_t Read(void *dest, std::size_t length) noexcept = 0;
+	virtual std::span<const std::byte> Read(std::span<std::byte> buffer) noexcept = 0;
 };
 
 class PreparedEncoder {

src/encoder/ToOutputStream.cxx
@@ -28,12 +28,12 @@ EncoderToOutputStream(OutputStream &os, Encoder &encoder)
 		/* read from the encoder */
 
 		std::byte buffer[32768];
-		size_t nbytes = encoder.Read(buffer, sizeof(buffer));
-		if (nbytes == 0)
+		const auto r = encoder.Read(std::span{buffer});
+		if (r.empty())
 			return;
 
 		/* write everything to the stream */
 
-		os.Write(buffer, nbytes);
+		os.Write(r.data(), r.size());
 	}
 }

src/encoder/plugins/FlacEncoderPlugin.cxx
@@ -71,10 +71,10 @@ public:
 
 	void SendTag(const Tag &tag) override;
 
-	void Write(const void *data, size_t length) override;
+	void Write(std::span<const std::byte> src) override;
 
-	size_t Read(void *dest, size_t length) noexcept override {
-		return output_buffer.Read((std::byte *)dest, length);
+	std::span<const std::byte> Read(std::span<std::byte> buffer) noexcept override {
+		return buffer.first(output_buffer.Read(buffer.data(), buffer.size()));
 	}
 
 private:
@@ -267,27 +267,27 @@ pcm16_to_flac(int32_t *out, const int16_t *in, std::size_t num_samples) noexcept
 }
 
 void
-FlacEncoder::Write(const void *data, size_t length)
+FlacEncoder::Write(std::span<const std::byte> src)
 {
 	void *exbuffer;
 	const void *buffer = nullptr;
 
 	/* format conversion */
 
-	const std::size_t num_frames = length / audio_format.GetFrameSize();
+	const std::size_t num_frames = src.size() / audio_format.GetFrameSize();
 	const std::size_t num_samples = num_frames * audio_format.channels;
 
 	switch (audio_format.format) {
 	case SampleFormat::S8:
-		exbuffer = expand_buffer.Get(length * 4);
-		pcm8_to_flac((int32_t *)exbuffer, (const int8_t *)data,
+		exbuffer = expand_buffer.Get(src.size() * 4);
+		pcm8_to_flac((int32_t *)exbuffer, (const int8_t *)src.data(),
 			     num_samples);
 		buffer = exbuffer;
 		break;
 
 	case SampleFormat::S16:
-		exbuffer = expand_buffer.Get(length * 2);
-		pcm16_to_flac((int32_t *)exbuffer, (const int16_t *)data,
+		exbuffer = expand_buffer.Get(src.size() * 2);
+		pcm16_to_flac((int32_t *)exbuffer, (const int16_t *)src.data(),
 			      num_samples);
 		buffer = exbuffer;
 		break;
@@ -296,7 +296,7 @@ FlacEncoder::Write(const void *data, size_t length)
 	case SampleFormat::S32:
 		/* nothing need to be done; format is the same for
 		   both mpd and libFLAC */
-		buffer = data;
+		buffer = src.data();
 		break;
 
 	default:

src/encoder/plugins/LameEncoderPlugin.cxx
@@ -23,6 +23,7 @@
 #include "util/NumberParser.hxx"
 #include "util/ReusableArray.hxx"
 #include "util/RuntimeError.hxx"
+#include "util/SpanCast.hxx"
 
 #include <lame/lame.h>
 
@@ -167,14 +168,14 @@ LameEncoder::~LameEncoder() noexcept
 }
 
 void
-LameEncoder::Write(std::span<const std::byte> src)
+LameEncoder::Write(std::span<const std::byte> _src)
 {
-	const auto *src = (const int16_t*)data;
+	const auto src = FromBytesStrict<const int16_t>(_src);
 
 	assert(output_begin == output_end);
 
-	const std::size_t num_frames = length / audio_format.GetFrameSize();
-	const std::size_t num_samples = length / audio_format.GetSampleSize();
+	const std::size_t num_samples = src.size();
+	const std::size_t num_frames = num_samples / audio_format.channels;
 
 	/* worst-case formula according to LAME documentation */
 	const std::size_t output_buffer_size = 5 * num_samples / 4 + 7200;
@@ -183,7 +184,7 @@ LameEncoder::Write(std::span<const std::byte> src)
 	/* this is for only 16-bit audio */
 
 	int bytes_out = lame_encode_buffer_interleaved(gfp,
-						       const_cast<short *>(src),
+						       const_cast<short *>(src.data()),
 						       num_frames,
 						       dest, output_buffer_size);
 
@@ -194,19 +195,18 @@ LameEncoder::Write(std::span<const std::byte> src)
 	output_end = dest + bytes_out;
 }
 
-size_t
-LameEncoder::Read(void *dest, size_t length) noexcept
+std::span<const std::byte>
+LameEncoder::Read(std::span<std::byte> buffer) noexcept
 {
 	const auto begin = output_begin;
 	assert(begin <= output_end);
 	const std::size_t remainning = output_end - begin;
-	if (length > remainning)
-		length = remainning;
+	const std::size_t nbytes = std::min(remainning, buffer.size());
 
-	memcpy(dest, begin, length);
+	memcpy(buffer.data(), begin, nbytes);
 
-	output_begin = begin + length;
-	return length;
+	output_begin = begin + nbytes;
+	return buffer.first(nbytes);
 }
 
 const EncoderPlugin lame_encoder_plugin = {

src/encoder/plugins/NullEncoderPlugin.cxx
@@ -29,12 +29,12 @@ public:
 		:Encoder(false) {}
 
 	/* virtual methods from class Encoder */
-	void Write(const void *data, size_t length) override {
-		buffer.Append({(const std::byte *)data, length});
+	void Write(std::span<const std::byte> src) override {
+		buffer.Append(src);
 	}
 
-	size_t Read(void *dest, size_t length) noexcept override {
-		return buffer.Read((std::byte *)dest, length);
+	std::span<const std::byte> Read(std::span<std::byte> b) noexcept override {
+		return b.first(buffer.Read(b.data(), b.size()));
 	}
 };
 

src/encoder/plugins/OggEncoder.hxx
@@ -50,7 +50,7 @@ public:
 		flush = true;
 	}
 
-	size_t Read(void *dest, size_t length) noexcept override {
+	std::span<const std::byte> Read(std::span<std::byte> buffer) noexcept override {
 		ogg_page page;
 		bool success = stream.PageOut(page);
 		if (!success) {
@@ -60,10 +60,11 @@ public:
 			}
 
 			if (!success)
-				return 0;
+				return {};
 		}
 
-		return ReadPage(page, dest, length);
+		return buffer.first(ReadPage(page, buffer.data(),
+					     buffer.size()));
 	}
 };
 

src/encoder/plugins/OpusEncoderPlugin.cxx
@@ -61,7 +61,7 @@ public:
 
 	/* virtual methods from class Encoder */
 	void End() override;
-	void Write(const void *data, size_t length) override;
+	void Write(std::span<const std::byte> src) override;
 
 	void PreTag() override;
 	void SendTag(const Tag &tag) override;
@@ -272,10 +272,8 @@ OpusEncoder::WriteSilence(unsigned fill_frames)
 }
 
 void
-OpusEncoder::Write(const void *_data, size_t length)
+OpusEncoder::Write(std::span<const std::byte> src)
 {
-	const auto *data = (const std::byte *)_data;
-
 	if (lookahead > 0) {
 		/* generate some silence at the beginning of the
 		   stream */
@@ -286,14 +284,12 @@ OpusEncoder::Write(const void *_data, size_t length)
 		lookahead = 0;
 	}
 
-	while (length > 0) {
-		size_t nbytes = buffer_size - buffer_position;
-		if (nbytes > length)
-			nbytes = length;
+	while (!src.empty()) {
+		const std::size_t nbytes = std::min(buffer_size - buffer_position,
+						    src.size());
 
-		memcpy(buffer + buffer_position, data, nbytes);
-		data += nbytes;
-		length -= nbytes;
+		memcpy(buffer + buffer_position, src.data(), nbytes);
+		src = src.subspan(nbytes);
 		buffer_position += nbytes;
 
 		if (buffer_position == buffer_size)

commit 96992617dddaf2b16efa1efa0ff65a1caa16a8f0

Fix hang on activating shout output (std::span.empty() returns true or 1 on empty span)
[mk: regression from commit 7e14f8f8300f059288cf388f21d8e708c6645756]

src/output/plugins/ShoutOutputPlugin.cxx
@@ -311,7 +311,7 @@ EncoderToShout(shout_t *shout_conn, Encoder &encoder)
 	while (true) {
 		std::byte buffer[32768];
 		const auto e = encoder.Read(std::span{buffer});
-		if (e.empty() == 0)
+		if (e.empty())
 			return;
 
 		int err = shout_send(shout_conn,