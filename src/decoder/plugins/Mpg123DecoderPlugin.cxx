cbb43f09f58c4ebb1167e825cd5838831e5a2677
Max Kellermann <max.kellermann@gmail.com>
2025-04-14 09:08:48 +0200

    decoder/mpg123: fix indent

--- src/decoder/plugins/Mpg123DecoderPlugin.cxx
+++ src/decoder/plugins/Mpg123DecoderPlugin.cxx
@@ -365,7 +365,7 @@ mpd_mpg123_stream_decode(DecoderClient &client, InputStream &is)
 	mpd_mpg123_open_stream(*handle, iohandle);
 
 	if (is.KnownSize())
-	    mpg123_set_filesize(handle, is.GetSize());
+		mpg123_set_filesize(handle, is.GetSize());
 
 	Decode(client, *handle, is.IsSeekable());
 }
