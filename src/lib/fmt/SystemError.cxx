cd2a58d607e9837477910d8cec0d12ee2db1ad8d
Max Kellermann <max.kellermann@gmail.com>
2025-07-31 21:19:46 +0200

    lib/fmt/SystemError: remove unnecessary FormatMessageA() suffix
    This causes the Windows error message to occur twice. And it causes
    libfmt to crash if CP_ACP is not ASCII-compatible (e.g. codepage-932 /
    shift_jis) due to our use of FormatMessageA(). See also
    https://github.com/MusicPlayerDaemon/MPD/pull/2305 for a discussion.
    
    Closes https://github.com/MusicPlayerDaemon/MPD/issues/2302

--- src/lib/fmt/SystemError.cxx
+++ src/lib/fmt/SystemError.cxx
@@ -14,30 +14,12 @@ VFmtSystemError(std::error_code code,
 
 #ifdef _WIN32
 
-#include <array>
-
-#include <windef.h> // for HWND (needed by winbase.h)
-#include <winbase.h> // for FormatMessageA()
-
 std::system_error
 VFmtLastError(DWORD code,
 	      fmt::string_view format_str, fmt::format_args args) noexcept
 {
-	std::array<char, 512> buffer;
-	const auto end = buffer.data() + buffer.size();
-
-	constexpr std::size_t max_prefix = sizeof(buffer) - 128;
-	auto [p, _] = fmt::vformat_to_n(buffer.data(),
-					buffer.size() - max_prefix,
-					format_str, args);
-	*p++ = ':';
-	*p++ = ' ';
-
-	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM |
-		       FORMAT_MESSAGE_IGNORE_INSERTS,
-		       nullptr, code, 0, p, end - p, nullptr);
-
-	return MakeLastError(code, buffer.data());
+	const auto msg = VFmtBuffer<512>(format_str, args);
+	return MakeLastError(code, msg);
 }
 
 #endif // _WIN32
