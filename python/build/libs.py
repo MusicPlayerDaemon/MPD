7e77add530ed304ab461dea40b2228500b344825
Max Kellermann <max.kellermann@gmail.com>
2026-01-03 14:24:23 +0100

    python/build/libs.py: update FFmpeg to 8.0.1

--- python/build/libs.py
+++ python/build/libs.py
@@ -79,8 +79,8 @@ gme = CmakeProject(
 )
 
 ffmpeg = FfmpegProject(
-    'http://ffmpeg.org/releases/ffmpeg-8.0.tar.xz',
-    'b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e',
+    'http://ffmpeg.org/releases/ffmpeg-8.0.1.tar.xz',
+    '05ee0b03119b45c0bdb4df654b96802e909e0a752f72e4fe3794f487229e5a41',
     'lib/libavcodec.a',
     [
         '--disable-shared', '--enable-static',
