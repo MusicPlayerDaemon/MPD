MusicPlayerDaemon/MPD

commit 0b5315b9e5a42cb0e88bf46a7579bb5641543f60

LocateUri: validate relative URIs with uri_safe_local()
This fixes an ugly path traversal bug that allowed clients to do
"listfiles ..".

Closes https://github.com/MusicPlayerDaemon/MPD/issues/2484

src/LocateUri.cxx
@@ -9,6 +9,7 @@
 #include "storage/Registry.hxx"
 #include "util/ASCII.hxx"
 #include "util/UriExtract.hxx"
+#include "util/UriUtil.hxx"
 
 #ifdef ENABLE_DATABASE
 #include "storage/StorageInterface.hxx"
@@ -115,6 +116,10 @@ LocateUri(UriPluginKind kind,
 					 , storage
 #endif
 					 );
-	else
+	else {
+		if (!uri_safe_local(uri))
+			throw std::invalid_argument{"Bad relative path"};
+
 		return LocatedUri(LocatedUri::Type::RELATIVE, uri);
+	}
 }

commit 3d832e087fb3aead617b12e3e3c14e29668d24df

LocateUri: allow empty string
An empty string is indeed a valid URI.

Fixes a regression cause by commit
0b5315b9e5a42cb0e88bf46a7579bb5641543f60

Closes https://github.com/MusicPlayerDaemon/MPD/issues/2488

src/LocateUri.cxx
@@ -8,6 +8,7 @@
 #include "ls.hxx"
 #include "storage/Registry.hxx"
 #include "util/ASCII.hxx"
+#include "util/StringCompare.hxx"
 #include "util/UriExtract.hxx"
 #include "util/UriUtil.hxx"
 
@@ -87,11 +88,11 @@ LocateAbsoluteUri(UriPluginKind kind, const char *uri
 
 LocatedUri
 LocateUri(UriPluginKind kind,
-	  const char *uri, const IClient *client
+	  const char *uri, const IClient *client,
 #ifdef ENABLE_DATABASE
-	  , const Storage *storage
+	  const Storage *storage,
 #endif
-	  )
+	  bool allow_empty)
 {
 	/* skip the obsolete "file://" prefix */
 	const char *path_utf8 = StringAfterPrefixCaseASCII(uri, "file://");
@@ -116,6 +117,8 @@ LocateUri(UriPluginKind kind,
 					 , storage
 #endif
 					 );
+	else if (allow_empty && StringIsEmpty(uri))
+		return LocatedUri(LocatedUri::Type::RELATIVE, uri);
 	else {
 		if (!uri_safe_local(uri))
 			throw std::invalid_argument{"Bad relative path"};

src/LocateUri.hxx
@@ -71,13 +71,14 @@ struct LocatedUri {
  * @param storage a #Storage instance which may be used to convert
  * absolute URIs to relative ones, using Storage::MapToRelativeUTF8();
  * that feature is disabled if this parameter is nullptr
+ * @param allow_empty is an empty URI allowed in this context?
  */
 LocatedUri
 LocateUri(UriPluginKind kind,
-	  const char *uri, const IClient *client
+	  const char *uri, const IClient *client,
 #ifdef ENABLE_DATABASE
-	  , const Storage *storage
+	  const Storage *storage,
 #endif
-	  );
+	  bool allow_empty=false);
 
 #endif

src/command/OtherCommands.cxx
@@ -83,11 +83,11 @@ handle_listfiles(Client &client, Request args, Response &r)
 	/* default is root directory */
 	const auto uri = args.GetOptional(0, "");
 
-	const auto located_uri = LocateUri(UriPluginKind::STORAGE, uri, &client
+	const auto located_uri = LocateUri(UriPluginKind::STORAGE, uri, &client,
 #ifdef ENABLE_DATABASE
-					   , nullptr
+					   nullptr,
 #endif
-					   );
+					   true);
 
 	switch (located_uri.type) {
 	case LocatedUri::Type::ABSOLUTE:
@@ -203,11 +203,11 @@ handle_lsinfo(Client &client, Request args, Response &r)
 		   compatibility, work around this here */
 		uri = "";
 
-	const auto located_uri = LocateUri(UriPluginKind::INPUT, uri, &client
+	const auto located_uri = LocateUri(UriPluginKind::INPUT, uri, &client,
 #ifdef ENABLE_DATABASE
-					   , nullptr
+					   nullptr,
 #endif
-					   );
+					   true);
 
 	switch (located_uri.type) {
 	case LocatedUri::Type::ABSOLUTE:

src/command/QueueCommands.cxx
@@ -70,11 +70,11 @@ handle_add(Client &client, Request args, [[maybe_unused]] Response &r)
 		: old_size;
 
 	const auto located_uri = LocateUri(UriPluginKind::INPUT, uri,
-					   &client
+					   &client,
 #ifdef ENABLE_DATABASE
-					   , nullptr
+					   nullptr,
 #endif
-					   );
+					   true);
 	switch (located_uri.type) {
 	case LocatedUri::Type::ABSOLUTE:
 		AddUri(client, located_uri);