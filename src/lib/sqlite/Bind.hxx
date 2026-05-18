1b67f49e077a583ba8e91aaab85ee917cadfadfd
Max Kellermann <max.kellermann@gmail.com>
2026-05-18 13:20:54 +0200

    lib/sqlite/Bind: simplify without recursive iteration

--- src/lib/sqlite/Bind.hxx
+++ src/lib/sqlite/Bind.hxx
@@ -22,22 +22,6 @@ Bind(sqlite3_stmt *stmt, unsigned i, const char *value)
 		throw SqliteError(stmt, result, "sqlite3_bind_text() failed");
 }
 
-template<typename... Args>
-static void
-BindAll2([[maybe_unused]] sqlite3_stmt *stmt, [[maybe_unused]] unsigned i)
-{
-	assert(int(i - 1) == sqlite3_bind_parameter_count(stmt));
-}
-
-template<typename... Args>
-static void
-BindAll2(sqlite3_stmt *stmt, unsigned i,
-	 const char *value, Args&&... args)
-{
-	Bind(stmt, i, value);
-	BindAll2(stmt, i + 1, std::forward<Args>(args)...);
-}
-
 /**
  * Throws #SqliteError on error.
  */
@@ -47,7 +31,8 @@ BindAll(sqlite3_stmt *stmt, Args&&... args)
 {
 	assert(int(sizeof...(args)) == sqlite3_bind_parameter_count(stmt));
 
-	BindAll2(stmt, 1, std::forward<Args>(args)...);
+	unsigned i = 1;
+	(Bind(stmt, i++, std::forward<Args>(args)), ...);
 }
 
 } // namespace Sqlite
