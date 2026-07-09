751138e0a551fae35813bfdf6d5669ce269b5e1a
Max Kellermann <max.kellermann@gmail.com>
2026-07-09 13:51:15 +0200

    lib/dbus/Connection: add namespace block

--- src/lib/dbus/Connection.cxx
+++ src/lib/dbus/Connection.cxx
@@ -5,29 +5,33 @@
 #include "Connection.hxx"
 #include "Error.hxx"
 
-ODBus::Connection
-ODBus::Connection::GetSystem()
+namespace ODBus {
+
+Connection
+Connection::GetSystem()
 {
-	ODBus::Error error;
+	Error error;
 	auto *c = dbus_bus_get(DBUS_BUS_SYSTEM, error);
 	error.CheckThrow("DBus connection error");
 	return Connection(c);
 }
 
-ODBus::Connection
-ODBus::Connection::GetSystemPrivate()
+Connection
+Connection::GetSystemPrivate()
 {
-	ODBus::Error error;
+	Error error;
 	auto *c = dbus_bus_get_private(DBUS_BUS_SYSTEM, error);
 	error.CheckThrow("DBus connection error");
 	return Connection(c);
 }
 
-ODBus::Connection
-ODBus::Connection::Open(const char *address)
+Connection
+Connection::Open(const char *address)
 {
-	ODBus::Error error;
+	Error error;
 	auto *c = dbus_connection_open(address, error);
 	error.CheckThrow("DBus connection error");
 	return Connection(c);
 }
+
+} // namespace ODBus
